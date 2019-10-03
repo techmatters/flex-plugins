956b62aa49608859b69370cfa0cd9fac05dac3dd
Nick Hurlburt <nick@techmatters.org>
2019-10-03 15:26:51 -0700

    Remove plugin-enforce-contact-data-submission, now fully folded into plugin-hrm-form

--- plugin-enforce-contact-data-submission/src/index.js
+++
@@ -1,4 +0,0 @@
-import * as FlexPlugin from 'flex-plugin';
-import EnforceContactDataSubmissionPlugin from './EnforceContactDataSubmissionPlugin';
-
-FlexPlugin.loadPlugin(EnforceContactDataSubmissionPlugin);
