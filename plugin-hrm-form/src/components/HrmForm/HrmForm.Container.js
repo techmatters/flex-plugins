d1a8c92d5f73671160a0fe757acef22f9ed5feab
Nick Hurlburt <nick@techmatters.org>
2019-10-04 16:12:02 -0700

    Removes field-level validation and just does submit validation with an alert box

--- plugin-hrm-form/src/components/HrmForm/HrmForm.Container.js
+++ plugin-hrm-form/src/components/HrmForm/HrmForm.Container.js
@@ -1,34 +1,68 @@
 import React from 'react'
 import HrmForm from './HrmForm'
+import { SubmissionError } from 'redux-form'
+
+
+const makeErrorString = errors => {
+  var myString = "Cannot complete task due to errors: \n";
+  Object.keys(errors).forEach( key => {
+    myString += "- " + key + ": " + errors[key] + "\n";
+  });
+  return myString;
+}
 
 class HrmFormContainer extends React.Component {
   submit = values => {
     const url = 'https://hrm.tl.barbarianrobot.com';
     // const url = 'http://localhost:8080';
+    const errors = {};
+    if (!values.ageBracket) {
+      errors.ageBracket = 'Age bracket is required';
+    }
+    if (!values.subcategory) {
+      errors.subcategory = 'Subcategory is required';
+    }
+    if (Object.keys(errors).length !== 0) {
+      alert(makeErrorString(errors));
+      throw new SubmissionError(errors);
+    }
     let formdata = {
       ...values,
       timestamp: 0,
       taskId: this.props.task.taskSid,
       reservationId: this.props.task.sid
     };
+    const completeTaskFunc = this.props.onCompleteTask;
+    const sid = this.props.task.sid;
     // print the form values to the console
-    console.log(formdata);
-    fetch(url + '/contacts', {
+    console.log("Sending: " + JSON.stringify(formdata));
+    return fetch(url + '/contacts', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formdata)
     })
     .then(function(response) {
+      if (!response.ok) {
+        console.log("Form error: " + response.statusText);
+        throw new SubmissionError({
+          _error: 'Unable to save form: ' + response.statusText
+        });
+      }
       return response.json();
     })
     .then(function(myJson) {
-      console.log(JSON.stringify(myJson));
+      console.log("Received: " + JSON.stringify(myJson));
+      return completeTaskFunc(sid);
     })
     .catch(function(response) {
-      alert("Failed!: " + response);
+      console.log("Caught something");
+      throw new SubmissionError({
+        _error: 'Unable to save data to server'
+      });
     });
-    this.props.onCompleteTask(this.props.task.sid);
   }
+
+
 
   render() {
     if (!this.props.task) {
@@ -36,7 +70,10 @@ class HrmFormContainer extends React.Component {
     }
     return (
       <div>
-        <HrmForm onSubmit={this.submit} form={ this.props.task.taskSid } />
+        <HrmForm 
+          onSubmit={this.submit}
+          form={ this.props.task.taskSid }
+        />
         { this.props.task.attributes && this.props.task.attributes.country &&
           <p>Country: { this.props.task.attributes.country }</p>
         }
