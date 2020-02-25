2002bc50c3423d1983cffaa49ce04c00dc54e2f3
Murilo Machado <murilovmachado@gmail.com>
2020-02-25 13:31:17 -0300

    Fake search results

--- plugin-hrm-form/src/components/Search.js
+++ plugin-hrm-form/src/components/Search.js
@@ -8,7 +8,62 @@ const handleSearch = async ({ firstName, lastName, counselor, area, phoneNumber,
   return results;
 };
 
-const results = [];
+const results = [
+  {
+    name: {
+      firstName: 'Jill',
+      lastName: 'Smith',
+    },
+    gender: 'Male',
+    age: '10-12',
+    language: 'Language 1',
+    nationality: 'Nationality 1',
+    ethnicity: 'Ethnicity 1',
+    school: {
+      name: 'State School',
+      gradeLevel: 'First grade',
+    },
+    location: {
+      streetAddress: 'Howard St',
+      city: 'San Francisco',
+      stateOrCountry: 'CA',
+      postalCode: '94105',
+      phone1: '415-555-0155',
+      phone2: '415-555-0158',
+    },
+    refugee: false,
+    disabledOrSpecialNeeds: false,
+    hiv: false,
+    callSummary: 'The child needs help',
+  },
+  {
+    name: {
+      firstName: 'Sarah',
+      lastName: 'Park',
+    },
+    gender: 'Female',
+    age: '18-25',
+    language: 'Language 2',
+    nationality: 'Nationality 2',
+    ethnicity: 'Ethnicity 2',
+    school: {
+      name: 'Federal School',
+      gradeLevel: 'Eighth grade',
+    },
+    location: {
+      streetAddress: 'Mission St',
+      city: 'San Francisco',
+      stateOrCountry: 'CA',
+      postalCode: '94105',
+      phone1: '415-555-0260',
+      phone2: '415-555-0265',
+    },
+    refugee: false,
+    disabledOrSpecialNeeds: false,
+    hiv: true,
+    callSummary: 'Young woman with HIV',
+  },
+];
 
 const Search = () =>
   <div>
