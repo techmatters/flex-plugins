0199dd7cbfb081602335fd6d76b40dc2a2ad4833
Nick Hurlburt <nick@techmatters.org>
2020-02-28 13:02:03 -0600

    Fix linting fail

--- plugin-hrm-form/src/___tests__/ActionCreators.test.js
+++ plugin-hrm-form/src/___tests__/ActionCreators.test.js
@@ -19,7 +19,7 @@ jest.mock('../states/ValidationRules', () => {
 
 test('deliberate fail', () => {
   expect(true).toBe(false);
-})
+});
 
 test('handleBlur sends the right action when called', () => {
   const dispatch = jest.fn(x => x);
