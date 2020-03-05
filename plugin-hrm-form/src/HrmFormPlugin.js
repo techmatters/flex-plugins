a120dba25c5a597998f79ece0b7caa48c6388cef
nickhurlburt <nickhurlburt@users.noreply.github.com>
2020-03-05 11:02:17 -0800

    Revert "Send a message to the caller when the counselor has ended the chat (#40)"
    This reverts commit 04f129ceeebffe898e5a14308f20234cec8fbd88.

--- plugin-hrm-form/src/HrmFormPlugin.js
+++ plugin-hrm-form/src/HrmFormPlugin.js
@@ -25,13 +25,9 @@ export default class HrmFormPlugin extends FlexPlugin {
     console.log(`Welcome to ${PLUGIN_NAME} Version ${PLUGIN_VERSION}`);
     this.registerReducers(manager);
 
-    const onCompleteTask = async (sid, task) => {
-      if (task.status !== 'wrapping') {
-        if (task.channelType === 'voice') {
-          await flex.Actions.invokeAction('HangupCall', { sid, task });
-        } else {
-          await flex.Actions.invokeAction('WrapupTask', { sid, task });
-        }
+    const onCompleteTask = (sid, task) => {
+      if (task.channelType === 'voice' && task.status !== 'wrapping') {
+        flex.Actions.invokeAction('HangupCall', { sid, task });
       }
       flex.Actions.invokeAction('CompleteTask', { sid, task });
     };
@@ -59,22 +55,6 @@ export default class HrmFormPlugin extends FlexPlugin {
     flex.Actions.addListener('beforeAcceptTask', payload => {
       manager.store.dispatch(Actions.initializeContactState(payload.task.taskSid));
     });
-
-    const exitMsg =
-      'Thank you for contacting the helpline. The counselor has left the chat but ' +
-      "don't hesitate to reach out again if you need help.";
-    const sendGoodbyeMessage = async (payload, original) => {
-      if (payload.task.taskChannelUniqueName === 'chat') {
-        await flex.Actions.invokeAction('SendMessage', {
-          body: exitMsg,
-          channelSid: payload.task.attributes.channelSid,
-        });
-        original(payload);
-      } else {
-        original(payload);
-      }
-    };
-    flex.Actions.replaceAction('WrapupTask', sendGoodbyeMessage);
 
     flex.Actions.addListener('beforeCompleteTask', (payload, abortFunction) => {
       manager.store.dispatch(Actions.saveContactState(payload.task, abortFunction, hrmBaseUrl));
