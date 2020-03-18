a1646a4e19e2c4f80473962be918be970b5b362e
gpaoloni <paolonigianfranco@gmail.com>
2020-03-18 14:11:21 -0300

    Version 0.3.7

--- plugin-hrm-form/src/HrmFormPlugin.js
+++ plugin-hrm-form/src/HrmFormPlugin.js
@@ -7,7 +7,7 @@ import reducers, { namespace } from './states';
 import { Actions } from './states/ContactState';
 
 const PLUGIN_NAME = 'HrmFormPlugin';
-const PLUGIN_VERSION = '0.3.6';
+const PLUGIN_VERSION = '0.3.7';
 
 export default class HrmFormPlugin extends FlexPlugin {
   constructor() {
