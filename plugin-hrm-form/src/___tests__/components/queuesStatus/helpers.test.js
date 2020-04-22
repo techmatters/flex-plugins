dc3b258cb3865733b37431c1632357305278e8fa
gpaoloni <paolonigianfranco@gmail.com>
2020-04-22 13:44:36 -0300

    Fixed and added tests

--- plugin-hrm-form/src/___tests__/components/queuesStatus/helpers.test.js
+++ plugin-hrm-form/src/___tests__/components/queuesStatus/helpers.test.js
@@ -1,4 +1,6 @@
 /* eslint-disable camelcase */
+import { omit } from 'lodash';
+
 import {
   newQueueEntry,
   initializeQueuesStatus,
@@ -9,11 +11,16 @@ import { channelTypes } from '../../../states/DomainConstants';
 
 test('Test newQueueEntry', () => {
   Object.keys(channelTypes).forEach(key => expect(newQueueEntry[key]).toEqual(0));
-  expect(newQueueEntry.oldestWaitingTask).toBeNull();
+  expect(newQueueEntry.longestWaitingTask).toStrictEqual({
+    taskId: null,
+    updated: null,
+    waitingMinutes: null,
+    intervalId: null,
+  });
 });
 
 const queuesNames = ['Queue One', 'Queue Two', 'Queue Nine Nine'];
-let queuesStatus;
+let cleanQueuesStatus;
 test('Test initializeQueuesStatus', () => {
   const queues = {
     Q1: { queue_name: queuesNames[0] },
@@ -28,38 +35,43 @@ test('Test initializeQueuesStatus', () => {
     expect(queuesNames.some(item => item === key)).toBeTruthy();
   });
 
-  queuesStatus = result;
+  cleanQueuesStatus = result;
 });
 
 const tasks = {
   T1: {
+    task_sid: 'T1',
     status: 'pending',
     date_updated: '2020-04-14T21:36:28.045Z',
     attributes: { channelType: channelTypes.facebook },
     queue_name: queuesNames[0],
   },
   T2: {
+    task_sid: 'T2',
     status: 'pending',
     date_updated: '2020-04-14T21:36:26.033Z',
     attributes: { channelType: channelTypes.web },
     queue_name: queuesNames[1],
   },
   T3: {
+    task_sid: 'T3',
     status: 'pending',
     date_updated: '2020-04-14T21:25:00.012Z',
     attributes: { channelType: channelTypes.facebook },
     queue_name: queuesNames[0],
   },
   T4: {
+    task_sid: 'T4',
     status: 'reserved',
   },
   T5: {
+    task_sid: 'T5',
     status: 'wrapping',
   },
 };
 
 test('Test addPendingTasks', () => {
-  const result1 = addPendingTasks(queuesStatus, tasks.T1);
+  const result1 = addPendingTasks(cleanQueuesStatus, tasks.T1);
   const result2 = addPendingTasks(result1, tasks.T2);
   const result3 = addPendingTasks(result2, tasks.T4);
   const result4 = addPendingTasks(result3, tasks.T3);
@@ -70,11 +82,41 @@ test('Test addPendingTasks', () => {
   expect(result4[queuesNames[0]].facebook).toBe(2);
 });
 
-test('Test updateQueuesStatus', () => {
-  const result = updateQueuesStatus(queuesStatus)(tasks);
+let queuesStatus;
+test('Test updateQueuesStatus with null prevQueuesStatus (first run)', () => {
+  const result = updateQueuesStatus(cleanQueuesStatus, tasks, null, jest.fn());
   expect(Object.keys(result).length).toBe(3);
   expect(result[queuesNames[0]].facebook).toBe(2);
   expect(result[queuesNames[1]].web).toBe(1);
-  expect(result[queuesNames[0]].oldestWaitingTask).toBe(tasks.T3.date_updated);
-  expect(result[queuesNames[1]].oldestWaitingTask).toBe(tasks.T2.date_updated);
+  expect(result[queuesNames[0]].longestWaitingTask.updated).toBe(tasks.T3.date_updated);
+  expect(result[queuesNames[1]].longestWaitingTask.updated).toBe(tasks.T2.date_updated);
+
+  queuesStatus = result;
+});
+
+test('Test updateQueuesStatus with same pending tasks', () => {
+  const result = updateQueuesStatus(cleanQueuesStatus, omit(tasks, 'T5'), queuesStatus, jest.fn());
+  expect(result).toStrictEqual(queuesStatus);
+});
+
+const tasks2 = {
+  ...tasks,
+  T6: {
+    task_sid: 'T6',
+    status: 'pending',
+    date_updated: '2020-03-14T21:25:00.012Z',
+    attributes: { channelType: channelTypes.facebook },
+    queue_name: queuesNames[0],
+  },
+};
+
+test('Test updateQueuesStatus with a new pending task (with older date_update)', () => {
+  const result = updateQueuesStatus(cleanQueuesStatus, tasks2, queuesStatus, jest.fn());
+  expect(Object.keys(result).length).toBe(3);
+  expect(result[queuesNames[0]].facebook).toBe(3);
+  expect(result[queuesNames[1]].web).toBe(1);
+  expect(result[queuesNames[0]].longestWaitingTask.updated).toBe(tasks2.T6.date_updated);
+  expect(result[queuesNames[1]].longestWaitingTask.updated).toBe(tasks2.T2.date_updated);
+
+  queuesStatus = result;
 });
