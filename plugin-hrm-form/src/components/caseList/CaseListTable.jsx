4ad753765cd9df287b38ad6882cd9fd2407e880e
gpaoloni <paolonigianfranco@gmail.com>
2020-07-02 10:26:21 -0300

    Table almost there

--- plugin-hrm-form/src/components/caseList/CaseListTable.jsx
+++ plugin-hrm-form/src/components/caseList/CaseListTable.jsx
@@ -1,18 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table, TableBody, TableCell, TableFooter, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@material-ui/core';
 
-import { Box } from '../../styles/HrmStyles';
+import {
+  CaseListTableCell,
+  CaseListTableRow,
+  TableContainer,
+  CaseListBodyCell,
+  TableBodyActiveFont,
+} from '../../styles/caseList';
 import CaseListTableHead from './CaseListTableHead';
 
+/**
+ * This component is splitted to make it easier to read, but is basically a 8 columns Table (7 for data, 1 for the "expand" button)
+ */
 const CaseListTable = ({ caseList }) => {
   return (
-    <Box>
+    <TableContainer>
       <Table>
         <CaseListTableHead />
-        <TableBody />
+        <TableBody>
+          <CaseListTableRow>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>#123456</TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>Kurt McKinley</TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>
+                Jill Peterson called to say Kurt was thinking of taking his Fanny pack leggings hammock, excepteur id
+                celiac irure direct trade put a bird on it enamel pin banjo quinoa exercitation. Umami pickled in shabby
+                chic, aliquip… readymade aliqua. Quinoa authentic ex, keffiyeh squid do laboris ut officia tattooed
+                skateboard. Artisan cloud bread XOXO dolore hoodie cillum salvia wayfarers small batch adipisicing lyft
+                sunt.
+              </TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>Gianfranco Paoloni</TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>Jun 8, 2019</TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <TableBodyActiveFont>Jul 10, 2019</TableBodyActiveFont>
+            </CaseListBodyCell>
+            <CaseListBodyCell>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+              </div>
+            </CaseListBodyCell>
+            <CaseListBodyCell />
+          </CaseListTableRow>
+        </TableBody>
       </Table>
-    </Box>
+    </TableContainer>
   );
 };
 
--- plugin-hrm-form/src/styles/caseList/index.js
+++ plugin-hrm-form/src/styles/caseList/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { TableCell } from '@material-ui/core';
+import { TableCell, TableRow } from '@material-ui/core';
 
 import { Absolute, FontOpenSans } from '../HrmStyles';
+
+export const TableContainer = styled('div')`
+  border-left: 15px solid ${props => props.theme.colors.base2};
+  border-right: 10px solid ${props => props.theme.colors.base2};
+`;
 
 export const CaseListContainer = styled(Absolute)`
   height: 100%;
@@ -15,9 +20,29 @@ export const CaseListTableCell = styled(TableCell)`
   text-transform: none !important;
 `;
 
+export const CaseListBodyCell = styled(CaseListTableCell)`
+  max-height: 85px !important;
+  vertical-align: top !important;
+  padding-top: 12px !important;
+`;
+
+export const CaseListTableRow = styled(TableRow)`
+  background: #ffffff;
+  border: 1px solid rgba(127, 134, 155, 0.07);
+  border-radius: 4px;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export const TableHeaderFont = styled(FontOpenSans)`
   font-weight: 600;
   font-size: 12px;
   line-height: 30px;
   letter-spacing: 1px;
+`;
+
+export const TableBodyActiveFont = styled(FontOpenSans)`
+  color: #192b33;
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 600;
 `;
