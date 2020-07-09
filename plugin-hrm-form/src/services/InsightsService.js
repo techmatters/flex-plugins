791412781d043de0660d0569fa87e7d4286b23f8
Nick Hurlburt <nick@techmatters.org>
2020-07-09 13:52:08 -0700

    Store all three categories concatenated in insights and do not store individual categories

--- plugin-hrm-form/src/services/InsightsService.js
+++ plugin-hrm-form/src/services/InsightsService.js
@@ -19,7 +19,8 @@ function getSubcategories(task) {
     });
   });
 
-  return result.splice(0, 2);
+  // grab all three categories -- does this do that?  this comment can be removed
+  return result.splice(0, 3);
 }
 
 function buildConversationsObject(task) {
@@ -33,8 +34,6 @@ function buildConversationsObject(task) {
   return {
     conversation_attribute_1: subcategories.join(';'),
     conversation_attribute_2: callType,
-    conversation_attribute_3: subcategories[0],
-    conversation_attribute_4: subcategories[1],
   };
 }
 
