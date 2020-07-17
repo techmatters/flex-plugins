5b61eb7b0a9784039c341814ef29af4bddf06997
Murilo Machado <murilovmachado@gmail.com>
2020-07-17 19:32:50 -0300

    Unit test for clicking on Cancel case

--- plugin-hrm-form/src/___tests__/components/case/Case.test.js
+++ plugin-hrm-form/src/___tests__/components/case/Case.test.js
@@ -10,6 +10,22 @@ import HrmTheme from '../../../styles/HrmTheme';
 import Case from '../../../components/case';
 import CaseDetails from '../../../components/case/CaseDetails';
 import { namespace, configurationBase, contactFormsBase } from '../../../states';
+import { cancelCase } from '../../../services/CaseService';
+
+jest.mock('../../../services/CaseService');
+
+/**
+ * Fix issue with Popper.js:
+ * https://stackoverflow.com/questions/60333156/how-to-fix-typeerror-document-createrange-is-not-a-function-error-while-testi
+ */
+global.document.createRange = () => ({
+  setStart: () => null,
+  setEnd: () => null,
+  commonAncestorContainer: {
+    nodeName: 'BODY',
+    ownerDocument: document,
+  },
+});
 
 expect.extend(toHaveNoViolations);
 const mockStore = configureMockStore([]);
@@ -115,6 +131,69 @@ test('Case (should render)', () => {
     status: 'open',
     date: '6/29/2020', // the day the createdAt number represents
   });
+});
+
+function openCancelMenu(wrapper) {
+  wrapper
+    .find('button')
+    .find('t')
+    .findWhere(t => t.prop('code') === 'BottomBar-Cancel')
+    .simulate('click');
+}
+
+function clickCancelCase(wrapper) {
+  wrapper
+    .find('MenuItem')
+    .find('t')
+    .findWhere(t => t.prop('code') === 'BottomBar-CancelNewCaseAndClose')
+    .simulate('click');
+}
+
+test('click cancel button', () => {
+  const ownProps = {
+    task: {
+      taskSid: 'task1',
+    },
+    handleCompleteTask: jest.fn(),
+  };
+  const initialState = createState({
+    [configurationBase]: {
+      counselors: {
+        list: [],
+        hash: { worker1: 'worker1 name' },
+      },
+    },
+    [contactFormsBase]: {
+      tasks: {
+        task1: {
+          childInformation: {
+            name: { firstName: { value: 'first' }, lastName: { value: 'first' } },
+          },
+          metadata: {
+            connectedCase: {
+              createdAt: '12345',
+              twilioWorkerId: 'worker1',
+              status: 'open',
+            },
+          },
+        },
+      },
+    },
+  });
+  const store = mockStore(initialState);
+
+  const wrapper = mount(
+    <StorelessThemeProvider themeConf={themeConf}>
+      <Provider store={store}>
+        <Case {...ownProps} />
+      </Provider>
+    </StorelessThemeProvider>,
+  );
+
+  openCancelMenu(wrapper);
+  clickCancelCase(wrapper);
+
+  expect(cancelCase).toHaveBeenCalled();
 });
 
 const Wrapped = withTheme(props => <Case {...props} />);
