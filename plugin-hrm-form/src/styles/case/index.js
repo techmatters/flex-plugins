a639e3409326a4252774e5a2011f058eab059611
gpaoloni <paolonigianfranco@gmail.com>
2020-07-29 17:12:14 -0300

    Implemented Case Summary

--- plugin-hrm-form/src/styles/case/index.js
+++ plugin-hrm-form/src/styles/case/index.js
@@ -99,15 +99,13 @@ export const CaseActionDetailFont = styled(FontOpenSans)`
 `;
 CaseActionDetailFont.displayName = 'CaseActionDetailFont';
 
-export const CaseActionTextArea = styled('textarea')`
+const BaseTextArea = styled('textarea')`
   resize: none;
-  width: 65%;
   background-color: ${props => props.theme.colors.base2};
   font-family: Open Sans;
-  font-size: 12px;
   font-weight: 500;
+  font-size: 12px;
   line-height: 15px;
-  margin-top: 10px;
   padding: 5px;
   border-style: none;
   border-radius: 4px;
@@ -115,4 +113,19 @@ export const CaseActionTextArea = styled('textarea')`
     outline: none;
   }
 `;
+
+export const CaseActionTextArea = styled(BaseTextArea)`
+  width: 65%;
+  margin-top: 10px;
+`;
 CaseActionTextArea.displayName = 'CaseActionTextArea';
+
+export const CaseSummaryTextArea = styled(BaseTextArea)`
+  height: 100%;
+  width: 100%;
+  margin: 10px 0px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+`;
+CaseSummaryTextArea.displayName = 'CaseSummaryTextArea';
