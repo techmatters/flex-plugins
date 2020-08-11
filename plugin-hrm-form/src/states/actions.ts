cbeceb875c8888c1d1a439e42bf1b8e278a709ae
gpaoloni <paolonigianfranco@gmail.com>
2020-08-11 12:22:24 -0300

    Common action for recreate contact state

--- plugin-hrm-form/src/states/actions.ts
+++ plugin-hrm-form/src/states/actions.ts
@@ -1,7 +1,12 @@
-import { GeneralActionType, INITIALIZE_CONTACT_STATE, REMOVE_CONTACT_STATE } from './types';
+import { GeneralActionType, INITIALIZE_CONTACT_STATE, RECREATE_CONTACT_STATE, REMOVE_CONTACT_STATE } from './types';
 
 export const initializeContactState = (taskId: string): GeneralActionType => ({
   type: INITIALIZE_CONTACT_STATE,
+  taskId,
+});
+
+export const recreateContactState = (taskId: string): GeneralActionType => ({
+  type: RECREATE_CONTACT_STATE,
   taskId,
 });
 
