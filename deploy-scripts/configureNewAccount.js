6aa69c6dc68af94410488b56c168d3c2e51ad742
Ariel R-A <raarielgrace@gmail.com>
2020-08-17 10:36:49 -0400

    Updated package linting.

--- deploy-scripts/configureNewAccount.js
+++ deploy-scripts/configureNewAccount.js
@@ -1,6 +1,44 @@
+/* eslint-disable no-console */
 const fs = require("fs").promises;
-let oldIdDict = {};
-let newIdDict = {};
+
+const oldIdDict = {};
+const newIdDict = {};
+
+/**
+ * Compares lists of JSON objects and creates or updates objects in the new
+ * account to match those of the original
+ * @param {*} orig The original list of objects
+ * @param {*} copy The new list of objects
+ * @param {*} prefix The prefix to use when saving SIDs (should be unique to the call)
+ * @param {*} props A list of key names to copy from the original objects
+ * @param {*} baseFn The function to call 'create' or 'update' on
+ */
+async function createMatchingList(orig, copy, prefix, props, baseFn) {
+  await Promise.all(
+    orig.map(async (obj) => {
+      const newConfig = Object.fromEntries(
+        Object.keys(obj)
+          .filter((key) => props.includes(key))
+          .map((key) => [key, obj[key]])
+      );
+
+      // Might need to add something where IDs are copied
+
+      const friendlyN = obj.friendlyName;
+      let newObj = copy.find((e) => e.friendlyName === friendlyN);
+
+      if (newObj == null) {
+        newConfig.friendlyName = friendlyN;
+        newObj = await baseFn.create(newConfig);
+      } else {
+        await baseFn(newObj.sid).update(newConfig);
+      }
+
+      oldIdDict[prefix + friendlyN] = obj.sid;
+      newIdDict[prefix + friendlyN] = newObj.sid;
+    })
+  );
+}
 
 /**
  * Copies the Workspaces, TaskChannels, and Activities of the staging account to
@@ -9,17 +47,19 @@ let newIdDict = {};
  * @param {*} newAccAuth The authentication token of the account to copy to.
  */
 async function setUpTaskRouter(newAccSid, newAccAuth) {
+  // eslint-disable-next-line global-require
   const newClient = require("twilio")(newAccSid, newAccAuth);
   let client;
   try {
     const flowStr = await fs.readFile("staging.json", "utf8");
     const vals = JSON.parse(flowStr);
+    // eslint-disable-next-line global-require
     client = require("twilio")(vals.AccountSid, vals.AuthToken);
   } catch (err) {
     console.log(err);
     console.log(
-      `ERROR: This script requires a file 'staging.json' with the SID ` +
-        `and auth token of the account you wish to copy the workspaces of.`
+      "ERROR: This script requires a file 'staging.json' with the SID " +
+        "and auth token of the account you wish to copy the workspaces of."
     );
     return;
   }
@@ -28,13 +68,13 @@ async function setUpTaskRouter(newAccSid, newAccAuth) {
   const oldWSes = await client.taskrouter.workspaces.list();
   await Promise.all(
     oldWSes.map(async (oldWS) => {
-      const friendlyN = oldWS["friendlyName"];
-      oldIdDict["WS_" + friendlyN] = oldWS["sid"];
+      const friendlyN = oldWS.friendlyName;
+      oldIdDict[`WS_${friendlyN}`] = oldWS.sid;
       const newWS = await newClient.taskrouter.workspaces.create({
         friendlyName: friendlyN,
-        template: oldWS["prioritizeQueueOrder"] === "FIFO" ? "FIFO" : "NONE",
+        template: oldWS.prioritizeQueueOrder === "FIFO" ? "FIFO" : "NONE",
       });
-      newIdDict["WS_" + friendlyN] = newWS["sid"];
+      newIdDict[`WS_${friendlyN}`] = newWS.sid;
     })
   );
 
@@ -82,43 +122,7 @@ async function setUpTaskRouter(newAccSid, newAccAuth) {
   }
 }
 
-/**
- * Compares lists of JSON objects and creates or updates objects in the new
- * account to match those of the original
- * @param {*} orig The original list of objects
- * @param {*} copy The new list of objects
- * @param {*} prefix The prefix to use when saving SIDs (should be unique to the call)
- * @param {*} props A list of key names to copy from the original objects
- * @param {*} baseFn The function to call 'create' or 'update' on
- */
-async function createMatchingList(orig, copy, prefix, props, baseFn) {
-  await Promise.all(
-    orig.map(async (obj) => {
-      let newConfig = Object.fromEntries(
-        Object.keys(obj)
-          .filter((key) => props.includes(key))
-          .map((key) => [key, obj[key]])
-      );
-
-      // Might need to add something where IDs are copied
-
-      const friendlyN = obj["friendlyName"];
-      let newObj = copy.find((e) => e["friendlyName"] === friendlyN);
-
-      if (newObj == null) {
-        newConfig["friendlyName"] = friendlyN;
-        newObj = await baseFn.create(newConfig);
-      } else {
-        await baseFn(newObj["sid"]).update(newConfig);
-      }
-
-      oldIdDict[prefix + friendlyN] = obj["sid"];
-      newIdDict[prefix + friendlyN] = newObj["sid"];
-    })
-  );
-}
-
-const argv = require("yargs")
+const { argv } = require("yargs")
   .usage(
     "npm run configure -- --sid={SID} --authToken={token} [--delete={workspace SID}]"
   )
@@ -131,19 +135,22 @@ const argv = require("yargs")
     "delete",
     "Optional. Switches script into delete mode and deletes the given workspace."
   )
-  .demandOption(["sid", "authToken"]).argv;
+  .demandOption(["sid", "authToken"]);
 
 if (argv.delete) {
+  // eslint-disable-next-line global-require
   const client = require("twilio")(argv.sid, argv.authToken);
   client.taskrouter
     .workspaces(argv.delete)
     .remove()
     .then(
+      // eslint-disable-next-line no-unused-vars
       (_) => console.log("Workspace deleted."),
       (err) => console.log(err)
     );
 } else {
   setUpTaskRouter(argv.sid, argv.authToken).then(
+    // eslint-disable-next-line no-unused-vars
     (_) => console.log("Workspace set-up complete."),
     (err) => console.log(err)
   );
--- deploy-scripts/copyFlow.js
+++ deploy-scripts/copyFlow.js
@@ -1,84 +1,6 @@
+/* eslint-disable no-console */
 const fs = require("fs").promises;
-
-/**
- * Copies the structure of a studio flow from one account and posts it, with
- * functional SIDS, to a new account.
- * @param {string} oldIdsFile - The relative path to the sids of the original account
- * @param {string} newIdsFile - The relative path to the sids of the new account
- * @param {string} flowFile - Optional string of a JSON file of the flow to copy
- * @param {boolean} update - Whether to update a flow or create a new one
- */
-async function copyAndPostFlow(
-  oldIdsFile,
-  newIdsFile,
-  flowFile,
-  update = false
-) {
-  const oldIdStr = await fs.readFile(oldIdsFile, "utf8");
-  const oldIds = JSON.parse(oldIdStr);
-  const client = require("twilio")(oldIds["AccountSid"], oldIds["AuthToken"]);
-
-  let toCopy;
-  if (flowFile != null) {
-    const flowStr = await fs.readFile(flowFile, "utf8");
-    toCopy = JSON.parse(flowStr);
-  } else {
-    toCopy = await client.studio.flows(oldIds["Flow_To_Copy"]).fetch();
-  }
-
-  // Delete keys with account and editing information about old flow
-  const keysToRemove = [
-    "sid",
-    "accountSid",
-    "revision",
-    "dateCreated",
-    "dateUpdated",
-    "webhookUrl",
-    "url",
-    "links",
-  ];
-  keysToRemove.forEach((key) => delete toCopy[key]);
-
-  const newIdStr = await fs.readFile(newIdsFile, "utf8");
-  const newIds = JSON.parse(newIdStr);
-  const newClient = require("twilio")(
-    newIds["AccountSid"],
-    newIds["AuthToken"]
-  );
-
-  let newFlow;
-  try {
-    newFlow = recReplaceIds(toCopy, oldIds, newIds);
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-
-  newFlow["status"] = "draft";
-  newFlow["commitMessage"] = "Copied flow.";
-
-  console.log(JSON.stringify(newFlow, null, 1));
-  const Confirm = require("prompt-confirm");
-  const prompt = new Confirm("This studio flow will be posted. Continue?");
-
-  const answer = await prompt.run();
-  if (!answer) return;
-
-  if (update) {
-    newClient.studio
-      .flows(newIds["Flow_To_Update"])
-      .update(newFlow)
-      .then(
-        (f) => console.log("Done! Flow " + f.sid + " has been updated."),
-        (err) => console.log(err)
-      );
-  } else {
-    newClient.studio.flows.create(newFlow).then(
-      (f) => console.log("Done! New flow draft created as " + f.sid),
-      (err) => console.log(err)
-    );
-  }
-}
+const Confirm = require("prompt-confirm");
 
 /**
  * Recursively replaces the sids with those of the new account
@@ -110,11 +32,11 @@ function recReplaceIds(flow, oldIdDict, newIdDict) {
             val.indexOf("TC") === 0
           ) {
             if (val !== "default") {
-              const idName = idKeys.find((key) => oldIdDict[key] === val);
+              const idName = idKeys.find((idKey) => oldIdDict[idKey] === val);
 
               if (idName == null) {
-                throw (
-                  "ERROR: The id " + val + " is not present in the fromId file."
+                throw new Error(
+                  `ERROR: The id ${val} is not present in the fromId file.`
                 );
               } else {
                 val = newIdDict[idName];
@@ -129,7 +51,85 @@ function recReplaceIds(flow, oldIdDict, newIdDict) {
   return newFlow;
 }
 
-const argv = require("yargs")
+/**
+ * Copies the structure of a studio flow from one account and posts it, with
+ * functional SIDS, to a new account.
+ * @param {string} oldIdsFile - The relative path to the sids of the original account
+ * @param {string} newIdsFile - The relative path to the sids of the new account
+ * @param {string} flowFile - Optional string of a JSON file of the flow to copy
+ * @param {boolean} update - Whether to update a flow or create a new one
+ */
+async function copyAndPostFlow(
+  oldIdsFile,
+  newIdsFile,
+  flowFile,
+  update = false
+) {
+  const oldIdStr = await fs.readFile(oldIdsFile, "utf8");
+  const oldIds = JSON.parse(oldIdStr);
+  // eslint-disable-next-line global-require
+  const client = require("twilio")(oldIds.AccountSid, oldIds.AuthToken);
+
+  let toCopy;
+  if (flowFile != null) {
+    const flowStr = await fs.readFile(flowFile, "utf8");
+    toCopy = JSON.parse(flowStr);
+  } else {
+    toCopy = await client.studio.flows(oldIds.Flow_To_Copy).fetch();
+  }
+
+  // Delete keys with account and editing information about old flow
+  const keysToRemove = [
+    "sid",
+    "accountSid",
+    "revision",
+    "dateCreated",
+    "dateUpdated",
+    "webhookUrl",
+    "url",
+    "links",
+  ];
+  keysToRemove.forEach((key) => delete toCopy[key]);
+
+  const newIdStr = await fs.readFile(newIdsFile, "utf8");
+  const newIds = JSON.parse(newIdStr);
+  // eslint-disable-next-line global-require
+  const newClient = require("twilio")(newIds.AccountSid, newIds.AuthToken);
+
+  let newFlow;
+  try {
+    newFlow = recReplaceIds(toCopy, oldIds, newIds);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  newFlow.status = "draft";
+  newFlow.commitMessage = "Copied flow.";
+
+  console.log(JSON.stringify(newFlow, null, 1));
+  const prompt = new Confirm("This studio flow will be posted. Continue?");
+
+  const answer = await prompt.run();
+  if (!answer) return;
+
+  if (update) {
+    newClient.studio
+      .flows(newIds.Flow_To_Update)
+      .update(newFlow)
+      .then(
+        (f) => console.log(`Done! Flow ${f.sid} has been updated.`),
+        (err) => console.log(err)
+      );
+  } else {
+    newClient.studio.flows.create(newFlow).then(
+      (f) => console.log(`Done! New flow draft created as ${f.sid}`),
+      (err) => console.log(err)
+    );
+  }
+}
+
+const { argv } = require("yargs")
   .usage(
     "npm run copyFlow -- --fromIds {file} --toIds {file} [--useFlow {file}] [--update]"
   )
@@ -138,24 +138,24 @@ const argv = require("yargs")
   .alias("u", "useFlow")
   .describe(
     "f",
-    `JSON filename with original account SIDs. To copy a flow on Twilio, ` +
-      `this file must have the key 'Flow_to_Copy' with a value of the flow's ` +
-      `SID.`
+    "JSON filename with original account SIDs. To copy a flow on Twilio, " +
+      "this file must have the key 'Flow_to_Copy' with a value of the flow's " +
+      "SID."
   )
   .describe(
     "t",
-    `JSON filename with SIDs of the account being copied to. The key names have ` +
-      `no required format, but corresponding SIDs must have the same keys ` +
-      `across the two files.`
+    "JSON filename with SIDs of the account being copied to. The key names have " +
+      "no required format, but corresponding SIDs must have the same keys " +
+      "across the two files."
   )
   .describe("u", "Use a local JSON instead of a Twilio flow")
   .describe(
     "update",
-    `Update an existing flow instead of creating one. There must be the key ` +
-      `Flow_to_Update' in the toIds file with a value of the flow's SID.`
+    "Update an existing flow instead of creating one. There must be the key " +
+      "Flow_to_Update' in the toIds file with a value of the flow's SID."
   )
   .boolean(["update"])
   .default("update", false)
-  .demandOption(["fromIds", "toIds"]).argv;
+  .demandOption(["fromIds", "toIds"]);
 
 copyAndPostFlow(argv.fromIds, argv.toIds, argv.useFlow, argv.update);
