67aa99a3674fbb8181f45555921cd2fde480494b
gpaoloni <paolonigianfranco@gmail.com>
2020-08-21 13:01:29 -0300

    Lint

--- plugin-hrm-form/src/components/common/forms/index.ts
+++ plugin-hrm-form/src/components/common/forms/index.ts
@@ -2,7 +2,6 @@ import { CallerFormInformation } from './CallerForm';
 import { createBlankForm } from '../../../states/ContactFormStateFactory';
 
 export * from './CallerForm';
-export { default as CallerForm } from './CallerForm';
 
 // TODO: fix this type error (createBlankForm must be typed or maybe create a separate function)
 export const newCallerFormInformation: CallerFormInformation = createBlankForm().callerInformation;
