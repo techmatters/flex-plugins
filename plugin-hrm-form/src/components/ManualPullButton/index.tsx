10fd5e2801a292773d38d8ebc92fddf9de4cabbe
gpaoloni <paolonigianfranco@gmail.com>
2020-10-08 14:22:54 -0300

    Manual pull button disabled if no pending chat

--- plugin-hrm-form/src/components/ManualPullButton/index.tsx
+++ plugin-hrm-form/src/components/ManualPullButton/index.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { ButtonBase } from '@material-ui/core';
 import { Template } from '@twilio/flex-ui';
+import { connect } from 'react-redux';
 
+import type { QueuesStatusState } from '../../states/queuesStatus/reducer';
+import { namespace, queuesStatusBase } from '../../states';
+import { isChatPending } from '../queuesStatus/helpers';
 import { ManualPullIconContainer, ManualPullIcon, ManualPullContent, ManualPullText } from '../../styles/HrmStyles';
 import { adjustChatCapacity } from '../../services/ServerlessService';
 
@@ -9,21 +14,39 @@ const increaseChatCapacity = async () => {
   await adjustChatCapacity('increase');
 };
 
-type Props = {};
+type OwnProps = {};
 
-const ManualPullButton: React.FC<Props> = () => (
-  <ButtonBase onClick={increaseChatCapacity} className="Twilio-TaskListBaseItem-UpperArea css-xz5ie1">
-    <ManualPullIconContainer>
-      <ManualPullIcon icon="Add" />
-    </ManualPullIconContainer>
-    <ManualPullContent>
-      <ManualPullText>
-        <Template code="ManualPullButtonText" />
-      </ManualPullText>
-    </ManualPullContent>
-  </ButtonBase>
-);
+// eslint-disable-next-line no-use-before-define
+type Props = OwnProps & ReturnType<typeof mapStateToProps>;
+
+const ManualPullButton: React.FC<Props> = ({ queuesStatusState }) => {
+  const disabled = !isChatPending(queuesStatusState.queuesStatus);
+  // Object.values(queuesStatusState.queuesStatus).forEach(e => console.log(e));
+
+  return (
+    <ButtonBase
+      onClick={increaseChatCapacity}
+      className="Twilio-TaskListBaseItem-UpperArea css-xz5ie1"
+      disabled={disabled}
+    >
+      <ManualPullIconContainer>
+        <ManualPullIcon icon="Add" />
+      </ManualPullIconContainer>
+      <ManualPullContent>
+        <ManualPullText>
+          <Template code="ManualPullButtonText" />
+        </ManualPullText>
+      </ManualPullContent>
+    </ButtonBase>
+  );
+};
 
 ManualPullButton.displayName = 'ManualPullButton';
 
-export default ManualPullButton;
+const mapStateToProps = (state, ownProps: OwnProps) => {
+  const queuesStatusState: QueuesStatusState = state[namespace][queuesStatusBase]; // casting type as inference is not working for the store yet
+
+  return { queuesStatusState };
+};
+
+export default connect(mapStateToProps, null)(ManualPullButton);
