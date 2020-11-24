ba31cda64373ace04d0d50336f90bdfcf89caa15
Murilo Machado <murilovmachado@gmail.com>
2020-11-24 18:19:09 -0300

    Change ... menu to be just the view case icon

--- plugin-hrm-form/src/components/search/CasePreview/CaseHeader.tsx
+++ plugin-hrm-form/src/components/search/CasePreview/CaseHeader.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import { format } from 'date-fns';
 import { Template } from '@twilio/flex-ui';
-import { Button } from '@material-ui/core';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import Link from '@material-ui/icons/Link';
 import CropFreeIcon from '@material-ui/icons/CropFree';
-import CallMergeIcon from '@material-ui/icons/CallMerge';
 
-import { Menu, MenuItem } from '../../menu';
-import { CaseHeaderContainer, CaseHeaderCaseId, CaseHeaderChildName, DateText } from '../../../styles/search';
+import {
+  CaseHeaderContainer,
+  CaseHeaderCaseId,
+  CaseHeaderChildName,
+  DateText,
+  StyledButtonBase,
+} from '../../../styles/search';
+import { StyledIcon, addHover, HiddenText } from '../../../styles/HrmStyles';
+
+const ViewCaseIcon = addHover(StyledIcon(CropFreeIcon));
 
 type OwnProps = {
   caseId: number;
@@ -27,16 +31,6 @@ const CaseHeader: React.FC<Props> = ({ caseId, childName, createdAt, updatedAt }
   const createdAtFormatted = `${format(new Date(createdAt), 'MMM d, yyyy')}`;
   const updatedAtFormatted = `${format(new Date(updatedAt), 'MMM d, yyyy')}`;
 
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const { firstName, lastName } = childName || {};
   return (
     <CaseHeaderContainer>
@@ -48,20 +42,12 @@ const CaseHeader: React.FC<Props> = ({ caseId, childName, createdAt, updatedAt }
       <DateText>
         <Template code="CaseHeader-Updated" />: {updatedAtFormatted}
       </DateText>
-      <Button onClick={handleClick}>
-        <DateText>
-          <MoreHorizIcon />
-        </DateText>
-      </Button>
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClickAway={handleClose}>
-        <MenuItem Icon={CropFreeIcon} text={<Template code="CaseHeader-ViewCase" />} onClick={handleClose} />
-        <MenuItem
-          Icon={CallMergeIcon}
-          text={<Template code="CaseHeader-MergeContactIntoCase" />}
-          onClick={handleClose}
-        />
-        <MenuItem Icon={Link} text={<Template code="CaseHeader-CopyChildInfo" />} onClick={handleClose} />
-      </Menu>
+      <StyledButtonBase onClick={() => null}>
+        <HiddenText>
+          <Template code="CaseHeader-ViewCase" />
+        </HiddenText>
+        <ViewCaseIcon style={{ fontSize: '20px' }} />
+      </StyledButtonBase>
     </CaseHeaderContainer>
   );
 };
