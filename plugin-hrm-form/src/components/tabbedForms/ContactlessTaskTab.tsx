e4140b59c184343be90bc069676844355975a785
gpaoloni <paolonigianfranco@gmail.com>
2020-11-27 14:12:33 -0300

    Fixed future validation error

--- plugin-hrm-form/src/components/tabbedForms/ContactlessTaskTab.tsx
+++ plugin-hrm-form/src/components/tabbedForms/ContactlessTaskTab.tsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import { ITask, withTaskContext } from '@twilio/flex-ui';
 import { connect, ConnectedProps } from 'react-redux';
-import { useFormContext } from 'react-hook-form';
+import { FieldError, useFormContext } from 'react-hook-form';
 import { isFuture } from 'date-fns';
+import { get } from 'lodash';
 
 import { createFormFromDefinition } from '../common/forms/formGenerators';
 import { updateContactLessTask } from '../../states/ContactState';
-import { Container, ColumnarBlock, TwoColumnLayout, Box } from '../../styles/HrmStyles';
+import { Container, ColumnarBlock, TwoColumnLayout, Box, FormError } from '../../styles/HrmStyles';
 import type { RootState } from '../../states';
 import { formDefinition } from './ContactlessTaskTabDefinition';
 import { splitDate, splitTime } from '../../utils/helpers';
@@ -21,11 +22,11 @@ type OwnProps = {
 type Props = OwnProps & ConnectedProps<typeof connector>;
 
 const ContactlessTaskTab: React.FC<Props> = ({ dispatch, display, task }) => {
-  const { getValues, setError, watch, errors } = useFormContext();
+  const { getValues, register, setError, trigger, watch, errors } = useFormContext();
 
   const contactlessTaskForm = React.useMemo(() => {
     const updateCallBack = () => {
-      const { contactlessTask } = getValues();
+      const { isFutureAux, ...contactlessTask } = getValues().contactlessTask;
       dispatch(updateContactLessTask(contactlessTask, task.taskSid));
     };
     return createFormFromDefinition(formDefinition)(['contactlessTask'])(updateCallBack).map(i => (
@@ -35,17 +36,38 @@ const ContactlessTaskTab: React.FC<Props> = ({ dispatch, display, task }) => {
     ));
   }, [dispatch, getValues, task.taskSid]);
 
+  // Add invisible field that errors if date + time are future (triggered by validaiton)
+  React.useEffect(() => {
+    register('contactlessTask.isFutureAux', {
+      validate: () => {
+        const { contactlessTask } = getValues();
+        const { date, time } = contactlessTask;
+        if (date && time) {
+          const [y, m, d] = splitDate(date);
+          const [mm, hh] = splitTime(time);
+          if (isFuture(new Date(y, m - 1, d, mm, hh))) {
+            return 'TimeCantBeGreaterThanNow'; // return non-null to generate an error, using the localized error key
+          }
+        }
+
+        return null;
+      },
+    });
+  }, [getValues, register, setError]);
+
   const date = watch('contactlessTask.date');
   const time = watch('contactlessTask.time');
 
+  // Trigger validation on isFutureAux (triggered by date or time onChange)
   React.useEffect(() => {
-    if (date && time) {
-      const [y, m, d] = splitDate(date);
-      const [mm, hh] = splitTime(time);
-      if (isFuture(new Date(y, m - 1, d, mm, hh)))
-        setError('contactlessTask.time', { message: 'TimeCantBeGreaterThanNow' });
-    }
-  }, [date, errors, setError, time]);
+    trigger('contactlessTask.isFutureAux');
+  }, [date, time, trigger]);
+
+  // Replicate error in time if there is error in isFutureAux
+  const isFutureError: FieldError = get(errors, 'contactlessTask.isFutureAux');
+  React.useEffect(() => {
+    if (isFutureError) setError('contactlessTask.time', { message: isFutureError.message });
+  }, [isFutureError, setError]);
 
   return (
     <div style={{ height: '100%', display: display ? 'block' : 'none' }}>
