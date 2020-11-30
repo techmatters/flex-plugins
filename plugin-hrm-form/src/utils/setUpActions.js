63fe6199e25afeef99144d1ec5f308e9de55c27f
gpaoloni <paolonigianfranco@gmail.com>
2020-11-30 10:34:50 -0300

    Fixed missing param in function call

--- plugin-hrm-form/src/utils/setUpActions.js
+++ plugin-hrm-form/src/utils/setUpActions.js
@@ -121,7 +121,7 @@ export const afterAcceptTask = setupObject => async payload => {
     };
 
     // Ignore event payload as we already have everything we want in afterAcceptTask arguments. Start at 0ms as many users are able to send the message right away
-    manager.chatClient.once('channelJoined', () => trySendWelcomeMessage(0));
+    manager.chatClient.once('channelJoined', () => trySendWelcomeMessage(0, 0));
   }
 };
 
