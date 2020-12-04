e2cb5389290f35aa40404ae93e2e264b5f0b4a03
gpaoloni <paolonigianfranco@gmail.com>
2020-12-04 14:59:38 -0300

    CallerInformationTab

--- plugin-hrm-form/src/components/tabbedForms/CallerInformationTab.tsx
+++ plugin-hrm-form/src/components/tabbedForms/CallerInformationTab.tsx
@@ -1,16 +1,55 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
+import { ITask, withTaskContext } from '@twilio/flex-ui';
+import { connect, ConnectedProps } from 'react-redux';
 
-import { CallerForm, CallerFormProps } from '../common/forms';
+import { RootState } from '../../states';
+import { updateForm } from '../../states/contacts/actions';
+import CallerTabDefinition from '../../formDefinitions/tabbedForms/CallerInformationTab.json';
+import { ColumnarBlock, Container, TwoColumnLayout } from '../../styles/HrmStyles';
+import { createFormFromDefinition, disperseInputs, splitInHalf } from '../common/forms/formGenerators';
+import type { FormDefinition } from '../common/forms/types';
 
-const CallerInformationTab: React.FC<CallerFormProps> = ({ callerInformation, defaultEventHandlers }) => {
-  // append callerInformation to the parents array (as caller form is used inside of the wider "contact form")
-  const callerEventHandlers: CallerFormProps['defaultEventHandlers'] = (parents, name) =>
-    defaultEventHandlers(['callerInformation', ...parents], name);
+type OwnProps = { task: ITask; display: boolean };
 
-  return <CallerForm callerInformation={callerInformation} defaultEventHandlers={callerEventHandlers} />;
+// eslint-disable-next-line no-use-before-define
+type Props = OwnProps & ConnectedProps<typeof connector>;
+
+const CallerInformationTab: React.FC<Props> = ({ dispatch, task, display }) => {
+  const { getValues } = useFormContext();
+
+  const [l, r] = React.useMemo(() => {
+    const updateCallback = () => {
+      const { callerInformation } = getValues();
+      dispatch(updateForm(task.taskSid, 'callerInformation', callerInformation));
+    };
+
+    // TODO: fix this typecasting
+    const callerFormDefinition = createFormFromDefinition(CallerTabDefinition as FormDefinition)(['callerInformation'])(
+      updateCallback,
+    );
+
+    return splitInHalf(disperseInputs(7)(callerFormDefinition));
+  }, [dispatch, getValues, task.taskSid]);
+
+  return (
+    <div style={{ display: display ? 'block' : 'none' }}>
+      <Container>
+        <TwoColumnLayout>
+          <ColumnarBlock>{l}</ColumnarBlock>
+          <ColumnarBlock>{r}</ColumnarBlock>
+        </TwoColumnLayout>
+      </Container>
+    </div>
+  );
 };
 
 CallerInformationTab.displayName = 'CallerInformationTab';
 
-export default CallerInformationTab;
+const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({});
+
+const connector = connect(mapStateToProps);
+const connected = connector(CallerInformationTab);
+
+export default withTaskContext<Props, typeof connected>(connected);
