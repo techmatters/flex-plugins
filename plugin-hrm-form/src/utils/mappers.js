c23f41c6b92c845bea782faf6f7aa32532457554
gpaoloni <paolonigianfranco@gmail.com>
2020-12-22 14:42:35 -0300

    Change age to be a dropdown; integrate chatbot correctly (#286)

--- plugin-hrm-form/src/utils/mappers.js
+++ plugin-hrm-form/src/utils/mappers.js
@@ -51,37 +51,17 @@ export const mapChannelForInsights = channel => {
   }
 };
 
+/**
+ * @param {string} age
+ * @returns {string}
+ */
 export const mapAge = age => {
-  let ageStr;
-  if (age === undefined) {
-    ageStr = 'Unknown';
-  } else {
-    const ageNum = parseInt(age, 10);
-    if (isNaN(ageNum)) {
-      ageStr = 'Unknown';
-    } else if (ageNum >= 0 && ageNum <= 3) {
-      ageStr = '0-03';
-    } else if (ageNum >= 4 && ageNum <= 6) {
-      ageStr = '04-06';
-    } else if (ageNum >= 7 && ageNum <= 9) {
-      ageStr = '07-09';
-    } else if (ageNum >= 10 && ageNum <= 12) {
-      ageStr = '10-12';
-    } else if (ageNum >= 13 && ageNum <= 15) {
-      ageStr = '13-15';
-    } else if (ageNum >= 16 && ageNum <= 17) {
-      ageStr = '16-17';
-    } else if (ageNum >= 18 && ageNum <= 25) {
-      ageStr = '18-25';
-    } else if (ageNum > 25 && ageNum <= 100) {
-      ageStr = '>25';
-    } else {
-      console.error('Invalid age value: %s', ageStr);
-      ageStr = 'Unknown';
-    }
-  }
+  const ageInt = parseInt(age, 10);
 
-  return ageStr;
+  if (ageInt >= 0 && ageInt <= 25) return age;
+  if (ageInt > 25) return '>25';
+
+  return 'Unknown';
 };
 
 export const mapGender = gender => {
