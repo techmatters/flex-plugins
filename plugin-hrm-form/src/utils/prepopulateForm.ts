430c9051c1d33e3f9d6b8aa5a0656f44e39538e4
gpaoloni <paolonigianfranco@gmail.com>
2021-02-02 13:35:41 -0300

    utils/prepopulateForm converted to TS

--- plugin-hrm-form/src/utils/prepopulateForm.ts
+++ plugin-hrm-form/src/utils/prepopulateForm.ts
@@ -1,16 +1,16 @@
-import { Manager } from '@twilio/flex-ui';
+import { ITask, Manager } from '@twilio/flex-ui';
 
 import { mapAge, mapGender } from './mappers';
 import * as RoutingActions from '../states/routing/actions';
 import { prepopulateFormCaller, prepopulateFormChild } from '../states/contacts/actions';
 import ChildInformationTab from '../formDefinitions/tabbedForms/ChildInformationTab.json';
 import CallerInformationTab from '../formDefinitions/tabbedForms/CallerInformationTab.json';
+import type { FormDefinition } from '../components/common/forms/types';
 
 /**
  * Given a form definition, grabs the "gender" named input and return the options values, or empty array.
- * @param {import('../components/common/forms/types').FormDefinition} definition
  */
-const getGenderOptions = definition => {
+const getGenderOptions = (definition: FormDefinition) => {
   const genderInputDef = definition.find(e => e.name === 'gender');
 
   if (genderInputDef.type === 'select') return genderInputDef.options.map(e => e.value) || [];
@@ -19,7 +19,7 @@ const getGenderOptions = definition => {
   return [];
 };
 
-export const prepopulateForm = task => {
+export const prepopulateForm = (task: ITask) => {
   // If this task came from the pre-survey
   if (task.attributes.memory) {
     const { answers } = task.attributes.memory.twilio.collected_data.collect_survey;
@@ -31,14 +31,14 @@ export const prepopulateForm = task => {
 
     if (answers.about_self.answer === 'Yes') {
       // future work: const ChildInformationTab = Manager.getInstance().store.getState() ... to grab the form definition when it's part of the global state (instead of bundled with the code)
-      const genderOptions = getGenderOptions(ChildInformationTab);
+      const genderOptions = getGenderOptions(ChildInformationTab as FormDefinition);
       const gender =
         !answers.gender || answers.gender.error ? 'Unknown' : mapGender(genderOptions)(answers.gender.answer);
 
       Manager.getInstance().store.dispatch(prepopulateFormChild(gender, age, task.taskSid));
     } else if (answers.about_self.answer === 'No') {
       // future work: const CallerInformationTab = Manager.getInstance().store.getState() ... to grab the form definition when it's part of the global state (instead of bundled with the code)
-      const genderOptions = getGenderOptions(CallerInformationTab);
+      const genderOptions = getGenderOptions(CallerInformationTab as FormDefinition);
       const gender =
         !answers.gender || answers.gender.error ? 'Unknown' : mapGender(genderOptions)(answers.gender.answer);
 
