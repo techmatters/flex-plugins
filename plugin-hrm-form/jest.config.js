7b70ad0de9b0cc0fa2416649b25ef91bee9485ff
gpaoloni <paolonigianfranco@gmail.com>
2021-02-04 16:38:51 -0300

    Tests running but most suits failing

--- plugin-hrm-form/jest.config.js
+++ plugin-hrm-form/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    transformIgnorePatterns: [
-        '/node_modules/(?!wavesurfer.js)',
-    ],
-}
+  transformIgnorePatterns: ['/node_modules/(?!wavesurfer.js)'],
+  transform: {
+    '(js|jsx)': 'babel-jest',
+    '.(ts|tsx)': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFiles: ['./src/setupTests.js'],
+};
