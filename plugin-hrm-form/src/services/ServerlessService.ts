346e631055e07a65af3c81969a87caf8935589a8
gpaoloni <paolonigianfranco@gmail.com>
2021-02-09 17:06:53 -0300

    Added service to fetch definitions

--- plugin-hrm-form/src/services/ServerlessService.ts
+++ plugin-hrm-form/src/services/ServerlessService.ts
@@ -3,6 +3,7 @@ import { ITask, Notifications } from '@twilio/flex-ui';
 
 import fetchProtectedApi from './fetchProtectedApi';
 import { getConfig } from '../HrmFormPlugin';
+import formsVersions from '../formDefinitions';
 
 type PopulateCounselorsReturn = { sid: string; fullName: string }[];
 
@@ -100,3 +101,9 @@ export const sendSystemMessage = async (body: { taskSid: ITask['taskSid']; messa
 
   return response;
 };
+
+/**
+ * Function that mimics the fetching of a version definition for all the forms used within the app.
+ * Later on this will be fetched in async way.
+ */
+export const getFormsVersion = async (version: string) => formsVersions[version];
