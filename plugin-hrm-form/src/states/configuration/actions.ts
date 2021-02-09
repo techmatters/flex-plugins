17156439a7afb924bf3a4334d3b0fef89c178546
gpaoloni <paolonigianfranco@gmail.com>
2021-02-09 17:05:13 -0300

    Added support to hold definitions in Redux

--- plugin-hrm-form/src/states/configuration/actions.ts
+++ plugin-hrm-form/src/states/configuration/actions.ts
@@ -1,4 +1,5 @@
 import * as t from './types';
+import type { FormsVersion } from '../../components/common/forms/types';
 
 // Action creators
 export const changeLanguage = (language: string): t.ConfigurationActionType => ({ type: t.CHANGE_LANGUAGE, language });
@@ -11,4 +12,15 @@ export const populateCounselorsState = (counselorsList: t.CounselorsList): t.Con
 export const chatCapacityUpdated = (capacity: number): t.ConfigurationActionType => ({
   type: t.CHAT_CAPACITY_UPDATED,
   capacity,
+});
+
+export const populateCurrentDefinitionVersion = (definitions: FormsVersion) => ({
+  type: t.POPULATE_CURRENT_DEFINITION_VERSION,
+  definitions,
+});
+
+export const updateFormsVersion = (version: string, definitions: FormsVersion) => ({
+  type: t.UPDATE_FORMS_VERSION,
+  version,
+  definitions,
 });
--- plugin-hrm-form/src/states/configuration/types.ts
+++ plugin-hrm-form/src/states/configuration/types.ts
@@ -1,7 +1,11 @@
+import type { FormsVersion } from '../../components/common/forms/types';
+
 // Action types
 export const CHANGE_LANGUAGE = 'CHANGE_LANGUAGE';
 export const POPULATE_COUNSELORS = 'POPULATE_COUNSELORS';
 export const CHAT_CAPACITY_UPDATED = 'CHAT_CAPACITY_UPDATED';
+export const POPULATE_CURRENT_DEFINITION_VERSION = 'POPULATE_CURRENT_DEFINITION_VERSION';
+export const UPDATE_FORMS_VERSION = 'UPDATE_FORMS_VERSION';
 
 export type CounselorsList = {
   sid: string;
@@ -23,4 +27,20 @@ type ChatCapacityUpdatedAction = {
   capacity: number;
 };
 
-export type ConfigurationActionType = ChangeLanguageAction | PopulateCounselorsAction | ChatCapacityUpdatedAction;
+type PopulateCurrentDefinitionVersion = {
+  type: typeof POPULATE_CURRENT_DEFINITION_VERSION;
+  definitions: FormsVersion;
+};
+
+type UpdateFormsVersion = {
+  type: typeof UPDATE_FORMS_VERSION;
+  version: string;
+  definitions: FormsVersion;
+};
+
+export type ConfigurationActionType =
+  | ChangeLanguageAction
+  | PopulateCounselorsAction
+  | ChatCapacityUpdatedAction
+  | PopulateCurrentDefinitionVersion
+  | UpdateFormsVersion;
