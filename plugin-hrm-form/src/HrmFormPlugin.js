fc819017736c785d1a3c6acd215d296dd01d5266
gpaoloni <paolonigianfranco@gmail.com>
2021-02-26 14:46:21 -0300

    definitionVersion pulled from service configuration

--- plugin-hrm-form/src/HrmFormPlugin.js
+++ plugin-hrm-form/src/HrmFormPlugin.js
@@ -38,8 +38,8 @@ export const getConfig = () => {
   const counselorName = manager.workerClient.attributes.full_name;
   const { configuredLanguage } = manager.serviceConfiguration.attributes;
   const featureFlags = manager.serviceConfiguration.attributes.feature_flags || {};
+  const { definitionVersion } = manager.serviceConfiguration.attributes;
   const { strings } = manager;
-  const definitionVersion = 'za-v1'; // will be moved to service configuration later on
 
   return {
     hrmBaseUrl,
