fc23ba8722db602cc3475096227c1694cd8353ae
Andrés Virga <andvirga@gmail.com>
2021-03-18 16:19:55 -0300

    Updated some offices

--- plugin-hrm-form/src/components/case/casePrint/mockedData.ts
+++ plugin-hrm-form/src/components/case/casePrint/mockedData.ts
@@ -1,8 +1,20 @@
 export const caseManagers = [
   {
+    office: 'National Office',
+    name: 'Bhavna Lutchman',
+    phone: '031 201 2059',
+    email: 'research@childlinesa.org.za',
+  },
+  {
     office: 'Gauteng',
     name: 'Bhavna Lutchman',
     phone: '031 201 2059',
     email: 'research@childlinesa.org.za',
+  },
+  {
+    office: 'Western Cape',
+    name: 'Nicole Paulsen',
+    phone: '21 762 8198',
+    email: 'info@childlinewc.org',
   },
 ];
