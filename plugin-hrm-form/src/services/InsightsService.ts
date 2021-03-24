7742e07c80cce0c610e610aefc929e0bfe38a5a3
gpaoloni <paolonigianfranco@gmail.com>
2021-03-24 15:05:53 -0300

    applyCustomUpdate bundles task attributes + helpline added to conversation_attribute_8 for ZA

--- plugin-hrm-form/src/services/InsightsService.ts
+++ plugin-hrm-form/src/services/InsightsService.ts
@@ -249,10 +249,10 @@ export const processHelplineConfig = (
 };
 
 const applyCustomUpdate = (customUpdate: OneToManyConfigSpec): InsightsUpdateFunction => {
-  return (attributes, contactForm, caseForm) => {
+  return (taskAttributes, contactForm, caseForm) => {
     if (isNonDataCallType(contactForm.callType)) return {};
 
-    const dataSource = { contactForm, caseForm };
+    const dataSource = { contactForm, caseForm, taskAttributes };
     // concatenate the values, taken from dataSource using paths (e.g. 'contactForm.childInformation.province')
     const value = customUpdate.paths.map(path => get(dataSource, path, '')).join(delimiter);
 
