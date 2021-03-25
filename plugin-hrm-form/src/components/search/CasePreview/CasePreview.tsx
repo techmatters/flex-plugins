cf782bae3c2d7aa4d3ee39ea018e92960f0f0a66
Andrés Virga <andvirga@gmail.com>
2021-03-25 16:02:11 -0300

    Added Counselor to Case Cards

--- plugin-hrm-form/src/components/search/CasePreview/CasePreview.tsx
+++ plugin-hrm-form/src/components/search/CasePreview/CasePreview.tsx
@@ -1,21 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Template } from '@twilio/flex-ui';
 
 import { Case } from '../../../types/types';
 import CaseHeader from './CaseHeader';
 import CaseSummary from './CaseSummary';
 import CaseTags from '../../case/CaseTags';
 import { Flex } from '../../../styles/HrmStyles';
-import { CaseWrapper } from '../../../styles/search';
+import { CaseWrapper, CaseFooter, CaseFooterText, CounselorText, SummaryText } from '../../../styles/search';
 
 type OwnProps = {
   currentCase: Case;
   onClickViewCase: () => void;
+  counselorsHash: { [sid: string]: string };
 };
 
 type Props = OwnProps;
 
-const CasePreview: React.FC<Props> = ({ currentCase, onClickViewCase }) => {
+const CasePreview: React.FC<Props> = ({ currentCase, onClickViewCase, counselorsHash }) => {
   const { id, createdAt, updatedAt, connectedContacts, status, info } = currentCase;
 
   const orphanedCase = !connectedContacts || connectedContacts.length === 0;
@@ -23,6 +25,7 @@ const CasePreview: React.FC<Props> = ({ currentCase, onClickViewCase }) => {
   const { name } = ((firstContact || {}).rawJson || {}).childInformation || {};
   const { categories, callSummary } = ((firstContact || {}).rawJson || {}).caseInformation || {};
   const summary = info?.summary || callSummary;
+  const counselor = counselorsHash[firstContact?.twilioWorkerId];
 
   return (
     <Flex>
@@ -37,7 +40,15 @@ const CasePreview: React.FC<Props> = ({ currentCase, onClickViewCase }) => {
           status={status}
         />
         <CaseSummary summary={summary} />
-        <CaseTags definitionVersion={info.definitionVersion} categories={categories} />
+        <CaseFooter>
+          <CaseTags definitionVersion={info.definitionVersion} categories={categories} />
+          <CaseFooterText>
+            <CounselorText style={{ marginRight: 5 }}>
+              <Template code="CallTypeAndCounselor-Label" />
+            </CounselorText>
+            {counselor && <SummaryText>{counselor}</SummaryText>}
+          </CaseFooterText>
+        </CaseFooter>
       </CaseWrapper>
     </Flex>
   );
