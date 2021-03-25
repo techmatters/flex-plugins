aa940981a78f3460f13376b421fef4e81569e230
Murilo Machado <murilovmachado@gmail.com>
2021-03-25 20:26:07 +0000

    Make all rules customized

--- plugin-hrm-form/src/permissions/index.ts
+++ plugin-hrm-form/src/permissions/index.ts
@@ -1,33 +1,32 @@
 import { getConfig } from '../HrmFormPlugin';
 import type * as t from '../types/types';
-import { canEditCaseSummary as canEditCaseSummaryV1 } from './v1';
-import { canEditCaseSummary as canEditCaseSummaryZaV1 } from './za-v1';
+import * as v1Rules from './v1';
+import * as zaV1Rules from './za-v1';
 
 export const PermissionActions = {
   OTHER: 'other',
   CLOSE_CASE: 'closeCase',
   REOPEN_CASE: 'reopenCase',
   ADD_NOTE: 'addNote',
-  EDIT_NOTE: 'editNote',
   ADD_REFERRAL: 'addReferral',
-  EDIT_REFERRAL: 'editReferral',
   ADD_HOUSEHOLD: 'addHousehold',
-  EDIT_HOUSEHOLD: 'editHousehold',
   ADD_PERPETRATOR: 'addPerpetrator',
-  EDIT_PERPETRATOR: 'editPerpetrator',
   ADD_INCIDENT: 'addIncident',
-  EDIT_INCIDENT: 'editIncident',
   EDIT_CASE_SUMMARY: 'editCaseSummary',
 };
 
 type PermissionActionsKeys = keyof typeof PermissionActions;
 export type PermissionActionType = typeof PermissionActions[PermissionActionsKeys];
 type Version = 'v1' | 'za-v1';
+type Rule = (isSupervisor: boolean, isCreator: boolean, isCaseOpen: boolean) => boolean;
+type Rules = {
+  canEditCaseSummary: Rule;
+  canEditGenericField: Rule;
+};
 
-type canEditCaseSummaryType = (isSpervisor: boolean, isCreator: boolean, isCaseOpen: boolean) => boolean;
-const canEditCaseSummaryMap: { [version in Version]: canEditCaseSummaryType } = {
-  v1: canEditCaseSummaryV1,
-  'za-v1': canEditCaseSummaryZaV1,
+const rulesMap: { [version in Version]: Rules } = {
+  v1: v1Rules,
+  'za-v1': zaV1Rules,
 };
 
 export const getPermissionsForCase = (caseObj: t.Case) => {
@@ -36,18 +35,16 @@ export const getPermissionsForCase = (caseObj: t.Case) => {
   const { twilioWorkerId } = caseObj;
   const isCreator = workerSid === twilioWorkerId;
   const isCaseOpen = caseObj.status === 'open';
-
-  const canEditFields = isSupervisor || (isCreator && isCaseOpen);
-  const canEditCaseSummary = canEditCaseSummaryMap[version];
+  const rules = rulesMap[version];
 
   const can = (action: PermissionActionType): boolean => {
     switch (action) {
       case PermissionActions.OTHER:
         return true;
       case PermissionActions.EDIT_CASE_SUMMARY:
-        return canEditCaseSummary(isSupervisor, isCreator, isCaseOpen);
+        return rules.canEditCaseSummary(isSupervisor, isCreator, isCaseOpen);
       default:
-        return canEditFields;
+        return rules.canEditGenericField(isSupervisor, isCreator, isCaseOpen);
     }
   };
 
