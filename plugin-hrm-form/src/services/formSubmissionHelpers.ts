ac61ba2a7aedbe98f51cc1b55c941b8233b3df3e
gpaoloni <paolonigianfranco@gmail.com>
2021-07-07 16:38:37 -0300

    Added issuer helpline as quickfix for the blank helpline bug in Insights

--- plugin-hrm-form/src/services/formSubmissionHelpers.ts
+++ plugin-hrm-form/src/services/formSubmissionHelpers.ts
@@ -43,7 +43,7 @@ export const submitContactForm = async (task: CustomITask, contactForm: Contact,
 
   if (isOfflineContactTask(task)) {
     const targetSid = contactForm.contactlessTask.createdOnBehalfOf as string;
-    const initialAttributes = { channelType: 'default', isContactlessTask: true };
+    const initialAttributes = { helpline, channelType: 'default', isContactlessTask: true };
     const finalAttributes = buildInsightsData(initialAttributes, contactForm, caseForm);
     const inBehalfTask = await assignOfflineContact(targetSid, finalAttributes);
     return saveToHrm(task, contactForm, workerSid, helpline, inBehalfTask.sid);
