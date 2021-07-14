37127a1cf0b98e41766f93819ae9afe56d330dfd
Jennifer Xu <jennxu23@stanford.edu>
2021-07-14 12:54:57 -0400

    added workspace id

--- scripts/insights.sh
+++ scripts/insights.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while read p; do
-    node downloadInsights.js --username "$1" --password "$2" --workspaceID fkg14xmswsy78us3gotb67cucw2e8s58 --objectID "$p"
+    node downloadInsights.js --username "$1" --password "$2" --workspaceID "$3" --objectID "$p"
 done
