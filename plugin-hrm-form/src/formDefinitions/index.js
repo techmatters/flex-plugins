3f0c9c0220f744059b83158c247f09a2ce5ec87b
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2021-07-30 18:09:49 +0200

    Et staging customisations

--- plugin-hrm-form/src/formDefinitions/index.js
+++ plugin-hrm-form/src/formDefinitions/index.js
@@ -1,7 +1,9 @@
 import v1 from './v1';
 import zaV1 from './za-v1';
+import etV1 from './et-v1';
 
 export default {
   v1, // Zambia V1
   'za-v1': zaV1, // South Africa v1
+  'et-v1': etV1, // Ethiopia v1
 };
