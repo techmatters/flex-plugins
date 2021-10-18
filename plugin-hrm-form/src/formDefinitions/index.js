0366d29eab91c4afb292ed32d42f7dd6e5b79c93
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2021-10-18 22:51:25 +0200

    Update index.js
    Add IN customizations

--- plugin-hrm-form/src/formDefinitions/index.js
+++ plugin-hrm-form/src/formDefinitions/index.js
@@ -3,6 +3,7 @@ import zaV1 from './za-v1';
 import etV1 from './et-v1';
 import mwV1 from './mw-v1';
 import brV1 from './br-v1';
+import inV1 from './in-v1';
 
 export default {
   v1, // Zambia V1
@@ -10,4 +11,5 @@ export default {
   'et-v1': etV1, // Ethiopia v1
   'mw-v1': mwV1, // Malawi v1
   'br-v1': brV1, // Safernet Brasil v1
+  'in-v1': inV1, // Aarambh Trustline v1
 };
