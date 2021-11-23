f8e30ea9200e3df70cf4c59c2d924c4d9207d13a
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2021-11-23 12:43:52 -0300

    Added csam services

---
+++ plugin-hrm-form/src/services/CSAMReportService.ts
@@ -0,0 +1,23 @@
+import fetchHrmApi from './fetchHrmApi';
+// import { CSAMReportEntry } from '../types/types';
+
+type CreateCSAMReportParams = {
+  csamReportId: string;
+  twilioWorkerId: string;
+  contactId?: number;
+};
+
+export const createCSAMReport = async ({ csamReportId, twilioWorkerId, contactId }: CreateCSAMReportParams) => {
+  const body = {
+    twilioWorkerId,
+    csamReportId,
+    contactId,
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+  };
+
+  return await fetchHrmApi('/csamReports', options);
+};
