785bddd2233dc5df7b0e056e998c3eec1e1a4c7b
Stephen Hand <steveh@techmatters.org>
2021-12-08 13:22:13 +0000

    Add standalone jest config

---
+++ plugin-hrm-form/jest-standalone.config.js
@@ -0,0 +1,10 @@
+// For rapidly running non flex UI dependent tests locally
+const { defaults } = require('jest-config');
+
+module.exports = config => {
+  config = config || { ...defaults, rootDir: '.' };
+  config.transformIgnorePatterns = ['/node_modules/(?!wavesurfer.js)'];
+  config.setupFiles = ['./src/___tests__/setup.js']
+
+  return config;
+};
