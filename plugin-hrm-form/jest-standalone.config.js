7209615c917749c3021b03da0a8278bfa3a10a21
Stephen Hand <steveh@techmatters.org>
2021-12-09 08:49:07 +0000

    Use Jest directly for CI (#556)
    Changes to run the tests directly in Jest (i.e. faster & more options)

---
+++ plugin-hrm-form/jest-standalone.config.js
@@ -0,0 +1,14 @@
+// For rapidly running non flex UI dependent tests locally
+const { defaults } = require('jest-config');
+
+module.exports = config => {
+  config = config || {
+    ...defaults,
+    rootDir: '.',
+    setupFiles: ['./src/setupTests.js'],
+    testEnvironment: 'jsdom',
+    testTimeout: 2 * 60 * 1000, // 2 minutes in ms
+  };
+
+  return config;
+};
