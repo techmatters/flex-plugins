38793cc1743ce80c1b08122c971be91a3ff111a9
Stephen Hand <steveh@techmatters.org>
2022-02-07 10:59:57 +0000

    Renamed provisioner resource for HRM static key in TF

--- twilio-iac/terraform-modules/hrmServiceIntegration/default/main.tf
+++ twilio-iac/terraform-modules/hrmServiceIntegration/default/main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "sync_api_key" {
+resource "null_resource" "hrm_static_api_key" {
   provisioner "local-exec" {
     working_dir = "${path.module}/../../../../scripts"
     command = "npm run twilioResources -- new-key-with-ssm-secret hrm-static-key ${var.short_environment}_TWILIO_${var.short_helpline}_HRM_STATIC_KEY ${var.helpline} ${var.environment}"
