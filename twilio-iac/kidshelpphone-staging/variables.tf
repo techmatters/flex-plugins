fe2d9f5098316cb13a7402aed59b37c568c203a8
Stephen Hand <steveh@techmatters.org>
2022-02-07 15:33:25 +0000

    Kids Help Phone (Canada) staging initial TF setup and deploy workflow

---
+++ twilio-iac/kidshelpphone-staging/variables.tf
@@ -0,0 +1,21 @@
+variable "account_sid" {}
+variable "serverless_url" {}
+variable "datadog_app_id" {}
+variable "datadog_access_token" {}
+
+variable "helpline" {
+  default = "Kids Help Phone"
+}
+variable "short_helpline" {
+  default = "CA"
+}
+variable "operating_info_key" {
+  default = "ca"
+}
+variable "environment" {
+  default = "Staging"
+}
+variable "short_environment" {
+  default = "STG"
+}
+
