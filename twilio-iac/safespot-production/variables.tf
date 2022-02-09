90f4691b9752c4101468d02b1adfea5c8463a932
Stephen Hand <steveh@techmatters.org>
2022-02-09 16:01:09 +0000

    Added provisioners to safespot prod TF config

--- twilio-iac/safespot-production/variables.tf
+++ twilio-iac/safespot-production/variables.tf
@@ -3,6 +3,12 @@ variable "serverless_url" {}
 variable "datadog_app_id" {}
 variable "datadog_access_token" {}
 
+variable "local_os" {
+  description = "The OS running the terraform script. The only value that currently changes behaviour from default is 'Windows'"
+  type        = string
+  default = "Linux"
+}
+
 variable "helpline" {
   default = "SafeSpot"
 }
@@ -19,3 +25,33 @@ variable "short_environment" {
   default = "PROD"
 }
 
+variable "definition_version" {
+  description = "Key that determines which set of form definitions this helpline will use"
+  type        = string
+  default = "jm-v1"
+}
+
+variable multi_office {
+  default = true
+  type = bool
+  description = "Sets the multipleOfficeSupport flag in Flex Service Configuration"
+}
+
+variable "feature_flags" {
+  description = "A map of feature flags that need to be set for this helpline's flex plugin"
+  type = map(bool)
+  default = {
+    "enable_fullstory_monitoring" = false
+    "enable_upload_documents" = true
+    "enable_previous_contacts" = true
+    "enable_case_management" = true
+    "enable_offline_contact" = true
+    "enable_transfers" = true
+    "enable_manual_pulling" = true
+    "enable_csam_report" = false
+    "enable_canned_responses" = true
+    "enable_dual_write" = false
+    "enable_save_insights" = true
+    "enable_post_survey" = false
+  }
+}
