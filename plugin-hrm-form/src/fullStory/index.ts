68303c684aca6a5babd4b855c7913df24da86554
mmythily <mythily@techmatters.org>
2022-02-24 15:04:57 -0500

    Implemented changes recommended based on PR comments

--- plugin-hrm-form/src/fullStory/index.ts
+++ plugin-hrm-form/src/fullStory/index.ts
@@ -28,20 +28,38 @@ export function recordEvent(eventType: string, payload: any) {
 }
 
 /**
+ * Recursive function that checks the error object(branch) for type and message properties(leaf) and returns an array of objects based on the nested nature of the branch or leaf.
+ * @param tabName - the tab or section at which the fields are present
+ * @param fieldName - the name of the field (i.e., - firstname, age)
+ * @param object - the error object
+ */
+function getFieldErrorObj(tabName: string, fieldName: string, object: any): any[] {
+  if (object.hasOwnProperty('type') && object.hasOwnProperty('message')) {
+    return [{ [fieldName]: { tab: tabName, ...object } }];
+  }
+  return Object.keys(object).flatMap(child => getFieldErrorObj(tabName, child, object[child]));
+}
+/**
+ * Transforms a form error object into single field errors - returns an array of all the field errors at error object for a form
+ * @param errorObj - the error object after clean up with serializableFormValidationError
+ */
+function transformErrorObj(errorObj: any) {
+  return Object.keys(errorObj).flatMap(tab => getFieldErrorObj(tab, tab, errorObj[tab]));
+}
+
+/**
  * Sends form validation errors to fullstory.com, under the event name 'Form Error'
  * Specvific form error information is sent as a JSON blob in a single property for now because I'm not sure how much fullstory likes dynamic payloads
- * In addition to 'Form Error', a custom 'Single Field Error' event is also sent which will separate the Form Error into specific field errors.
- * To achieve 'Single Field Error', the original error is cleaned up with serializableFormValidationError method and flattened into the errorArr which holds all the error objects in an array.
+ * In addition to 'Form Error', several 'Custom Error' events are also sent which are separated into specific field errors. The original error is cleaned up with serializableFormValidationError method and flattened into the errorArr
  * @param origin - A string identifying the form the error originated from
  * @param formError - The error object produced by the form validation (i.e. the first parameter of the error handler)
  */
 export function recordFormValidationError(origin: string, formError: FieldValues): void {
   let errorJson: string = '{}';
-  let errorObj: object = {};
+  let errorArr: any;
   try {
     errorJson = JSON.stringify(serializableFormValidationError(formError));
-
-    errorObj = serializableFormValidationError(formError);
+    errorArr = transformErrorObj(serializableFormValidationError(formError));
   } catch (serializeError) {
     console.warn(
       `Error serializing error data for form validation error from ${origin}, sending event with no data`,
@@ -55,16 +73,12 @@ export function recordFormValidationError(origin: string, formError: FieldValues
     errors_str: errorJson,
   });
 
-  const errorArr = Object.values(errorObj)
-    .flatMap(obj => Object.entries(obj))
-    .map(([key, value]) => ({ [key]: value }));
-
-  errorArr.map(error => {
+  errorArr.forEach(error => {
     return recordEvent('Custom Error', {
       // eslint-disable-next-line camelcase
       form_str: origin,
       // eslint-disable-next-line camelcase
-      field_str: `${Object.keys(error)}`,
+      field_str: Object.keys(error)[0],
       // eslint-disable-next-line camelcase
       error_str: JSON.stringify(error),
     });
