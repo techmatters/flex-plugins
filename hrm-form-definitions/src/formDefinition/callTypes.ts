da735da14c2b840a961d6e7aede26a70a8dd41b9
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-02-28 12:59:48 -0300

    Expanded call types to be any string

--- hrm-form-definitions/src/formDefinition/callTypes.ts
+++ hrm-form-definitions/src/formDefinition/callTypes.ts
@@ -1,16 +1,9 @@
+// Data callTypes. Preserving name to avoid big refactor
 export const callTypes = {
   child: 'Child calling about self',
   caller: 'Someone calling about a child',
-  silent: 'Silent',
-  blank: 'Blank',
-  joke: 'Joke',
-  hangup: 'Hang up',
-  wrongnumber: 'Wrong Number',
-  abusive: 'Abusive',
-  adult: 'Adult Contact',
-  test: 'test',
 } as const;
 
-export type CallTypeKeys = keyof typeof callTypes;
-export type CallTypes = typeof callTypes[keyof typeof callTypes];
 export type DataCallTypes = typeof callTypes['child' | 'caller'];
+export type CallTypeKeys = DataCallTypes | string; // This results in "strings" as it's a broader type. Leaving the DataCallTypes in intentionally to emphasize them.
+export type CallTypes = string;
