6dc036450c6b0f2784c770173a9be8c4e9f893e8
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2022-03-04 19:48:02 +0200

    Update ca.ts

--- plugin-hrm-form/src/permissions/ca.ts
+++ plugin-hrm-form/src/permissions/ca.ts
@@ -3,6 +3,7 @@ export const canEditCaseSummary = (isSupervisor: boolean, isCreator: boolean, is
 
 export const canReopenCase = (isSupervisor: boolean, isCreator: boolean, isCaseOpen: boolean) => isSupervisor;
 
+export const canEditNote = (isSupervisor: boolean, isCaseOpen: boolean) => isSupervisor;
 /**
  * For now, all the other actions are the same, and can use the below permission.
  */
