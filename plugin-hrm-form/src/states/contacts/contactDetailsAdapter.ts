cbbd0539f54173b459d953cfa2b5595667cd66ce
Stephen Hand <steveh@techmatters.org>
2022-05-09 10:48:42 +0100

    contactDetailsAdapter unit tests

--- plugin-hrm-form/src/states/contacts/contactDetailsAdapter.ts
+++ plugin-hrm-form/src/states/contacts/contactDetailsAdapter.ts
@@ -9,13 +9,14 @@ import { SearchContact } from '../../types/types';
  * @param {string[]} accumulator
  * @param {[string, boolean]} currentValue
  */
-const subcatsReducer = (accumulator, [subcat, bool]) => (bool ? [...accumulator, subcat] : accumulator);
+const subcatsReducer = (accumulator, [subcat, bool]: [string, boolean]) =>
+  bool ? [...accumulator, subcat] : accumulator;
 
 /**
  * @param {{ [category: string]: string[] }} accumulator
  * @param {[string, { [subcategory: string]: boolean }]} currentValue
  */
-const catsReducer = (accumulator, [cat, subcats]) => {
+const catsReducer = (accumulator, [cat, subcats]: [string, Record<string, boolean>]) => {
   const subcatsList = Object.entries(subcats).reduce(subcatsReducer, []);
 
   if (!subcatsList.length) return accumulator;
@@ -27,7 +28,7 @@ const catsReducer = (accumulator, [cat, subcats]) => {
  * @param {{ [category: string]: { [subcategory: string]: boolean } }} categories categories object
  * @returns {{ [category: string]: string[] }} returns an object containing each truthy subcategory under the category name
  */
-export const retrieveCategories = categories => {
+export const retrieveCategories = (categories: Record<string, Record<string, boolean>>): Record<string, string[]> => {
   if (!categories) return {};
 
   return Object.entries(categories).reduce(catsReducer, {});
@@ -41,8 +42,7 @@ export const hrmServiceContactToSearchContact = (contact): SearchContact => {
   const { callType, caseInformation } = contact.rawJson;
   const categories = retrieveCategories(caseInformation.categories);
   const notes = caseInformation.callSummary;
-  const channelType = contact.channel;
-  const { conversationDuration, csamReports, createdBy, helpline } = contact;
+  const { conversationDuration, csamReports, createdBy, helpline, channel } = contact;
 
   return {
     contactId: contact.id,
@@ -55,7 +55,7 @@ export const hrmServiceContactToSearchContact = (contact): SearchContact => {
       categories,
       counselor: contact.twilioWorkerId,
       notes,
-      channel: channelType,
+      channel,
       conversationDuration,
       createdBy,
     },
