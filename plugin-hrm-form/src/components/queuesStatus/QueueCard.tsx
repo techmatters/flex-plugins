e12d1ed6f50c3050002226a7cdcd0dcbe7a2d133
acedeywin <stephenokpala@gmail.com>
2022-05-19 16:05:54 +0100

    CHI-1186: fix small UX design issues

--- plugin-hrm-form/src/components/queuesStatus/QueueCard.tsx
+++ plugin-hrm-form/src/components/queuesStatus/QueueCard.tsx
@@ -28,7 +28,7 @@ const getChannelUI = (
       className="channel-box-inner-value"
       data-testid="channel-box-inner-value"
     >
-      {value}
+      {value === 0 ? '-' : value}
     </ChannelBox>
     <ChannelLabel>{shortChannel}</ChannelLabel>
   </ChannelColumn>
@@ -127,7 +127,7 @@ const QueuesCard: React.FC<Props> = props => {
       <Box paddingLeft="10px" paddingTop="10px" data-testid={`Queue-Status${qName}`}>
         <HiddenText id={`name-${qName}`}>
           <Template code="QueueCard-Name" />
-        </HiddenText>
+        </HiddenText> 
         <HiddenText aria-labelledby={`name-${qName}`} />
         <QueueName>{qName}</QueueName>
         <Box marginTop="7px" marginBottom="14px">
