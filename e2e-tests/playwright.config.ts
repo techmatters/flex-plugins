60cb223dc3b15385490289e833098971f1f7bdf9
Stephen Hand <steveh@techmatters.org>
2022-06-17 14:29:54 +0100

    Fix merge conflict

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -6,12 +6,7 @@ const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./global-setup'),
   use: {
     storageState: 'temp/state.json',
-<<<<<<< HEAD
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
-    // headless: false,    // Browser can run headlessly by default
-=======
     baseURL: process.env.PLAYWRIGHT_BASEURL ?? 'http://localhost:3000',
->>>>>>> e3849bb442fb85573ec4b1e421c65992f4acd5e9
     permissions: ['microphone'],
   },
   timeout: 60000,
