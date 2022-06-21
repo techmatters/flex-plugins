fd845769a3b2199e1a1120a91dc95a3536b59a18
Murilo Machado <murilovmachado@gmail.com>
2022-06-21 19:35:10 +0100

    Update UI Version action

---
+++ .github/actions/update-ui-version/dist/index.js
@@ -0,0 +1,103 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 191:
+/***/ ((__unused_webpack_module, __webpack_exports__, __nccwpck_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__nccwpck_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ../../../../../../.nvm/versions/node/v14.18.2/lib/node_modules/@vercel/ncc/dist/ncc/@@notfound.js?@actions/core
+var core = __nccwpck_require__(610);
+;// CONCATENATED MODULE: ../../../plugin-hrm-form/package-lock.json
+const package_lock_namespaceObject = JSON.parse('{"HO":{"bq1":{"version":"1.30.2","resolved":"https://registry.npmjs.org/@twilio/flex-ui/-/flex-ui-1.30.2.tgz","integrity":"sha512-j8UIV+BDiPITs3GrLpo4BpFREviLuvB07M9ZPeD5e0M5HcSILR65edXcPTgFkv7kfO1UZiN6c0+i+1WaXV+new==","dev":true,"requires":{"@gooddata/gooddata-js":"13.2.0","@material-ui/core":"3.9.4","@material-ui/lab":"3.0.0-alpha.30","@twilio/flex-insights-identity-client-js":"2.0.6","@twilio/flex-insights-player":"2.3.1","@twilio/flex-sdk":"^0.9.0","@twilio/flex-ui-core":"0.56.2","@twilio/player":"1.0.1","@types/deep-diff":"^1.0.0","@types/react-select":"2.0.19","async-sema":"^3.0.0","axios":"^0.21.1","core-js":"^3.0.1","deep-diff":"^1.0.2","emotion":"9.2.6","emotion-theming":"9.2.6","eventemitter3":"4.0.0","history":"4.7.2","hoist-non-react-statics":"3.3.1","jwt-decode":"^3.1.2","lodash.debounce":"4.0.8","lodash.isequal":"4.5.0","lodash.merge":"4.6.2","lodash.snakecase":"4.1.1","lodash.throttle":"4.1.1","loglevel":"1.6.7","query-string":"6.2.0","react":"^16.5.2","react-dom":"^16.5.2","react-emotion":"9.2.6","react-flip-move":"3.0.3","react-jss":"8.6.1","react-redux":"~5.1.0","react-router":"4.3.1","react-router-dom":"4.3.1","react-router-redux":"5.0.0-alpha.9","react-select":"2.4.3","redux":"3.7.2","redux-logger":"3.0.6","redux-promise-middleware":"4.2.1","reselect":"4.0.0","semver":"~5.7.0","twilio-chat":"~3.4.0","twilio-client":"1.13.1","twilio-sync":"^0.12.3","uuid":"^3.3.2"},"dependencies":{"@material-ui/core":{"version":"3.9.4","resolved":"https://registry.npmjs.org/@material-ui/core/-/core-3.9.4.tgz","integrity":"sha512-r8QFLSexcYZbnqy/Hn4v8xzmAJV41yaodUVjmbGLi1iGDLG3+W941hEtEiBmxTRRqv2BdK3r4ijILcqKmDv/Sw==","dev":true,"requires":{"@babel/runtime":"^7.2.0","@material-ui/system":"^3.0.0-alpha.0","@material-ui/utils":"^3.0.0-alpha.2","@types/jss":"^9.5.6","@types/react-transition-group":"^2.0.8","brcast":"^3.0.1","classnames":"^2.2.5","csstype":"^2.5.2","debounce":"^1.1.0","deepmerge":"^3.0.0","dom-helpers":"^3.2.1","hoist-non-react-statics":"^3.2.1","is-plain-object":"^2.0.4","jss":"^9.8.7","jss-camel-case":"^6.0.0","jss-default-unit":"^8.0.2","jss-global":"^3.0.0","jss-nested":"^6.0.1","jss-props-sort":"^6.0.0","jss-vendor-prefixer":"^7.0.0","normalize-scroll-left":"^0.1.2","popper.js":"^1.14.1","prop-types":"^15.6.0","react-event-listener":"^0.6.2","react-transition-group":"^2.2.1","recompose":"0.28.0 - 0.30.0","warning":"^4.0.1"}},"@material-ui/system":{"version":"3.0.0-alpha.2","resolved":"https://registry.npmjs.org/@material-ui/system/-/system-3.0.0-alpha.2.tgz","integrity":"sha512-odmxQ0peKpP7RQBQ8koly06YhsPzcoVib1vByVPBH4QhwqBXuYoqlCjt02846fYspAqkrWzjxnWUD311EBbxOA==","dev":true,"requires":{"@babel/runtime":"^7.2.0","deepmerge":"^3.0.0","prop-types":"^15.6.0","warning":"^4.0.1"}},"core-js":{"version":"3.21.1","resolved":"https://registry.npmjs.org/core-js/-/core-js-3.21.1.tgz","integrity":"sha512-FRq5b/VMrWlrmCzwRrpDYNxyHP9BcAZC+xHJaqTgIE5091ZV1NTmyh0sGOg5XqpnHvR0svdy0sv1gWA1zmhxig==","dev":true},"csstype":{"version":"2.6.20","resolved":"https://registry.npmjs.org/csstype/-/csstype-2.6.20.tgz","integrity":"sha512-/WwNkdXfckNgw6S5R125rrW8ez139lBHWouiBvX8dfMFtcn6V81REDqnH7+CRpRipfYlyU1CmOnOxrmGcFOjeA==","dev":true},"deepmerge":{"version":"3.3.0","resolved":"https://registry.npmjs.org/deepmerge/-/deepmerge-3.3.0.tgz","integrity":"sha512-GRQOafGHwMHpjPx9iCvTgpu9NojZ49q794EEL94JVEw6VaeA8XTUyBKvAkOOjBX9oJNiV6G3P+T+tihFjo2TqA==","dev":true},"eventemitter3":{"version":"4.0.0","resolved":"https://registry.npmjs.org/eventemitter3/-/eventemitter3-4.0.0.tgz","integrity":"sha512-qerSRB0p+UDEssxTtm6EDKcE7W4OaoisfIMl4CngyEhjpYglocpNg6UEqCvemdGhosAsg4sO2dXJOdyBifPGCg==","dev":true},"loglevel":{"version":"1.6.7","resolved":"https://registry.npmjs.org/loglevel/-/loglevel-1.6.7.tgz","integrity":"sha512-cY2eLFrQSAfVPhCgH1s7JI73tMbg9YC3v3+ZHVW67sBS7UxWzNEk/ZBbSfLykBWHp33dqqtOv82gjhKEi81T/A==","dev":true},"popper.js":{"version":"1.16.1","resolved":"https://registry.npmjs.org/popper.js/-/popper.js-1.16.1.tgz","integrity":"sha512-Wb4p1J4zyFTbM+u6WuO4XstYx4Ky9Cewe4DWrel7B0w6VVICvPwdOpotjzcf6eD8TsckVnIMNONQyPIUFOUbCQ==","dev":true},"react-emotion":{"version":"9.2.6","resolved":"https://registry.npmjs.org/react-emotion/-/react-emotion-9.2.6.tgz","integrity":"sha512-3GwLQ0Vib/X0G7KWY7N1F/7NmfoBLkkg3g9Jd8t6By5QUH0PyCSUVdDmfFBbgVKsAcpkjA0vcsMbiWeZusaRrA==","dev":true,"requires":{"babel-plugin-emotion":"^9.2.6","create-emotion-styled":"^9.2.6"}},"semver":{"version":"5.7.1","resolved":"https://registry.npmjs.org/semver/-/semver-5.7.1.tgz","integrity":"sha512-sauaDf/PZdVgrLTNYHRtpXa1iRiKcaebiKQ1BJdpQlWH2lCvexQdX55snPFyK7QzpudqbCI0qXFfOasHdyNDGQ==","dev":true},"uuid":{"version":"3.4.0","resolved":"https://registry.npmjs.org/uuid/-/uuid-3.4.0.tgz","integrity":"sha512-HjSDRw6gZE5JMggctHBcjVak08+KEVhSIiDzFnT9S9aegmp85S/bReBVTb4QTFaRNptJ9kuYaNhnbNEOkbKb/A==","dev":true}}}}}');
+;// CONCATENATED MODULE: ./index.js
+/**
+ * If changes are made to this file, it needs to be recompiled using @vercel/ncc (https://github.com/vercel/ncc).
+ * 1) Install vercel/ncc by running this command in your terminal. npm i -g @vercel/ncc
+ * 2) Compile your index.js file. ncc build index.js --license licenses.txt
+ * For details see https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github 
+ */ 
+
+
+
+
+const uiVersion = package_lock_namespaceObject.HO.bq1;
+if (uiVersion) {
+  console.log(`flex ui version: ${uiVersion}`);
+  (0,core.setOutput)(`flex ui version: ${uiVersion}`, true);
+} else {
+  console.log('Flex ui version not found');
+  (0,core.setFailed)('Flex ui version not found');
+}
+ 
+
+/***/ }),
+
+/***/ 610:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__nccwpck_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(191);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
---
+++ .github/actions/update-ui-version/index.js
@@ -0,0 +1,19 @@
+/**
+ * If changes are made to this file, it needs to be recompiled using @vercel/ncc (https://github.com/vercel/ncc).
+ * 1) Install vercel/ncc by running this command in your terminal. npm i -g @vercel/ncc
+ * 2) Compile your index.js file. ncc build index.js --license licenses.txt
+ * For details see https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github 
+ */ 
+
+import { setOutput, setFailed } from '@actions/core';
+import packageLock from '../../../plugin-hrm-form/package-lock.json';
+
+const uiVersion = packageLock.dependencies['@twilio/flex-ui'];
+if (uiVersion) {
+  console.log(`flex ui version: ${uiVersion}`);
+  setOutput(`flex ui version: ${uiVersion}`, true);
+} else {
+  console.log('Flex ui version not found');
+  setFailed('Flex ui version not found');
+}
+ 
