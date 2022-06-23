594924c4e431ab4192c8ec30846b950c722a0eeb
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-06-23 13:30:01 -0300

    Revert "CHI-1189: Category pill buttons have color everywhere they appear"

--- plugin-hrm-form/src/utils/categories.tsx
+++ plugin-hrm-form/src/utils/categories.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import HrmTheme from '../styles/HrmTheme';
 import { ContactTag, TagText, TagMiddleDot } from '../styles/search';
-import { getConfig, getDefinitionVersions } from '../HrmFormPlugin';
+import { getDefinitionVersions } from '../HrmFormPlugin';
 import { ContactRawJson } from '../types/types';
 
 const getCategoryColor = (definitionVersion: ContactRawJson['definitionVersion'], category: string) => {
-  const { helpline } = getConfig();
-
-  const categories = getDefinitionVersions().definitionVersions[definitionVersion].tabbedForms.IssueCategorizationTab(
-    helpline,
-  );
+  const categories = getDefinitionVersions().definitionVersions[definitionVersion].tabbedForms.IssueCategorizationTab;
 
   return categories[category] ? categories[category].color : HrmTheme.colors.defaultCategoryColor;
 };
