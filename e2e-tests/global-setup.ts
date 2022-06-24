d7f8609bce476d051ae75e58b0aa883d9327c51b
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-06-24 17:00:28 -0300

    Centralized env vars to use dotenv

--- e2e-tests/global-setup.ts
+++ e2e-tests/global-setup.ts
@@ -3,6 +3,20 @@ import { FullConfig } from '@playwright/test';
 import { differenceInMilliseconds } from 'date-fns';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { oktaSsoLoginViaApi, oktaSsoLoginViaGui } from './okta/sso-login';
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig();
+
+export const environmentVariables = {
+  PLAYWRIGHT_USER_USERNAME: process.env.PLAYWRIGHT_USER_USERNAME,
+  PLAYWRIGHT_USER_PASSWORD: process.env.PLAYWRIGHT_USER_PASSWORD,
+  PLAYWRIGHT_BASEURL: process.env.PLAYWRIGHT_BASEURL,
+  PLAYWRIGHT_BROWSER_TELEMETRY_LEVEL: process.env.PLAYWRIGHT_BROWSER_TELEMETRY_LEVEL,
+  PLAYWRIGHT_BROWSER_TELEMETRY_LOG_RESPONSE_BODY:
+    process.env.PLAYWRIGHT_BROWSER_TELEMETRY_LOG_RESPONSE_BODY,
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+  DEBUG: process.env.DEBUG,
+};
 
 async function globalSetup(config: FullConfig) {
   const start = new Date();
@@ -15,14 +29,14 @@ async function globalSetup(config: FullConfig) {
 
   await oktaSsoLoginViaApi(
     config.projects[0].use.baseURL!,
-    process.env.PLAYWRIGHT_USER_USERNAME!,
-    process.env.PLAYWRIGHT_USER_PASSWORD!,
-    process.env.TWILIO_ACCOUNT_SID!,
+    environmentVariables.PLAYWRIGHT_USER_USERNAME!,
+    environmentVariables.PLAYWRIGHT_USER_PASSWORD!,
+    environmentVariables.TWILIO_ACCOUNT_SID!,
   );
   /* await oktaSsoLoginViaGui(
     config,
-    process.env.PLAYWRIGHT_USER_USERNAME ?? 'NOT SET',
-    process.env.PLAYWRIGHT_USER_PASSWORD ?? 'NOT SET',
+    environmentVariables.PLAYWRIGHT_USER_USERNAME ?? 'NOT SET',
+    environmentVariables.PLAYWRIGHT_USER_PASSWORD ?? 'NOT SET',
   );
    */
   console.log(
--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,12 +1,13 @@
 // playwright.config.ts
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { PlaywrightTestConfig } from '@playwright/test';
+import { environmentVariables } from './global-setup';
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./global-setup'),
   use: {
     storageState: 'temp/state.json',
-    baseURL: process.env.PLAYWRIGHT_BASEURL ?? 'http://localhost:3000',
+    baseURL: environmentVariables.PLAYWRIGHT_BASEURL ?? 'http://localhost:3000',
     permissions: ['microphone'],
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
