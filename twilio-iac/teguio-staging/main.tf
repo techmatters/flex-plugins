98ff1ee24f75dd8e02f06445df1fddfa032ce3ef
Stephen Okpalaononuju <stephenokpala@gmail.com>
2022-06-27 09:58:36 +0100

    CHI-1188: set up te guio colombia II helpline

--- twilio-iac/teguio-staging/main.tf
+++ twilio-iac/teguio-staging/main.tf
@@ -45,7 +45,7 @@ module "services" {
 module "taskRouter" {
   source = "../terraform-modules/taskRouter/default"
   serverless_url = var.serverless_url
-  helpline = var.helpline
+  helpline = "Te Guio Colombia"
 }
 
 module studioFlow {
