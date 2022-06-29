f51f0bfbb3618cc8d58203f375a22027dd2050bb
mythilytm <mythily@techmatters.org>
2022-06-28 21:25:48 -0400

    Added padding in Contact Sections

--- plugin-hrm-form/src/styles/search/index.tsx
+++ plugin-hrm-form/src/styles/search/index.tsx
@@ -321,9 +321,8 @@ export const DetailsContainer = styled('div')`
 
 export const SectionTitleContainer = styled(Row)<ColorProps>`
   background-color: #ecedf1;
-  padding: 8px;
-  padding-left: 18px;
-  margin: 2px 0;
+  padding: 8px 25px 8px;
+  margin:2px 0;
   border-left: ${({ color }) => (color ? `6px solid ${color}` : 'none')};
 `;
 SectionTitleContainer.displayName = 'SectionTitleContainer';
