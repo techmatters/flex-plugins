2d8bd1fb6f5a73dfea1c7d5df05d4a62c95cbb03
Stephen Hand <steveh@techmatters.org>
2022-07-14 21:35:12 +0100

    Removed redundant style

--- plugin-hrm-form/src/styles/contact/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'react-emotion';
-
-import { StyledNextStepButton } from '../HrmStyles';
-
-export const LoadConversationButton = styled(StyledNextStepButton)`
-  display: inline-flex;
-`;
-LoadConversationButton.displayName = 'LoadConversationButton';
