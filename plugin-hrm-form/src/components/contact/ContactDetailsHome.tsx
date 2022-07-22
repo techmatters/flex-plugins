ffcee4dbde6ff61dabfed4b9c8bdd8f0eabc36a7
Stephen Hand <steveh@techmatters.org>
2022-07-22 09:59:04 +0100

    Fixed reservation lookup for transcripts / voice recordings UI

--- plugin-hrm-form/src/components/contact/ContactDetailsHome.tsx
+++ plugin-hrm-form/src/components/contact/ContactDetailsHome.tsx
@@ -117,7 +117,7 @@ const ContactDetailsHome: React.FC<Props> = function ({
 
   const loadConversationIntoOverlay = async () => {
     await Actions.invokeAction(Insights.Player.Action.INSIGHTS_PLAYER_PLAY, {
-      taskSid: savedContact.details.conversationMedia[0],
+      taskSid: savedContact.details.conversationMedia[0].reservationSid,
     });
   };
 
@@ -260,8 +260,7 @@ const ContactDetailsHome: React.FC<Props> = function ({
       {((featureFlags.enable_voice_recordings && channel === channelTypes.voice) ||
         (featureFlags.enable_transcripts && channel !== channelTypes.voice)) &&
         canViewTranscript &&
-        savedContact.details.conversationMedia &&
-        savedContact.details.conversationMedia.length &&
+        savedContact.details.conversationMedia?.length &&
         typeof savedContact.overview.conversationDuration === 'number' && (
           <SectionTitleContainer style={{ justifyContent: 'right', paddingTop: '10px', paddingBottom: '10px' }}>
             <SectionActionButton type="button" onClick={loadConversationIntoOverlay}>
