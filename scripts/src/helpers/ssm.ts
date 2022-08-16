0f03e8483213fd70137c7e5cddb23cc6bb15dceb
Stephen Hand <steveh@techmatters.org>
2022-08-16 10:13:05 +0100

    Fix SSM lib to use AWS_REGION value rather than hardcoding to us-east-1

--- scripts/src/helpers/ssm.ts
+++ scripts/src/helpers/ssm.ts
@@ -7,7 +7,7 @@ AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
-  region: 'us-east-1',
+  region: process.env.AWS_REGION ?? 'us-east-1',
 });
 
 const ssm = new AWS.SSM();
