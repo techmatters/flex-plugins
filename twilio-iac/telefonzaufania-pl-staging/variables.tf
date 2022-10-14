8718cac423c8a080801bad6def484bd39c342e4d
Alejandro Rivera <alnrivera@gmail.com>
2022-10-14 13:56:07 -0300

    Updating aws_account_id var

--- twilio-iac/telefonzaufania-pl-staging/variables.tf
+++ twilio-iac/telefonzaufania-pl-staging/variables.tf
@@ -3,12 +3,7 @@ variable "auth_token" {}
 variable "serverless_url" {}
 variable "datadog_app_id" {}
 variable "datadog_access_token" {}
-
-variable "aws_account_id" {
-  description = "Numeric AWS account ID used in ARNs"
-  type        = string
-  default     = null
-}
+variable "aws_account_id" {}
 
 variable "local_os" {
   description = "The OS running the terraform script. The only value that currently changes behaviour from default is 'Windows'"
