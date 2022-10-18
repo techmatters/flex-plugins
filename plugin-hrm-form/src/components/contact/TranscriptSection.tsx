5f3c1a87acc2c3a46cb3d6ddfeeee14129b92d7c
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-10-17 23:28:05 -0300

    Added loadTranscript action (and logic for it)

--- plugin-hrm-form/src/components/contact/TranscriptSection.tsx
+++ plugin-hrm-form/src/components/contact/TranscriptSection.tsx
@@ -1,11 +1,13 @@
-import { Template } from '@twilio/flex-ui';
+import { Template, MessageListItem, MessageList } from '@twilio/flex-ui';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { format } from 'date-fns';
 
 import { contactFormsBase, namespace, RootState } from '../../states';
 import { getFileDownloadUrl } from '../../services/ServerlessService';
 import { SectionActionButton } from '../../styles/search';
+import { loadTranscript } from '../../states/contacts/existingContacts';
 
 type OwnProps = {
   contactId: string;
@@ -14,7 +16,7 @@ type OwnProps = {
 // eslint-disable-next-line no-use-before-define
 type Props = OwnProps & ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
-const TranscriptSection: React.FC<Props> = ({ contactId, transcriptUrl, transcript }) => {
+const TranscriptSection: React.FC<Props> = ({ contactId, transcriptUrl, transcript, loadTranscript }) => {
   const [loading, setLoading] = useState(false);
 
   if (loading) {
@@ -29,7 +31,8 @@ const TranscriptSection: React.FC<Props> = ({ contactId, transcriptUrl, transcri
         const transcriptPreSignedUrl = await getFileDownloadUrl(fileNameAtAws, '');
         const transcriptJson = await fetch(transcriptPreSignedUrl.downloadUrl);
         const transcriptParsed = await transcriptJson.json();
-        console.log('>>>>>>>>>>>>>>', transcriptParsed);
+        // TODO: the current example of a transcript contains more stuff. Here we only want the transcript itself probably?
+        loadTranscript(contactId, transcriptParsed.transcript);
         setLoading(false);
       } catch (err) {
         console.error(`Error loading the transcript for contact ${contactId}, transcriptUrl ${transcriptUrl}`, err);
@@ -43,13 +46,33 @@ const TranscriptSection: React.FC<Props> = ({ contactId, transcriptUrl, transcri
     );
   }
 
-  return <div>TranscriptSection</div>;
+  console.log(transcript);
+  return (
+    <div className="Twilio-MessageList">
+      {transcript.messages.map(m => {
+        return (
+          <div className="Twilio-MessageListItem-BubbleContainer" key={m.sid}>
+            <div className="Twilio-MessageBubble-default">
+              <div className="Twilio-MessageBubble-Header">
+                <div className="Twilio-MessageBubble-UserName">{m.from}</div>
+                <div className="Twilio-MessageBubble-Time">{format(new Date(m.dateCreated), 'h:mm aaaaa')}m</div>
+              </div>
+              <div className="Twilio-MessageBubble-Body">{m.body}</div>
+              <div className="Twilio-MessageBubble-end" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({
   transcript: state[namespace][contactFormsBase].existingContacts[ownProps.contactId]?.transcript,
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  loadTranscript,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TranscriptSection);
