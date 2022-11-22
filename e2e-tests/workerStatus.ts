b0fa58a39036e11c016847e7694856f7e240f9c5
Stephen Hand <steveh@techmatters.org>
2022-11-22 07:26:24 +0000

    Fix E2E tests for Flex v2.0.0-beta.3

--- e2e-tests/workerStatus.ts
+++ e2e-tests/workerStatus.ts
@@ -14,23 +14,17 @@ export function statusIndicator(page: Page) {
     userActivityDropdownButton: page.locator("//button[@data-test='activity-dropdown-button']"),
     userActivityDropdownOption: (status: WorkerStatus) =>
       page.locator(
-        `//div[@aria-label='Actions']//button[@data-paste-element='MENU_ITEM']//span[text()='${status}']`,
+        `//div[@aria-label='Activity selection']//button[@data-paste-element='MENU_ITEM']//span[text()='${status}']`,
       ),
   };
 
   return {
     setStatus: async function (status: WorkerStatus) {
       const statusSelector = selectors.userActivityDropdownOption(status);
-      for (let i = 0; i < OPEN_ATTEMPTS; i++) {
-        try {
-          await selectors.userActivityDropdownButton.click();
-          console.log('Worker status dropdown should be open');
-          await statusSelector.waitFor({ state: 'visible', timeout: 3000 });
-          console.log('Worker status option spotted');
-        } catch (err) {
-          console.log(`Worker status option not spotted, retrying opening menu (attempt ${i+1}).`);
-        }
-      }
+      await selectors.userActivityDropdownButton.click();
+      console.log('Worker status dropdown should be open');
+      await statusSelector.waitFor({ state: 'visible', timeout: 3000 });
+      console.log('Worker status option spotted');
       await statusSelector.click();
       console.log('Worker status option clicked');
       await expect(selectors.userActivityDropdownButton).toContainText(status.toLocaleString());
