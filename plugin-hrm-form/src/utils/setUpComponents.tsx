a806bb42c9ec2750646de2882e49ed0cf3e5dadd
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-12-06 18:49:03 -0300

    Removed unnecesary import

--- plugin-hrm-form/src/utils/setUpComponents.tsx
+++ plugin-hrm-form/src/utils/setUpComponents.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react';
 import * as Flex from '@twilio/flex-ui';
-import type { MessageInputChildrenProps } from '@twilio/flex-ui-core/src/components/channel/MessageInput/MessageInputImpl';
 
 import { AcceptTransferButton, RejectTransferButton, TransferButton } from '../components/transfer';
 import * as TransferHelpers from './transfer';
