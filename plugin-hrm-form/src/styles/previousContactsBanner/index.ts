b11dd5c8c2e1b9bec99486e5553ae16dbc46c92f
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-12-09 20:36:20 -0300

    Exported yellow banner constant height

--- plugin-hrm-form/src/styles/previousContactsBanner/index.ts
+++ plugin-hrm-form/src/styles/previousContactsBanner/index.ts
@@ -1,9 +1,11 @@
 import { styled } from '@twilio/flex-ui';
 
+export const YellowBannerHeight = '36px';
+
 export const YellowBanner = styled('div')`
   display: flex;
   background-color: #fdfad3;
-  height: 36px;
+  height: ${YellowBannerHeight};
   font-size: 13px;
   align-items: center;
   justify-content: center;
