5396d2ebe48084f83d5810834c8f17209c2de26a
Stephen Hand <steveh@techmatters.org>
2023-01-11 22:39:43 +0000

    Fixed CSAM reports copt on contact details

--- plugin-hrm-form/src/styles/HrmStyles.tsx
+++ plugin-hrm-form/src/styles/HrmStyles.tsx
@@ -1192,14 +1192,3 @@ export const StyledCSAMReportDropdownList = styled('button')`
   border: none;
 `;
 StyledCSAMReportDropdownList.displayName = 'StyledCSAMReportDropdownList';
-
-export const StyledCSAMReportHeader = styled('li')`
-  position: relative;
-  font-size: 12px;
-  display: block;
-  color: inherit;
-  padding: 7px 0 7px 0;
-  text-decoration: none;
-  font-weight: 900;
-`;
-StyledCSAMReportHeader.displayName = 'StyledCSAMReportHeader';
