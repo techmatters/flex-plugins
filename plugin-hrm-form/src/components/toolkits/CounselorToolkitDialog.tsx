f2db09b3e53fbb89f2030829d582f7a21cb2cbc4
Stephen Okpalaononuju <stephenokpala@gmail.com>
2023-02-02 12:31:51 +0100

    add license statement to toolkits files

--- plugin-hrm-form/src/components/toolkits/CounselorToolkitDialog.tsx
+++ plugin-hrm-form/src/components/toolkits/CounselorToolkitDialog.tsx
@@ -1,9 +1,21 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
 import React from 'react';
 import { Button, Popover } from '@material-ui/core';
-import GridIcon from '@material-ui/icons/GridOn';
-import ListIcon from '@material-ui/icons/List';
 import { Template } from '@twilio/flex-ui';
-import { CategoriesDefinition } from 'hrm-form-definitions';
 
 import { Row } from '../../styles/HrmStyles';
 import { ConfirmContainer, ConfirmText, CancelButton } from '../../styles/search';
