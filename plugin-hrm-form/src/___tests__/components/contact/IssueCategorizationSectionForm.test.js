5b7666707ac491e582afd636a1e39d7bab077fd3
Stephen Okpalaononuju <stephenokpala@gmail.com>
2023-02-07 15:31:13 +0100

    Add Tipsheet to json file and remove modal

--- plugin-hrm-form/src/___tests__/components/contact/IssueCategorizationSectionForm.test.js
+++ plugin-hrm-form/src/___tests__/components/contact/IssueCategorizationSectionForm.test.js
@@ -47,19 +47,6 @@ const themeConf = {
   colorTheme: HrmTheme,
 };
 
-const helplineEntry = [
-  {
-    label: 'label1',
-    value: 'label1',
-    default: true,
-    manager: {
-      name: 'test',
-      phone: '0954380213',
-      email: 'test@mail.ta',
-    },
-  },
-];
-
 getAseloFeatureFlags.mockReturnValue({
   // eslint-disable-next-line camelcase
   featureFlags: { enable_counselor_toolkits: true },
@@ -107,7 +94,6 @@ test('Click on view subcategories as grid icon', () => {
             definition={definition}
             display={true}
             stateApi={forTask({ taskSid: taskId })}
-            helplineInformation={{ label: 'label1', helplines: helplineEntry }}
           />
         </FormProvider>
       </Provider>
@@ -154,7 +140,6 @@ test('Click on view subcategories as list icon', () => {
             definition={definition}
             display={true}
             stateApi={forTask({ taskSid: taskId })}
-            helplineInformation={{ label: 'label1', helplines: helplineEntry }}
           />
         </FormProvider>
       </Provider>
