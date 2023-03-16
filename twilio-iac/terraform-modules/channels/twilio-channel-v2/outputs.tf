01a6945c21203e67054b29c20bcccae7678ea14b
Alejandro Rivera <alnrivera@gmail.com>
2023-03-16 16:36:49 -0300

    new updates

--- twilio-iac/terraform-modules/channels/twilio-channel-v2/outputs.tf
+++ twilio-iac/terraform-modules/channels/twilio-channel-v2/outputs.tf
@@ -4,7 +4,7 @@ output "channel_messaging_studio_flow_sid" {
 }
 
 
-output "channel_flow_sid" {
-  description = "Twilio SID of the 'Flex Flow' studio flow."
-  value = twilio_flex_flex_flows_v1.channel_flow.sid
+output "channel_conversations_address_sid" {
+  description = "Twilio SID of the Conversion Address in Flex"
+  value = twilio_conversations_configuration_addresses_v1.conversations_address.sid
 }
