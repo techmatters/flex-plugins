dafada30865e20dea447e7e7657af77dcc484367
Robert Bo Davis <bo@interrobang.consulting>
2023-03-23 16:48:30 -0400

    chore: cleanup

--- twilio-iac/terraform-modules/stages/chatbot/main.tf
+++ twilio-iac/terraform-modules/stages/chatbot/main.tf
@@ -18,7 +18,7 @@ locals {
   default_chatbot_sids = {
     post_survey = var.default_autopilot_chatbot_enabled ? module.chatbots[0].post_survey_chatbot_sid : ""
   }
-  chatbot_sids = merge(local.chatbot_sids, local.custom_chatbot_sids)
+  chatbot_sids            = merge(local.chatbot_sids, local.custom_chatbot_sids)
   post_survey_chatbot_url = "https://channels.autopilot.twilio.com/v1/${local.twilio_account_sid}/${local.chatbot_sids["post_survey"]}/twilio-chat"
 }
 
@@ -40,7 +40,7 @@ provider "twilio" {
 module "chatbots" {
   count = var.default_autopilot_chatbot_enabled ? 1 : 0
 
-  source = "../../chatbots/default"
+  source         = "../../chatbots/default"
   serverless_url = local.serverless_url
 }
 
@@ -50,9 +50,6 @@ moved {
 }
 
 resource "aws_ssm_parameter" "twilio_post_survey_bot_chat_url_old" {
-  # Deserialise the JSON used for the keys - this way we can have multiple values per key
-  # note: this can also be accomplished in a more "tf native" way by using an array of objects and a `for` loop.
-  # see: https://github.com/techmatters/flex-plugins/blob/1edf877bba4760370af16f41045fa14956d5620f/twilio-iac/terraform-modules/aws/default/main.tf#L206
   name        = "${var.short_environment}_${jsondecode(each.value)[0]}_${var.short_helpline}_${each.key}"
   type        = "SecureString"
   value       = local.post_survey_chatbot_url
@@ -60,14 +57,14 @@ resource "aws_ssm_parameter" "twilio_post_survey_bot_chat_url_old" {
 }
 
 resource "aws_ssm_parameter" "twilio_post_survey_bot_chat_url" {
-  name  = "/${lower(var.environment)}/twilio/${local.twilio_account_sid}/post_survey_bot_chat_url"
-  type  = "SecureString"
-  value = local.post_survey_chatbot_url
+  name        = "/${lower(var.environment)}/twilio/${local.twilio_account_sid}/post_survey_bot_chat_url"
+  type        = "SecureString"
+  value       = local.post_survey_chatbot_url
   description = "Twilio account - Post Survey Bot Chat URL"
 
   tags = {
     Environment = lower(var.environment)
-    Name = "/${lower(var.environment)}/twilio/${local.twilio_account_sid}/post_survey_bot_chat_url"
-    Terraform = true
+    Name        = "/${lower(var.environment)}/twilio/${local.twilio_account_sid}/post_survey_bot_chat_url"
+    Terraform   = true
   }
 }
