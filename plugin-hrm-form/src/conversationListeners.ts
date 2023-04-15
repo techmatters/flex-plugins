d7374be1e7ee6c67db136fde7b14a7de35db4fdd
Stephen Hand <steveh@techmatters.org>
2023-04-15 08:08:44 +0100

    Tidied up conversationListeners parameter handling

--- plugin-hrm-form/src/conversationListeners.ts
+++ plugin-hrm-form/src/conversationListeners.ts
@@ -1,20 +1,20 @@
 import { Conversation } from '@twilio/conversations';
 
-type ConversationListener = Parameters<Conversation['on']>;
+type ConversationListenerParams = Parameters<Conversation['on']>;
 
-const aseloListeners: Record<string, ConversationListener[]> = {};
+const aseloListeners: Record<string, ConversationListenerParams[]> = {};
 
-export const addAseloListener = (conversation: Conversation, ...[event, listener]: ConversationListener) => {
-  conversation.on(event, listener);
-  aseloListeners[conversation.sid] = [...(aseloListeners[conversation.sid] || []), [event, listener]];
+export const addAseloListener = (conversation: Conversation, ...listenerArgs: ConversationListenerParams) => {
+  conversation.on(...listenerArgs);
+  aseloListeners[conversation.sid] = [...(aseloListeners[conversation.sid] || []), listenerArgs];
 };
 
 export const deactivateAseloListeners = (conversation: Conversation) => {
   const listeners = aseloListeners[conversation.sid] || [];
-  listeners.forEach(([event, listener]) => conversation.off(event, listener as any));
+  listeners.forEach(listenerArgs => conversation.off(...listenerArgs));
 };
 
 export const reactivateAseloListeners = (conversation: Conversation) => {
   const listeners = aseloListeners[conversation.sid] || [];
-  listeners.forEach(([event, listener]) => conversation.on(event, listener as any));
+  listeners.forEach(listenerArgs => conversation.on(...listenerArgs));
 };
