cc1c4f14f91b6e6a7f6bff93a68b35b44c103313
Murilo Machado <murilovmachado@gmail.com>
2023-04-18 09:37:11 +0100

    Send typing indicator information

--- plugin-hrm-form/src/components/AseloMessageInput.tsx
+++ plugin-hrm-form/src/components/AseloMessageInput.tsx
@@ -1,18 +1,42 @@
 import { MessageInputChildrenProps } from '@twilio/flex-ui-core/src/components/channel/MessageInput/MessageInputImpl';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Manager, Template, withTheme } from '@twilio/flex-ui';
 import { useForm } from 'react-hook-form';
+import debounce from 'lodash/debounce';
 
 import CannedResponses from './CannedResponses';
 import useTraceUpdate from '../hooks/useTraceUpdate';
 
 type Props = Partial<MessageInputChildrenProps>;
 
+type Conversation = Awaited<ReturnType<Manager['conversationsClient']['getConversationBySid']>>;
+
 const AseloMessageInput: React.FC<Props> = props => {
+  const [conversation, setConversation] = useState<Conversation>();
   const { register, handleSubmit, setValue, getValues } = useForm();
+  const { conversationSid } = props;
+
+  useEffect(() => {
+    const fetchConversation = async () => {
+      const conversation = await Manager.getInstance().conversationsClient.getConversationBySid(conversationSid);
+      setConversation(conversation);
+    };
+
+    fetchConversation();
+  }, [conversationSid]);
+
+  const triggerTyping = () => {
+    if (conversation) {
+      conversation.typing();
+    }
+  };
+
+  const handleChange = debounce(triggerTyping, 500, {
+    leading: true,
+    trailing: true,
+  });
 
   useTraceUpdate(props, 'AseloMessageInput');
-  const { conversationSid } = props;
 
   return (
     <div key="textarea">
@@ -22,15 +46,15 @@ const AseloMessageInput: React.FC<Props> = props => {
         ref={ref => {
           register(ref);
         }}
+        onChange={handleChange}
       />
       <Button
         onClick={handleSubmit(async () => {
           const message = getValues('messageInputArea');
 
-          return Manager.getInstance()
-            .conversationsClient.getConversationBySid(conversationSid)
-            .then(conversation => conversation.sendMessage(message))
-            .then(() => setValue('messageInputArea', ''));
+          if (conversation) {
+            conversation.sendMessage(message).then(() => setValue('messageInputArea', ''));
+          }
         })}
       >
         <Template code="Send" />
