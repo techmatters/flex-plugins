46cc755337fdfbe0f5eab349195c403e3f6f4801
Stephen Hand <steveh@techmatters.org>
2023-04-25 19:17:06 +0100

    Comments on the updateAllAssetUrls.ts scripts

--- scripts/src/terraformSupplemental/updateAllAssetUrls.ts
+++ scripts/src/terraformSupplemental/updateAllAssetUrls.ts
@@ -5,6 +5,11 @@ import {
 import { patchFeatureFlags } from './updateFlexServiceConfiguration';
 import { logDebug } from '../helpers/log';
 
+/**
+ * This script was used to update the asset urls for all environments in a one off operation.
+ * It is left here as an example of how to combine the runAgainstAllAccountsForEnvironment and patchFeatureFlags
+ * scripts to update the configurations for all accounts in an environment.
+ */
 async function main() {
   // eslint-disable-next-line no-restricted-syntax
   for (const environment of [
