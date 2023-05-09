01eb747bb7c5d9114d4453b2564163c1f7fd7958
Robert Bo Davis <bo@interrobang.consulting>
2023-05-09 14:37:51 -0400

    chore: fix typo

--- twilio-iac/scripts/verifyPreWork.sh
+++ twilio-iac/scripts/verifyPreWork.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 printf "\n\n"
-read -p "Have you checked/updted the terraform spreadsheet (https://techmatters.app.box.com/file/1109527438079)? (y/N)" -n 1 -r
+read -p "Have you checked/updated the terraform spreadsheet (https://techmatters.app.box.com/file/1109527438079)? (y/N)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     exit 0
 fi
