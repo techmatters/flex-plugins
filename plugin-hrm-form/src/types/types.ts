bca81b0d467b123839d75484cd4f236042d444f1
Stephen Hand <steveh@techmatters.org>
2023-05-29 17:00:59 +0100

    Add feature flag to switch resources over to use elastic search

--- plugin-hrm-form/src/types/types.ts
+++ plugin-hrm-form/src/types/types.ts
@@ -260,6 +260,7 @@ export type FeatureFlags = {
   enable_counselor_toolkits: boolean; // Enables Counselor Toolkits
   enable_emoji_picker: boolean; // Enables Emoji Picker
   enable_aselo_messaging_ui: boolean; // Enables Aselo Messaging UI iinstead of the default Twilio one - reduced functionality for low spec clients.
+  enable_resources_elastic_search: boolean; // Use the EasticSearch powered search for resources, rather than the interim name only version.
 };
 /* eslint-enable camelcase */
 
