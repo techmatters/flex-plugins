298d11f87a7caf1c07e77779ed7bf5f47d5acc25
Mythily Mudunuru <102122005+mythilytm@users.noreply.github.com>
2023-06-13 11:59:49 -0400

    CHI1938 Conference Add party and Phone Dialog UI 2 (#1444)
    * First step: add a participant
    
    * Added button to remove participant
    
    * Use conferenceSid instead than taskSid
    
    * Fix bug that leaves a stuck screen when convo is undefined
    
    * Licence header :)
    
    * Implemented toogle hold behavior
    
    * Conference Dialog for phone input
    
    * Dial Button and input ui mods
    
    * CHI1938 Conference Custom Actions (#1448)
    
    * ToggleMute work in progress
    
    * ToggleMute done, hangup in progress
    
    * Custom Conference Actions Hangup, Microphone
    
    * Hang-up button clean up
    
    * Add enable_conferencing feature flag
    
    * Add enable_conferencing feature flag - lint
    
    * Disable and enable Button based on participants
    
    ---------
    
    Co-authored-by: Gianfranco Paoloni <paolonigianfranco@gmail.com>

---
+++ plugin-hrm-form/src/components/Conference/ConferenceActions/Hangup.tsx
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import React from 'react';
+import { TaskContextProps, TaskHelper, withTaskContext } from '@twilio/flex-ui';
+import { CallEndOutlined as CallEndIcon } from '@material-ui/icons';
+
+import { conferenceApi } from '../../../services/ServerlessService';
+import { StyledConferenceButtonWrapper, StyledHangUpButton } from './styles';
+
+type Props = TaskContextProps;
+
+const Hangup: React.FC<Props> = ({ call, task }) => {
+  if (!task || !call) {
+    return null;
+  }
+
+  const handleClick = async () => {
+    await conferenceApi.removeParticipant({
+      callSid: call?.parameters?.CallSid,
+      conferenceSid: task?.attributes?.conference?.sid,
+    });
+  };
+
+  const isLiveCall = TaskHelper.isLiveCall(task);
+
+  return (
+    <StyledConferenceButtonWrapper>
+      <StyledHangUpButton disabled={!isLiveCall} onClick={handleClick}>
+        <CallEndIcon />
+      </StyledHangUpButton>
+      <span>Hang Up</span>
+    </StyledConferenceButtonWrapper>
+  );
+};
+
+export default withTaskContext(Hangup);
---
+++ plugin-hrm-form/src/conference/setUpConferenceComponents.tsx
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import React from 'react';
+import { CallCanvasActions, ParticipantCanvas, TaskHelper } from '@twilio/flex-ui';
+
+import ConferencePanel from '../components/Conference/ConferenceActions/ConferencePanel';
+import ToggleMute from '../components/Conference/ConferenceActions/ToggleMute';
+import Hangup from '../components/Conference/ConferenceActions/Hangup';
+import HoldParticipantButton from '../components/Conference/HoldParticipantButton';
+import RemoveParticipantButton from '../components/Conference/RemoveParticipantButton';
+
+export const setupConferenceComponents = () => {
+  CallCanvasActions.Content.remove('toggleMute', {
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  CallCanvasActions.Content.remove('dialpad', {
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  CallCanvasActions.Content.remove('hangup', {
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+
+  CallCanvasActions.Content.add(<ConferencePanel key="conference-panel" />, {
+    sortOrder: -1,
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  CallCanvasActions.Content.add(<ToggleMute key="conference-toggle" />, {
+    sortOrder: 1,
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  CallCanvasActions.Content.add(<Hangup key="conference-hangup" />, {
+    sortOrder: 3,
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+
+  ParticipantCanvas.Actions.Content.remove('hold', {
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  ParticipantCanvas.Actions.Content.add(<HoldParticipantButton key="hold-participant" />, {
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+  ParticipantCanvas.Actions.Content.add(<RemoveParticipantButton key="remove-participant" />, {
+    sortOrder: 99,
+    if: props => TaskHelper.isCallTask(props.task) && TaskHelper.isLiveCall(props.task),
+  });
+};
