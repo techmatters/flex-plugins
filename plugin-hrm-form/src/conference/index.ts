dc01844a5cbe6cd9a525ffa02f35f8ea3d07ea66
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-06-16 11:33:51 -0300

    Prevent hangup in conference if participant is on hold (#1453)

--- plugin-hrm-form/src/conference/index.ts
+++ plugin-hrm-form/src/conference/index.ts
@@ -14,3 +14,4 @@
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 export { setupConferenceComponents } from './setUpConferenceComponents';
+export { setUpConferenceActions } from './setUpConferenceActions';
