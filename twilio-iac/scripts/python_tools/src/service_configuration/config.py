fffc16df39632e533745077514786abd1c442eb4
Robert Bo Davis <bo@interrobang.consulting>
2023-07-17 09:05:42 -0400

    feat: json only output for reuse in other tools

--- twilio-iac/scripts/python_tools/src/service_configuration/config.py
+++ twilio-iac/scripts/python_tools/src/service_configuration/config.py
@@ -13,11 +13,67 @@ ACTIONS = {
     'SHOW_NEW': 'show_new',
     'SHOW_DIFF': 'show_diff',
     'PLAN': 'plan',
-    'UPDATE': 'update',
+    'APPLY': 'apply',
+}
+
+JSON_ACTIONS = [
+    ACTIONS['SHOW_REMOTE'],
+    ACTIONS['SHOW_LOCAL'],
+    ACTIONS['SHOW_NEW'],
+    ACTIONS['PLAN']
+]
+
+ENVIRONMENTS = [
+    'development',
+    'staging',
+    'production',
+]
+
+ARGS = {
+    'action': {
+        'choices': ACTIONS.values(),
+        'nargs': '?',
+        'default': ACTIONS['SHOW'],
+        'help': 'Action to take.',
+    },
+    '--account_sid': {
+        'required': False,
+        'default': os.environ.get('TWILIO_ACCOUNT_SID'),
+        'help': 'Twilio account SID.',
+    },
+    '--helpline_code': {
+        'required': False,
+        'default': os.environ.get('HL'),
+        'help': 'Helpline short code. Examples: as, e2e, th, etc.',
+    },
+    '--environment': {
+        'choices': ENVIRONMENTS,
+        'required': False,
+        'default': os.environ.get('HL_ENV'),
+        'help': 'Helpline Environment',
+    },
+    '--prop': {
+        'required': False,
+        'default': None,
+        'help': 'Property to update. Example: attributes.hrm_api_version',
+    },
+    '--dry_run': {
+        'required': False,
+        'default': False,
+        'help': 'Enable dry run.',
+    },
+    '--json': {
+        'required': False,
+        'action': 'store_true',
+        'default': False,
+        'help': 'Enable json only output.',
+    },
 }
 
 
 class ConfigDict(TypedDict):
+    # WARNING: ConfigDict is used in the magic __getattr__ method, so keys
+    # must not bet the same as any methods or attributes of the Config class
     action: str
     prop: NotRequired[str]
     helpline_code: str | None
@@ -48,23 +104,19 @@ class Config():
         self.init_arg_parser()
         self.parse_args()
         self._ssm_client = SSMClient(
-            f"arn:aws:iam::712893914485:role/tf-twilio-iac-{self.get_value('environment')}")
+            f"arn:aws:iam::712893914485:role/tf-twilio-iac-{self.environment}")
         self.init_service_configurations()
+
+    def __getattr__(self, name: str):
+        try:
+            return self.get_value(name)
+        except KeyError:
+            raise AttributeError(f"No such attribute: {name}")
 
     def init_arg_parser(self) -> None:
         self._arg_parser = ArgumentParser()
-        self._arg_parser.add_argument(
-            'action', choices=ACTIONS.values(), nargs='?', default=ACTIONS['SHOW'])
-        self._arg_parser.add_argument(
-            '--account_sid', required=False, default=os.environ.get('TWILIO_ACCOUNT_SID'))
-        self._arg_parser.add_argument(
-            '--helpline_code', required=False, default=os.environ.get('HL'))
-        self._arg_parser.add_argument(
-            '--environment', required=False, default=os.environ.get('HL_ENV'))
-        self._arg_parser.add_argument(
-            '--prop', required=False, default=None)
-        self._arg_parser.add_argument(
-            '--dry_run', required=False, default=False)
+        for arg, options in ARGS.items():
+            self._arg_parser.add_argument(arg, **options)
 
     def parse_args(self):
         args = self._arg_parser.parse_args()
@@ -76,6 +128,8 @@ class Config():
         if auth_token:
             self._config['auth_token'] = auth_token
 
+        self.validate_json()
+
     def init_service_configurations(self):
         account_sid = self._config.get('account_sid')
         auth_token = self._config.get('auth_token')
@@ -83,7 +137,10 @@ class Config():
         environment = self._config.get('environment')
 
         if (helpline_code) and environment:
-            print(f"Initializing service configuration for {helpline_code}")
+            if (not self.json):
+                print(
+                    f"Initializing service configuration for {helpline_code}")
+
             self.init_service_configuration(
                 helpline_code=helpline_code,
                 environment=environment,
@@ -96,8 +153,8 @@ class Config():
             self.init_service_configurations_for_environment(environment)
             return
 
-        raise Exception(
-            "Invalid configuration. Please provide environment and optionally an account_sid or helpline_code ")
+        print("ERROR: Invalid configuration. Please provide environment and optionally an account_sid or helpline_code ")
+        exit(1)
 
     def init_service_configuration(self, **kwargs: Unpack[InitServiceConfigurationArgsDict]):
         twilio_client = Twilio(**kwargs)
@@ -123,6 +180,12 @@ class Config():
 
     def get_account_sids(self):
         return self._config['service_configurations'].keys()
+
+    def validate_json(self):
+        if self.json and (self.action not in JSON_ACTIONS):
+            print("ERROR: JSON output is only available for the following actions: " +
+                  ', '.join(JSON_ACTIONS))
+            exit(1)
 
 
 config = Config()
--- twilio-iac/scripts/python_tools/src/service_configuration/manager.py
+++ twilio-iac/scripts/python_tools/src/service_configuration/manager.py
@@ -2,11 +2,11 @@ import json
 from pygments import highlight, lexers, formatters
 from termcolor import colored
 from .config import config
-from .service_configuration import ServiceConfiguration
+from .service_configuration import DeepDiff, ServiceConfiguration
 
 
 def main():
-    action = config.get_value('action')
+    action = config.action
     for account_sid in config.get_account_sids():
         service_configuration = config.get_service_configuration(
             account_sid)
@@ -14,9 +14,16 @@ def main():
         globals()[action](service_configuration)
 
 
-def print_json(structure):
+def print_text(text: object):
+    if (config.json):
+        return
+
+    print(text)
+
+
+def print_json(structure: object):
     formatted_json = json.dumps(structure, sort_keys=True, indent=4)
-    colorful_json = highlight(
+    colorful_json: str = highlight(
         formatted_json,
         lexers.JsonLexer(),
         formatters.TerminalFormatter()
@@ -24,7 +31,11 @@ def print_json(structure):
     print(colorful_json)
 
 
-def print_plan(plan):
+def print_plan(plan: DeepDiff):
+    if (config.json):
+        print(plan.to_json())
+        return
+
     lines = plan.pretty().split('\n')  # split the diff into lines
 
     for line in lines:
@@ -39,42 +50,42 @@ def print_plan(plan):
 
 
 def print_service_configuration_info(service_configuration: ServiceConfiguration):
-    print("\n------------------")
-    print(f"Environment: {service_configuration.environment}")
-    print(f"Helpline Code: {service_configuration.helpline_code}")
-    print(f"Account SID: {service_configuration.account_sid}")
-    print("")
+    print_text("\n------------------")
+    print_text(f"Environment: {service_configuration.environment}")
+    print_text(f"Helpline Code: {service_configuration.helpline_code}")
+    print_text(f"Account SID: {service_configuration.account_sid}")
+    print_text("")
 
 
 def show(service_configuration: ServiceConfiguration):
     show_remote(service_configuration)
     show_new(service_configuration)
-    show_plan(service_configuration)
+    plan(service_configuration)
 
 
 def show_remote(service_configuration: ServiceConfiguration):
-    print("Remote:")
+    print_text("Remote:")
     print_json(service_configuration.remote_state)
 
 
 def show_local(service_configuration: ServiceConfiguration):
-    print("Local:")
+    print_text("Local:")
     print_json(service_configuration.local_state)
 
 
 def show_new(service_configuration: ServiceConfiguration):
-    print("New:")
+    print_text("New:")
     print_json(service_configuration.new_state)
 
 
-def show_plan(service_configuration: ServiceConfiguration):
-    print("Plan:")
+def plan(service_configuration: ServiceConfiguration):
+    print_text("Plan:")
     print_plan(service_configuration.plan)
 
 
-def update(service_configuration: ServiceConfiguration):
-    show_plan(service_configuration)
-    if config.get_value('dry_run'):
+def apply(service_configuration: ServiceConfiguration):
+    plan(service_configuration)
+    if config.dry_run:
         print("Dry run enabled. Exiting...")
         return
 
