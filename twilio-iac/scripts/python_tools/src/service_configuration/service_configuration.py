3b0da1e92eb098c7993ef844cf3cb7429e6d96f1
Robert Bo Davis <bo@interrobang.consulting>
2023-07-19 11:02:03 -0400

    chore: scaffolding for syncer class

--- twilio-iac/scripts/python_tools/src/service_configuration/service_configuration.py
+++ twilio-iac/scripts/python_tools/src/service_configuration/service_configuration.py
@@ -10,6 +10,20 @@ from ..twilio import Twilio
 
 JSON_PATH_ROOT = "/app/twilio-iac/helplines"
 JSON_CONFIG_PATH_PARTIAL = "configs/service-configuration"
+JSON_CONFIGS = [
+    {
+        "key": "defaults",
+        "path_tpl": f"{JSON_PATH_ROOT}/{JSON_CONFIG_PATH_PARTIAL}/defaults.json",
+    },
+    {
+        "key": "common",
+        "path_tpl": f"{JSON_PATH_ROOT}/{{helpline_code}}/{JSON_CONFIG_PATH_PARTIAL}/common.json",
+    },
+    {
+        "key": "environment",
+        "path_tpl": f"{JSON_PATH_ROOT}/{{helpline_code}}/{JSON_CONFIG_PATH_PARTIAL}/{{environment}}.json",
+    },
+]
 
 SSM_FIELDS = {
     "attributes.serverless_base_url": "/{environment}/serverless/{account_sid}/base_url",
@@ -22,7 +36,7 @@ TEMPLATE_FIELDS = {
 }
 
 
-def set_nested_key(data: dict, keys: List[str], value: object):
+def set_nested_key(data: dict[str, object], keys: List[str], value: object):
     if len(keys) > 1:
         key = keys.pop(0)
         if key not in data:
@@ -32,16 +46,30 @@ def set_nested_key(data: dict, keys: List[str], value: object):
     else:
         data[keys[0]] = value
 
+
 class InitArgsDict(TypedDict):
     twilio_client: Twilio
     ssm_client: SSMClient
 
+
+class LocalConfigsItemDict(TypedDict):
+    path: str
+    data: dict[str, object]
+
+
+class LocalConfigsDict(TypedDict):
+    defaults: LocalConfigsItemDict
+    common: LocalConfigsItemDict
+    environment: LocalConfigsItemDict
+
+
 class ServiceConfiguration():
     _ssm_client: SSMClient
     _twilio_client: Twilio
-    remote_state: dict
-    local_state: dict = {}
-    new_state: dict = {}
+    remote_state: dict[str, object]
+    local_state: dict[str, object] = {}
+    new_state: dict[str, object] = {}
+    local_configs: dict[str, object] = {}
     plan: DeepDiff
     account_sid: str
     helpline_code: str
@@ -59,21 +87,26 @@ class ServiceConfiguration():
         self.init_plan()
 
     def init_local_state(self):
-        paths: list[str] = [
-            f"{JSON_PATH_ROOT}/{JSON_CONFIG_PATH_PARTIAL}/defaults.json",
-            f"{JSON_PATH_ROOT}/{self.helpline_code.lower()}/{JSON_CONFIG_PATH_PARTIAL}/common.json",
-            f"{JSON_PATH_ROOT}/{self.helpline_code.lower()}/{JSON_CONFIG_PATH_PARTIAL}/{self.environment}.json"
-        ]
+        for conf in JSON_CONFIGS:
+            key = conf['key']
+            path = conf['path_tpl'].format(
+                environment=self.environment,
+                helpline_code=self.helpline_code
+            )
+            self.local_configs[key] = {
+                'path': path,
+                'data': {}
+            }
 
-        for path in paths:
             if not path_exists(path):
                 print(f"Could not load {path}... skipping")
                 continue
 
             with open(path, 'r') as f:
+                self.local_configs[key]['data'] = json.load(f)
                 self.local_state = always_merger.merge(
-                    self.local_state,
-                    json.load(f)
+                    deepcopy(self.local_state),
+                    self.local_configs[key]['data'],
                 )
 
         self.init_template_fields()
