87e65c00c4728d64284787f589e38f6bdd83eef6
Alejandro Rivera <alnrivera@gmail.com>
2023-08-01 11:31:45 -0400

    lex structure

--- twilio-iac/terraform-modules/stages/lex/main.tf
+++ twilio-iac/terraform-modules/stages/lex/main.tf
@@ -18,11 +18,18 @@ module "lex" {
   environment    = var.environment
   language       = each.key
 
-  slot_types = merge([
+  /*slot_types = merge([
     for file_path in fileset("/app/twilio-iac/helplines/configs/lex/${each.key}/slot_types", "*.json") :
     jsondecode(file("/app/twilio-iac/helplines/configs/lex/${each.key}/slot_types/${file_path}"))
-  ]...)
+  ]...)*/
   intents = merge([for bot in each.value : jsondecode(file("/app/twilio-iac/helplines/configs/lex/${each.key}/intents/${bot}.json"))]...)
+  
+  slot_type_names = flatten([
+      for obj_key, obj_value in intents : [
+        for slot_name, slot_data in obj_value["slots"] : slot_data["slot_type"]
+      ]
+    ])
+  
   bots    = merge([for bot in each.value : jsondecode(file("/app/twilio-iac/helplines/configs/lex/${each.key}/bots/${bot}.json"))]...)
 }
 
