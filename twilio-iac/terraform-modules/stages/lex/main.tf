d09a43d60820aefd91a3b4e5b38f5d9c5e9bcd55
Robert Bo Davis <bo@interrobang.consulting>
2023-08-04 09:21:29 -0400

    refactor: move config file parsing into configuration layer of terragrunt

--- twilio-iac/terraform-modules/stages/lex/main.tf
+++ twilio-iac/terraform-modules/stages/lex/main.tf
@@ -2,67 +2,6 @@ provider "awscc" {
   region = var.helpline_region
 }
 
-
-locals {
-  helpline       = var.helpline
-  short_helpline = var.short_helpline
-  environment    = var.environment
-  bots_definitions = tomap({
-    for language, bots in var.lex_bot_languages :
-    language => merge(
-      [
-        for bot in bots :
-        fileexists("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/bots/${bot}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/bots/${bot}.json")) :
-        fileexists("/app/twilio-iac/helplines/configs/lex/${language}/bots/${bot}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${language}/bots/${bot}.json")) :
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${substr(language, 0, 2)}/bots/${bot}.json"))
-
-
-    ]...)
-  })
-
-  intents_definitions = tomap({
-    for language, bots in var.lex_bot_languages :
-    language => merge(
-      [
-        for bot in bots :
-        fileexists("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/intents/${bot}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/intents/${bot}.json")) :
-        fileexists("/app/twilio-iac/helplines/configs/lex/${language}/intents/${bot}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${language}/intents/${bot}.json")) :
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${substr(language, 0, 2)}/intents/${bot}.json"))
-
-
-    ]...)
-  })
-
-  slot_types_names = tomap({
-    for language, bots in var.lex_bot_languages :
-    language => distinct(flatten([
-      for obj_key, obj_value in local.intents_definitions[language] : [
-        for slot_name, slot_data in obj_value["slots"] :
-        slot_data["slot_type"]
-      ]
-  ])) })
-
-  slot_types_definitions = tomap({
-    for language, bots in var.lex_bot_languages :
-    language => merge(
-      [
-        for slot_type in local.slot_types_names[language] :
-        fileexists("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/slot_types/${slot_type}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/${local.short_helpline}/configs/lex/${language}/slot_types/${slot_type}.json")) :
-        fileexists("/app/twilio-iac/helplines/configs/lex/${language}/slot_types/${slot_type}.json") ?
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${language}/slot_types/${slot_type}.json")) :
-        jsondecode(file("/app/twilio-iac/helplines/configs/lex/${substr(language, 0, 2)}/slot_types/${slot_type}.json"))
-      ]...
-  ) })
-
-
-
-}
-
 module "lex" {
   source = "../../lex/v1"
 
@@ -78,12 +17,10 @@ module "lex" {
   environment    = var.environment
   language       = each.key
 
-  bots = local.bots_definitions[each.key]
-  intents = local.intents_definitions[each.key]
-  slot_types = local.slot_types_definitions[each.key]
-
+  bots       = var.lex_bots[each.key]
+  intents    = var.lex_intents[each.key]
+  slot_types = var.lex_slot_types[each.key]
 }
-
 
 module "lexv2" {
   source = "../../lex/v2"
