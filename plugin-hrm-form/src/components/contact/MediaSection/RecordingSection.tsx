6e64ce2ffc258e79fc62de9b12d30b0b7145c9b6
Mythily Mudunuru <102122005+mythilytm@users.noreply.github.com>
2023-09-05 17:49:59 -0400

    External Audio Recordings with Styling and Error handling (#1628)
    * Audio recording blob is fetched and plays
    
    * Implement styling for loading state and button, refactor naming
    
    * Error handling for recording failures
    
    * Add view recording permission logic for contact
    
    * Added more error handling if there is no voiceRecording
    
    * Add empty viewRecording permission set to all helplines except dev

---
+++ plugin-hrm-form/src/components/contact/MediaSection/RecordingSection.tsx
@@ -0,0 +1,101 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+import React, { useState } from 'react';
+import { Template } from '@twilio/flex-ui';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { ErrorFont, LoadMediaButton, LoadMediaButtonText } from './styles';
+import { S3StoredRecording } from '../../../types/types';
+import { getFileDownloadUrl } from '../../../services/ServerlessService';
+
+type OwnProps = { contactId: string; externalStoredRecording: S3StoredRecording };
+
+const RecordingSection: React.FC<OwnProps> = ({ contactId, externalStoredRecording }) => {
+  const [voiceRecording, setVoiceRecording] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const fetchAndLoadRecording = async () => {
+    try {
+      setLoading(true);
+      setShowButton(false);
+
+      const recordingPreSignedUrl = await getFileDownloadUrl(externalStoredRecording.location.key);
+      const recordingResponse = await fetch(recordingPreSignedUrl.downloadUrl);
+      const recordingBlob = await recordingResponse.blob();
+      const voiceRecording = new Audio(URL.createObjectURL(recordingBlob));
+      setVoiceRecording(voiceRecording.src);
+
+      setLoading(false);
+    } catch (error) {
+      handleFetchAndLoadException(error);
+    }
+  };
+
+  const handleFetchAndLoadException = err => {
+    console.error(
+      `Error loading the recording for contact ${contactId}, recording url ${externalStoredRecording.location.key}`,
+      err,
+    );
+    const errorMessage = 'RecordingSection-Error';
+
+    setErrorMessage(errorMessage);
+    setLoading(false);
+  };
+
+  if (errorMessage) {
+    return (
+      <ErrorFont>
+        <Template code={errorMessage} />
+      </ErrorFont>
+    );
+  }
+
+  if (loading) {
+    return <CircularProgress size={30} />;
+  }
+
+  if (showButton) {
+    return (
+      <LoadMediaButton
+        style={{ justifyContent: 'center', paddingTop: '10px', paddingBottom: '10px', whiteSpace: 'nowrap' }}
+        type="button"
+        onClick={fetchAndLoadRecording}
+      >
+        <LoadMediaButtonText>
+          <Template code="ContactDetails-LoadRecording-Button" />
+        </LoadMediaButtonText>
+      </LoadMediaButton>
+    );
+  }
+
+  return (
+    <div>
+      {voiceRecording ? (
+        <audio controls src={voiceRecording} preload="auto" onError={handleFetchAndLoadException}>
+          <track kind="captions" />
+        </audio>
+      ) : (
+        <ErrorFont>
+          <Template code={errorMessage} />
+        </ErrorFont>
+      )}
+    </div>
+  );
+};
+
+export default RecordingSection;
