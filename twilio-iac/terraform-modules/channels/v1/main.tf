d8e23880b71a06a5143aa79de2521494e6d7a324
Alejandro Rivera <alnrivera@gmail.com>
2023-09-18 10:39:58 -0300

    ph updates

--- twilio-iac/terraform-modules/channels/v1/main.tf
+++ twilio-iac/terraform-modules/channels/v1/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+/*
 data "aws_ssm_parameter" "webhook_url_studio_errors" {
   name = "/${var.environment}/slack/webhook_url_studio_errors"
 }
@@ -15,7 +15,11 @@ locals {
   #Marking this as non sensitive since we need to see the studio flow definition when running a plan to validate changes.
   webhook_url_studio_errors = nonsensitive(data.aws_ssm_parameter.webhook_url_studio_errors.value)
 }
-
+*/
+locals {
+  #Marking this as non sensitive since we need to see the studio flow definition when running a plan to validate changes.
+  webhook_url_studio_errors = "https://www.google.com"
+}
 #I'm not sure about this resource, the idea is to have 1 studio flow json template and also as few as possible
 #channel attribute templates.
 
