abfb5ff072150e0f8ddef20839a68aa60713a7bf
mythilytm <mythily@techmatters.org>
2023-11-20 15:12:25 -0500

    Convert NonDataCallTypeDialog to tsx

---
+++ plugin-hrm-form/src/components/callTypeButtons/NonDataCallTypeDialog.tsx
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Template } from '@twilio/flex-ui';
+
+import { Box, Row, HiddenText } from '../../styles/HrmStyles';
+import {
+  CloseTaskDialog,
+  CloseTaskDialogText,
+  NonDataCallTypeDialogContainer,
+  ConfirmButton,
+  CancelButton,
+  CloseButton,
+} from '../../styles/callTypeButtons';
+import TabPressWrapper from '../TabPressWrapper';
+
+type NonDataCallTypeDialogProps = {
+  isOpen: boolean;
+  isCallTask: boolean;
+  isInWrapupMode: boolean;
+  handleConfirm: () => void;
+  handleCancel: () => void;
+};
+
+const NonDataCallTypeDialog: React.FC<NonDataCallTypeDialogProps> = ({
+  isOpen,
+  isCallTask,
+  isInWrapupMode,
+  handleConfirm,
+  handleCancel,
+}) => {
+  const renderTemplate = () => {
+    let code;
+    if (isInWrapupMode) {
+      code = 'CallType-CloseContact';
+    } else if (isCallTask) {
+      code = 'TaskHeaderEndCall';
+    } else {
+      code = 'TaskHeaderEndChat';
+    }
+
+    return <Template code={code} />;
+  };
+
+  return (
+    <CloseTaskDialog onClose={handleCancel} open={isOpen}>
+      <TabPressWrapper>
+        <NonDataCallTypeDialogContainer>
+          <Box marginLeft="auto">
+            <HiddenText id="CloseButton">
+              <Template code="CloseButton" />
+            </HiddenText>
+            <CloseButton tabIndex={3} aria-label="CloseButton" onClick={handleCancel} />
+          </Box>
+          <CloseTaskDialogText>
+            <Template code="NonDataCallTypeDialog-CloseConfirm" />
+          </CloseTaskDialogText>
+          <Box marginBottom="32px">
+            <Row>
+              <ConfirmButton
+                data-fs-id="Task-EndCallOrChat-Button"
+                autoFocus
+                tabIndex={1}
+                onClick={handleConfirm}
+                disabled={false}
+              >
+                {renderTemplate()}
+              </ConfirmButton>
+              <CancelButton tabIndex={2} onClick={handleCancel}>
+                {/* eslint-disable-next-line react/jsx-max-depth */}
+                <Template code="CancelButton" />
+              </CancelButton>
+            </Row>
+          </Box>
+        </NonDataCallTypeDialogContainer>
+      </TabPressWrapper>
+    </CloseTaskDialog>
+  );
+};
+
+export default NonDataCallTypeDialog;
