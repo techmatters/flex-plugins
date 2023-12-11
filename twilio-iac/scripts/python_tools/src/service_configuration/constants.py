9be4f47af7fb0d6203b88b15027e223e91b3ad00
Gianfranco <paolonigianfranco@gmail.com>
2023-12-11 14:28:51 -0300

    chore: Service Configuration Manager role based access improvements (#1919)

--- twilio-iac/scripts/python_tools/src/service_configuration/constants.py
+++ twilio-iac/scripts/python_tools/src/service_configuration/constants.py
@@ -1,2 +1,10 @@
 """ Constants for the service configuration manager """
-AWS_ROLE_ARN: str = 'arn:aws:iam::712893914485:role/twilio-iac-service-config-manager'
+AWS_ROLE_ARNS: dict[str, str] = {
+    'developer': 'arn:aws:iam::712893914485:role/twilio-iac-service-config-developer',
+    'manager': 'arn:aws:iam::712893914485:role/twilio-iac-service-config-manager',
+}
+
+def get_aws_role_arn(environment: str):
+    role_key = "manager" if environment == "production" else "developer"
+    aws_role_arn = AWS_ROLE_ARNS.get(role_key)
+    return aws_role_arn
