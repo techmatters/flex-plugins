a5c161f2998f22a5b986b08cd26a11860d5e841c
mythilytm <mythily@techmatters.org>
2024-03-14 10:46:49 -0400

    revert to StyledLink in search styles

--- plugin-hrm-form/src/components/search/TagsAndCounselor.tsx
+++ plugin-hrm-form/src/components/search/TagsAndCounselor.tsx
@@ -19,8 +19,8 @@ import React from 'react';
 import { Template } from '@twilio/flex-ui';
 import { DefinitionVersion } from 'hrm-form-definitions';
 
-import { Flex } from '../../styles';
-import { SilentText, SubtitleLabel, SummaryText, TagsWrapper, TagText } from './styles';
+import { Flex, PillText } from '../../styles';
+import { SilentText, SubtitleLabel, SummaryText, TagsWrapper } from './styles';
 import CategoryWithTooltip from '../common/CategoryWithTooltip';
 import { getContactTags } from '../../utils/categories';
 
@@ -59,7 +59,7 @@ const TagsAndCounselor: React.FC<Props> = props => {
       <TagsWrapper>
         {props.nonDataCallType && (
           <SilentText>
-            <TagText>{props.nonDataCallType}</TagText>
+            <PillText>{props.nonDataCallType}</PillText>
           </SilentText>
         )}
       </TagsWrapper>
