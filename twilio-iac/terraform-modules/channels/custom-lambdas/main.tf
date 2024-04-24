67b6f68b746b6d16d2fec0c56f94d1bb98249492
Stephen Hand <steveh@techmatters.org>
2024-04-24 09:46:22 +0100

    Fix ssm parameter imports

--- twilio-iac/terraform-modules/channels/custom-lambdas/main.tf
+++ twilio-iac/terraform-modules/channels/custom-lambdas/main.tf
@@ -3,12 +3,13 @@ data "aws_ssm_parameter" "alb_http_listener_arn" {
 }
 
 module "message_lambdas" {
-  source = "../message-handler-lambda"
-  for_each = toset(var.message_handler_lambdas)
-  channel = var.channel
-  name = each.value
-  environment = var.environment
-  region = var.region
-  short_helpline = var.short_helpline
-  policy_template = ""
+  source           = "../message-handler-lambda"
+  for_each         = toset(var.message_handler_lambdas)
+  channel          = var.channel
+  name             = each.value
+  environment      = var.environment
+  region           = var.region
+  short_helpline   = var.short_helpline
+  policy_template  = ""
   alb_listener_arn = data.aws_ssm_parameter.alb_http_listener_arn.value
+}
