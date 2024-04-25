7965ab8e1bf4b6dfb081d875178cd4eec00fc935
Stephen Hand <steveh@techmatters.org>
2024-04-25 12:54:33 +0100

    E2E test fix

--- e2e-tests/hrm/hrmRequest.ts
+++ e2e-tests/hrm/hrmRequest.ts
@@ -20,11 +20,12 @@ import { getConfigValue, localOverrideEnv } from '../config';
 export const apiHrmRequest =
   (apiRequest: APIRequestContext, flexToken: string) =>
   async (hrmPath: string, method: 'get' | 'delete' | 'patch' = 'get', body?: any) => {
-    const hrmRoot =
+    const hrmRoot = `${
       (getConfigValue('hrmRoot') as string) ||
       `https://hrm-${localOverrideEnv}.tl.techmatters.org/v0/accounts/${getConfigValue(
         'twilioAccountSid',
-      )}`.toString();
+      )}`
+    }-aselo_test`;
     const resp = await apiRequest[method](new URL(`${hrmRoot}/${hrmPath}`).toString(), {
       data: body,
       headers: {
