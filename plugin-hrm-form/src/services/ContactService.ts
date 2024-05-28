0ad35b97f542b576870060935373a3143c0fad04
Stephen Hand <steveh@techmatters.org>
2024-05-28 11:30:00 +0100

    Use customChannelType in preference to channelType if it is set on the task attributes

--- plugin-hrm-form/src/services/ContactService.ts
+++ plugin-hrm-form/src/services/ContactService.ts
@@ -173,7 +173,7 @@ export const createContact = async (
   const { definitionVersion } = getHrmConfig();
   const contactForApi: Contact = {
     ...contact,
-    channel: task.channelType as Contact['channel'],
+    channel: (task.attributes?.channelType || task.channelType) as Contact['channel'],
     rawJson: {
       definitionVersion,
       ...contact.rawJson,
