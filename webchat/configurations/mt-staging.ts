ddd12294fcf262c22dba88c8be6c01baff18c50e
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2024-07-11 13:53:08 +0200

    Update Age and Gender

--- webchat/configurations/mt-staging.ts
+++ webchat/configurations/mt-staging.ts
@@ -99,7 +99,7 @@ const preEngagementConfig: PreEngagementFormDefinition = {
     {
       label: 'Age/Età/літа',
       type: 'select',
-      name: 'ageRange',
+      name: 'age',
       required: true,
       defaultValue: '',
       options: [
@@ -212,12 +212,192 @@ const preEngagementConfig: PreEngagementFormDefinition = {
           label: "25"
         },
         {
-          value: ">25",
-          label: ">25"
+          value: "26",
+          label: "26"
+        },
+        {
+          value: "27",
+          label: "27"
+        },
+        {
+          value: "28",
+          label: "28"
+        },
+        {
+          value: "29",
+          label: "29"
+        },
+        {
+          value: "30",
+          label: "30"
+        },
+        {
+          value: "31",
+          label: "31"
+        },
+        {
+          value: "32",
+          label: "32"
+        },
+        {
+          value: "33",
+          label: "33"
+        },
+        {
+          value: "34",
+          label: "34"
+        },
+        {
+          value: "35",
+          label: "35"
+        },
+        {
+          value: "36",
+          label: "36"
+        },
+        {
+          value: "37",
+          label: "37"
+        },
+        {
+          value: "38",
+          label: "38"
+        },
+        {
+          value: "39",
+          label: "39"
+        },
+        {
+          value: "40",
+          label: "40"
+        },
+        {
+          value: "41",
+          label: "41"
+        },
+        {
+          value: "42",
+          label: "42"
+        },
+        {
+          value: "43",
+          label: "43"
+        },
+        {
+          value: "44",
+          label: "44"
+        },
+        {
+          value: "45",
+          label: "45"
+        },
+        {
+          value: "46",
+          label: "46"
+        },
+        {
+          value: "47",
+          label: "47"
+        },
+        {
+          value: "48",
+          label: "48"
+        },
+        {
+          value: "49",
+          label: "49"
+        },
+        {
+          value: "50",
+          label: "50"
+        },
+        {
+          value: "51",
+          label: "51"
+        },
+        {
+          value: "52",
+          label: "52"
+        },
+        {
+          value: "53",
+          label: "53"
+        },
+        {
+          value: "54",
+          label: "54"
+        },
+        {
+          value: "55",
+          label: "55"
+        },
+        {
+          value: "56",
+          label: "56"
+        },
+        {
+          value: "57",
+          label: "57"
+        },
+        {
+          value: "58",
+          label: "58"
+        },
+        {
+          value: "59",
+          label: "59"
+        },
+        {
+          value: "60",
+          label: "60"
+        },
+        {
+          value: "61",
+          label: "61"
+        },
+        {
+          value: "62",
+          label: "62"
+        },
+        {
+          value: "63",
+          label: "63"
+        },
+        {
+          value: "64",
+          label: "64"
+        },
+        {
+          value: "65",
+          label: "65"
+        },
+        {
+          value: "66",
+          label: "66"
+        },
+        {
+          value: "67",
+          label: "67"
+        },
+        {
+          value: "68",
+          label: "68"
+        },
+        {
+          value: "69",
+          label: "69"
+        },
+        {
+          value: "70",
+          label: "70"
+        },
+        {
+          value: ">70",
+          label: ">70"
         },
         {
           value: "Unknown",
-          label: "Unknown"
+          label: "Rather not say"
         },
       ],
     },
