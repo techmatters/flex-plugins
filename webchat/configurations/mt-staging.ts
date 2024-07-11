9685ebf3fa43caf80b68bff113ba29d03cd8b23b
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2024-07-11 12:26:38 +0200

    Update mt-staging.ts
    Add Gender and Expanded Age options to Pre engagement form

--- webchat/configurations/mt-staging.ts
+++ webchat/configurations/mt-staging.ts
@@ -77,6 +77,19 @@ const preEngagementConfig: PreEngagementFormDefinition = {
   submitLabel: 'StartChat',
   fields: [
     {
+      type: 'select',
+      name: 'language',
+      label: 'Select your language',
+      defaultValue: '',
+      required: true,
+      options: [
+        { value: '', label: '' },
+        { value: 'en-MT', label: 'English' },
+        { value: 'mt-MT', label: 'Maltese' },
+        { value: 'ukr-MT', label: 'Ukrainian' },
+      ],
+    },
+    {
       type: 'input-text',
       name: 'friendlyName',
       label: 'Nickname',
@@ -95,13 +108,146 @@ const preEngagementConfig: PreEngagementFormDefinition = {
           label: ""
         },
         {
-          value: ">=16",
-          label: ">=16"
+          value: "00",
+          label: "0"
         },
         {
-          value: "<16",
-          label: "<16"
-        }
+          value: "01",
+          label: "1"
+        },
+        {
+          value: "02",
+          label: "2"
+        },
+        {
+          value: "03",
+          label: "3"
+        },
+        {
+          value: "04",
+          label: "4"
+        },
+        {
+          value: "05",
+          label: "5"
+        },
+        {
+          value: "06",
+          label: "6"
+        },
+        {
+          value: "07",
+          label: "7"
+        },
+        {
+          value: "08",
+          label: "8"
+        },
+        {
+          value: "09",
+          label: "9"
+        },
+        {
+          value: "10",
+          label: "10"
+        },
+        {
+          value: "11",
+          label: "11"
+        },
+        {
+          value: "12",
+          label: "12"
+        },
+        {
+          value: "13",
+          label: "13"
+        },
+        {
+          value: "14",
+          label: "14"
+        },
+        {
+          value: "15",
+          label: "15"
+        },
+        {
+          value: "16",
+          label: "16"
+        },
+        {
+          value: "17",
+          label: "17"
+        },
+        {
+          value: "18",
+          label: "18"
+        },
+        {
+          value: "19",
+          label: "19"
+        },
+        {
+          value: "20",
+          label: "20"
+        },
+        {
+          value: "21",
+          label: "21"
+        },
+        {
+          value: "22",
+          label: "22"
+        },
+        {
+          value: "23",
+          label: "23"
+        },
+        {
+          value: "24",
+          label: "24"
+        },
+        {
+          value: "25",
+          label: "25"
+        },
+        {
+          value: ">25",
+          label: ">25"
+        },
+        {
+          value: "Unknown",
+          label: "Unknown"
+        },
+      ],
+    },
+    {
+      label: 'Gender/Sess/Стать',
+      type: 'select',
+      name: 'gender',
+      required: true,
+      defaultValue: '',
+      options: [
+        {
+          value: "",
+          label: ""
+        },
+        {
+          value: 'Female',
+          label: 'Female/Mara/Жінка',
+        },
+        {
+          value: 'Male',
+          label: 'Male/Raġel/Чоловік',
+        },
+        {
+          value: 'Other',
+          label: 'Others/Oħrajn/Інші',
+        },
+        {
+          value: 'Unknown',
+          label: 'Rather not say/Ma nixtieqx naghti risposta/Не хочу відповідати',
+        },
       ],
     },
     {
