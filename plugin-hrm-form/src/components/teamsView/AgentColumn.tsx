bb41831ca246d03cfc4f9b1957f41e0367c16ac4
mythilytm <mythily@techmatters.org>
2024-08-22 19:47:10 -0400

    make worker name focusable

--- plugin-hrm-form/src/components/teamsView/AgentColumn.tsx
+++ plugin-hrm-form/src/components/teamsView/AgentColumn.tsx
@@ -72,13 +72,13 @@ const AgentCell = ({ item }) => {
   const fullName = item?.worker?.fullName ?? '';
 
   return (
-    <div style={{ marginRight: '4px', padding: '4px' }} role="button" tabIndex={0} aria-label={fullName}>
+    <div style={{ marginRight: '4px', padding: '4px' }}>
       {fullName.length > MAX_NAME_LENGTH ? (
         <Tooltip title={fullName} enterDelay={500} enterTouchDelay={500}>
-          <AgentFullName>{`${fullName.substring(0, MAX_NAME_LENGTH)}…`}</AgentFullName>
+          <AgentFullName role="button" tabIndex={0} aria-label={fullName}>{`${fullName.substring(0, MAX_NAME_LENGTH)}…`}</AgentFullName>
         </Tooltip>
       ) : (
-        <AgentFullName>{fullName}</AgentFullName>
+        <AgentFullName role="button" tabIndex={0} aria-label={fullName}>{fullName}</AgentFullName>
       )}
       <Labels />
     </div>
