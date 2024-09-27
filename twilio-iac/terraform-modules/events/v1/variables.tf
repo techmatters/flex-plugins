8f5032409dd8f2de6b862573fb53704eb393de08
janorivera <alnrivera@gmail.com>
2024-09-27 11:27:11 -0300

    Twilio Event Streams: Sink and subscription via TG (#2533)
    * adding new events module
    
    * update
    
    * update
    
    * updates
    
    * update
    
    * updates
    
    * more changes
    
    * change of logic
    
    * support for multiple subscriptions
    
    * updates
    
    * format
    
    * webhook url updates
    
    * support for multiple webhooks
    
    * updating defaults
    
    * back to default
    
    * task router sub
    
    * testing apply
    
    * different approach
    
    * json encode
    
    * jsonencode
    
    * updates
    
    * removing sinks
    
    * re-adding sinks
    
    * removing sink
    
    * readding sinks
    
    * Adding task router defaults
    
    * removing subscriptions
    
    * readding sink

---
+++ twilio-iac/terraform-modules/events/v1/variables.tf
@@ -0,0 +1,24 @@
+variable "short_environment" {
+  description = "Short upper case environment identifier, typically 'PROD', 'STG' or 'DEV'"
+  type        = string
+}
+
+variable "short_helpline" {
+  description = "Short (usually 2 letter) upper case code for helpline"
+  type        = string
+}
+
+variable "default_webhook_url" {
+  description = "The default webhook url."
+  type        = string
+}
+
+variable "subscriptions" {
+  type = map(object({
+    webhook_url         = optional(string),
+    events               = list(object({
+      type = string,
+      schema_version = optional(string)
+    }))
+  }))
+}
