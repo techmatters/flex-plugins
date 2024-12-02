18bd5ef4f392dc183c05b8a9b5eb874e58f04398
Stephen Hand <steveh@techmatters.org>
2024-12-02 14:44:01 +0000

    Start adding fat lambda with routing / pipelining support

---
+++ lambdas/twilio-account-scoped/src/Result.ts
@@ -0,0 +1,130 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+type ResultBase = {
+  readonly _tag: 'Result';
+};
+
+type NewErrorResultParams<TError> = {
+  message: string;
+  error: TError;
+  extraProperties?: Record<string, any>;
+};
+
+export type ErrorResult<TError> = ResultBase & {
+  status: 'error';
+  message: string;
+  error: TError;
+  readonly unwrap: () => never;
+};
+
+class ResultError<T extends ErrorResult<any>> extends Error {
+  errorResult: T;
+
+  constructor(public result: T) {
+    super(result.message);
+    this.errorResult = result;
+  }
+}
+
+export const newErr = <TError>({
+  message,
+  error,
+}: NewErrorResultParams<TError>): ErrorResult<TError> => {
+  return {
+    _tag: 'Result',
+    status: 'error',
+    message,
+    error,
+    unwrap: function (this: ErrorResult<TError>) {
+      throw new ResultError(this);
+    },
+  };
+};
+
+type AsyncResultTypeFunction<TError extends ErrorResult<any>, TData> = (
+  ...p: any[]
+) => Promise<Result<TError, TData>>;
+
+type AsyncResultTypeFunctionWrapper<TError extends ErrorResult<any>, TData> = (
+  workFunc: AsyncResultTypeFunction<TError, TData>,
+) => Promise<Result<TError, TData>>;
+
+/**
+ * Wraps a function that takes an async worker function to execute.
+ * When said function is given a Result<> returning function, it will detect ErrorResults and throw them in the context of the executing function we are wrapping
+ * Then it will catch the error once it bubbles through the executing function, rewrap it as an ErrorResult and return it.
+ * This is primarily useful for pg promise functions, where automatic transaction management is based around promise resolution / rejection.
+ * You can use this function to wrap the pg-promise function so that it sees the error result or the worker as a rejection and rolls back accordingly, but your own code can still use the ErrorResult
+ * e.g.:
+ * ```typescript
+ * const TResult<'QUERY_FAILED', string>ensureRejection(tx.one(conn => {
+ *   try {
+ *     await conn.query('SELECT * FROM table');
+ *     return newOkFromData('success');
+ *   } catch (err) {
+ *     return newErr({ message: 'Failed to query', error: 'QUERY_FAILED' });
+ *   }
+ * });
+ * ```
+ * The above function will pass a rejection to the pg-promise transaction manager if the query fails, but will still return an ErrorResult to the caller.
+ * @throws ResultError
+ */
+export const ensureRejection = <TError extends ErrorResult<any>, TData>(
+  inputWrapper: AsyncResultTypeFunctionWrapper<TError, TData>,
+): AsyncResultTypeFunctionWrapper<TError, TData> => {
+  return async (workFunc: AsyncResultTypeFunction<TError, TData>) => {
+    try {
+      let errorUnwrapper: typeof workFunc = async (...p: Parameters<typeof workFunc>) => {
+        const initialWorkResult = await workFunc(...p);
+        initialWorkResult.unwrap(); // Will throw an error if it's an error result
+        return initialWorkResult;
+      };
+      return await inputWrapper(errorUnwrapper);
+    } catch (e) {
+      if (e instanceof ResultError<TError>) {
+        return e.errorResult;
+      }
+      throw e;
+    }
+  };
+};
+
+type SuccessResult<TData> = ResultBase & {
+  status: 'success';
+  data: TData;
+  readonly unwrap: () => TData;
+};
+
+export const newOk = <TData>(data: TData): SuccessResult<TData> => ({
+  _tag: 'Result',
+  status: 'success',
+  data,
+  unwrap: () => data,
+});
+
+export type Result<TErrorResult extends ErrorResult<any>, TData> =
+  | TErrorResult
+  | SuccessResult<TData>;
+
+const isResult = (r: unknown): r is Result<any, any> => (r as any)?._tag === 'Result';
+
+export const isErr = <TError extends ErrorResult<any>>(
+  result: Result<ErrorResult<any>, any>,
+): result is TError => isResult(result) && result.status === 'error';
+
+export const isOk = <TData>(result: Result<any, TData>): result is SuccessResult<TData> =>
+  isResult(result) && result.status === 'success';
---
+++ lambdas/twilio-account-scoped/src/router.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import { ErrorResult, newErr, Result } from './Result';
+
+/**
+ * Super simple router sufficient for directly ported Twilio Serverless functions
+ * All endpoints are POSTS and have no variables in their paths or query strings, everything is in the request body
+ * Therefore a simple map of path -> handler will suffice
+ * We should evolve these endpoints to be, dare I say it, more RESTful in the future.
+ * At that point we should decide whether to evolve this router or replace it with a 3rd party one
+ */
+
+export type HttpError = {
+  statusCode: number;
+  cause?: Error;
+};
+
+type HttpRequest = {
+  method: string;
+  path: string;
+  body: any;
+};
+
+export type Handler = (
+  event: HttpRequest,
+) => Promise<Result<ErrorResult<HttpError>, any>>;
+
+type PipelineStep<Item> = (item: Item) => Promise<Result<ErrorResult<HttpError>, Item>>;
+
+export type Route = {
+  requestPipeline: PipelineStep<HttpRequest>[];
+  handler: Handler;
+};
+
+const ROUTES: Record<string, Route> = {
+  '/custom-channel/line/flex-to-native': {
+    requestPipeline: [],
+    handler: async (event: HttpRequest) =>
+      newErr({
+        message: `Not implemented: ${event.method} - ${event.path}`,
+        error: {
+          statusCode: 405,
+        },
+      }),
+  },
+};
+
+export const lookupRoute = (event: HttpRequest): Route => ROUTES[event.path];
