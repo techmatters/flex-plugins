9d8f28be826578d854175799558d8d14890463dc
mythilytm <mythily@techmatters.org>
2025-01-08 15:57:22 -0500

    add a modal for queues

--- plugin-hrm-form/src/components/queuesView/SwitchboardingCard.tsx
+++ plugin-hrm-form/src/components/queuesView/SwitchboardingCard.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 import React, { useState } from 'react';
 import { QueuesStats, Template, Manager } from '@twilio/flex-ui';
 import { FormLabel, FormGroup, FormControlLabel, FormControl, FormHelperText, Switch } from '@material-ui/core';
@@ -16,9 +32,11 @@ export const setUpSwitchboarding = () => {
 
 const SwitchboardingTile = () => {
   const [isSwitchboarding, setIsSwitchboarding] = useState(false);
+  const [openQueuesModal, setOpenQueuesModal] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsSwitchboarding(!isSwitchboarding);
+    // setIsSwitchboarding(!isSwitchboarding);
+    setOpenQueuesModal(!openQueuesModal);
   };
 
   const renderSwitch = () => (
@@ -40,6 +58,24 @@ const SwitchboardingTile = () => {
   return (
     <>
       <Box>{renderSwitch()}</Box>
+      {openQueuesModal && (
+        <dialog>
+          <h1>Queues</h1>
+          <ul>
+            {Array.isArray(queues) &&
+              queues.map((queue: any) => (
+                <li key={queue.sid}>
+                  <span>{queue.friendlyName}</span>
+                  <span>{queue.sid}</span>
+                  <span>{queuesConfig[queue.sid]?.config?.targetWorkersExpression}</span>
+                </li>
+              ))}
+          </ul>
+          <button type="button" onClick={() => setOpenQueuesModal(false)}>
+            Close
+          </button>
+        </dialog>
+      )}
     </>
   );
 };
