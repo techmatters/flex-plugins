70440b90d4cd0e90bc1c123adf7cae69d7630379
janorivera <alnrivera@gmail.com>
2025-01-24 17:28:25 -0300

    lex bot v2

--- twilio-iac/terraform-modules/lex/v2/main.tf
+++ twilio-iac/terraform-modules/lex/v2/main.tf
@@ -1,25 +1,45 @@
 terraform {
   required_providers {
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = "0.52.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 }
 
-data "aws_caller_identity" "current" {}
-
-resource "awscc_lex_bot" "this" {
-  name = "${var.environment}-${var.helpline}"
-  # role_arn = aws_iam_service_linked_role.lex_service_role.arn
-  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/lexv2.amazonaws.com/AWSServiceRoleForLexV2Bots"
-  data_privacy = {
-    child_directed = var.lex_config.child_directed
+resource "aws_lexv2models_bot" "test_bot" {
+  name        = "example"
+  description = "Example description"
+  data_privacy {
+    child_directed = false
   }
-  idle_session_ttl_in_seconds = var.lex_config.idle_session_ttl_in_seconds
-  bot_locales                 = var.lex_config.bot_locales
+  idle_session_ttl_in_seconds = 60
+  role_arn                    = aws_iam_role.test_role.arn
+  type                        = "Bot"
+
+  tags = {
+    foo = "bar"
+  }
 }
 
-output "lex_config" {
-  value = var.lex_config
+resource "aws_iam_role" "test_role" {
+  name = "example"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lexv2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    created_by = "aws"
+  }
 }
