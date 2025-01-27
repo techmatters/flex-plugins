33cc073907c6db53cedd41d338d61339905e55b0
janorivera <alnrivera@gmail.com>
2025-01-27 17:54:00 -0300

    updates

--- twilio-iac/terraform-modules/lex/v2/main.tf
+++ twilio-iac/terraform-modules/lex/v2/main.tf
@@ -15,10 +15,10 @@ data "aws_iam_role" "role-lex-v2-bot" {
   name = "lex-v2-bot"
 }
 
-resource "aws_lexv2models_bot" "test_bot" {
+resource "aws_lexv2models_bot" "this" {
   for_each = var.bots
   name     = replace("${local.name_prefix}_${each.key}", "2", "")
-  description = "Example description"
+  description = each.value.description
   data_privacy {
     child_directed = true
   }
@@ -27,6 +27,30 @@ resource "aws_lexv2models_bot" "test_bot" {
   type                        = "Bot"
 
   tags = {
-    foo = "bar"
+    foo = "terraform"
   }
+}
+
+resource "aws_lexv2models_bot_locale" "this" {
+  for_each                         = var.bots
+  bot_id                           = aws_lexv2models_bot.this["${each.key}"].id
+  bot_version                      = "DRAFT"
+  locale_id                        = "en_US"
+  n_lu_intent_confidence_threshold = 0.70
+}
+
+resource "aws_lexv2models_bot_version" "this" {
+  bot_id = aws_lexv2models_bot.this["${each.key}"].id
+  locale_specification = {
+    "en_US" = {
+      source_bot_version = "DRAFT"
+    }
+  }
+}
+
+resource "aws_lexv2models_intent" "example" {
+  bot_id      = aws_lexv2models_bot.this["${each.key}"].id
+  bot_version = aws_lexv2models_bot_locale.this.bot_version
+  name        = 
+  locale_id   = aws_lexv2models_bot_locale.this.locale_id
 }
