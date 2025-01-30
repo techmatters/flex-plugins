de78615bc018ca5a9b54e3ac22d40e00fdc5f0ed
janorivera <alnrivera@gmail.com>
2025-01-30 17:53:04 -0300

    fix

--- twilio-iac/terraform-modules/stages/lex/main.tf
+++ twilio-iac/terraform-modules/stages/lex/main.tf
@@ -37,7 +37,7 @@ module "lex_v2" {
   language       = each.key
 
   lex_v2_bots = var.lex_v2_bots[each.key]
-  lex_v2_intents    = var.lex_intents[each.key]
+  lex_v2_intents    = var.lex_v2_intents[each.key]
   //lex_v2_slots = var.lex_slots[each.key]
   lex_v2_slot_types = var.lex_v2_slot_types[each.key]
 }
