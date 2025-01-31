c1642582688cf232c77b51a8fdd6c515b55f5abf
mythilytm <mythily@techmatters.org>
2025-01-30 20:10:57 -0500

    permissions flag

--- plugin-hrm-form/src/HrmFormPlugin.tsx
+++ plugin-hrm-form/src/HrmFormPlugin.tsx
@@ -179,8 +179,6 @@ export default class HrmFormPlugin extends FlexPlugin {
   async init(flex: typeof Flex, manager: Flex.Manager) {
     loadCSS('https://use.fontawesome.com/releases/v5.15.4/css/solid.css');
 
-    await validateAndSetPermissionRules();
-
     setUpMonitoring(manager.workerClient, manager.serviceConfiguration);
 
     console.log(`Welcome to ${PLUGIN_NAME}`);
@@ -191,6 +189,7 @@ export default class HrmFormPlugin extends FlexPlugin {
     const config = getHrmConfig();
     const featureFlags = getAseloFeatureFlags();
 
+    await validateAndSetPermissionRules();
     /*
      * localization setup (translates the UI if necessary)
      * WARNING: the way this is done right now is "hacky". More info in initLocalization declaration
