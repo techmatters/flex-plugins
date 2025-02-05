6a11c77340a70d68b06ab78896b1ce296f343b91
janorivera <alnrivera@gmail.com>
2025-02-05 17:22:32 -0300

    test

--- twilio-iac/terraform-modules/lex/v2/main.tf
+++ twilio-iac/terraform-modules/lex/v2/main.tf
@@ -429,7 +429,9 @@ resource "null_resource" "add_intent_closing_response" {
         --intent-id ${split(":", aws_lexv2models_intent.this["${each.value.bot_name}_${each.value.config.intentName}"].id)[0]} \
         --intent-name ${each.value.config.intentName} \
         ${each.value.config.intentClosingSetting != null ? "--intent-closing-setting '${jsonencode(each.value.config.intentClosingSetting)}'" : ""} \
+        ${each.value.config.initialResponseSetting != null ? "--initial-response-setting '${jsonencode(each.value.config.initialResponseSetting)}'" : ""} \
         ${each.value.config.sampleUtterances != null ? "--sample-utterances '${jsonencode(each.value.config.sampleUtterances)}'" : ""} 
+        
         EOT
     }
     depends_on = [
