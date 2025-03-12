759a2b7b57b38895127b5e20156b3c92f833cba1
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2025-03-12 18:15:53 -0300

    chore: lint

--- plugin-hrm-form/src/components/customIntegrations/uscr/styles.tsx
+++ plugin-hrm-form/src/components/customIntegrations/uscr/styles.tsx
@@ -27,24 +27,5 @@ const styleIcon = withStyles({
   },
 });
 
-export const SendIcon = styleIcon(Send);
-SendIcon.displayName = 'SendIcon';
 export const CheckCircleIcon = styleIcon(CheckCircle);
 CheckCircleIcon.displayName = 'CheckCircleIcon';
-
-export const Button = styled('button')`
-  display: flex;
-  align-items: center;
-  padding: 6px 12px;
-  font-weight: 600;
-  font-family: 'Open Sans';
-  font-size: 13px;
-  background-color: #ecedf1;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #d8d8d8;
-  }
-`;
