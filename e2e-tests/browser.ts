c2cc5cd1201fa6fa8ea601004a09fc9ec16f53fd
Stephen Hand <steveh@techmatters.org>
2025-06-25 09:19:17 +0100

    Linter

--- e2e-tests/browser.ts
+++ e2e-tests/browser.ts
@@ -61,7 +61,6 @@ export const setupContextAndPage = async (browser: Browser): Promise<SetupPageRe
       `Visited /${process.env.TWILIO_RUNTIME_DOMAIN} - waiting for logged in page element to load`,
     );
   } else {
-
     await page.goto(`/`, { waitUntil: 'domcontentloaded' });
   }
   // There are multiple elements so we need to use waitForSelector instead of a locator/waitFor
