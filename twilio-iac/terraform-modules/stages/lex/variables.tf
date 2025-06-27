8f23745d1cdf948317e30f52f61dfe62a1b514bd
janorivera <alnrivera@gmail.com>
2025-06-27 17:08:09 -0400

    production version

--- twilio-iac/terraform-modules/stages/lex/variables.tf
+++ twilio-iac/terraform-modules/stages/lex/variables.tf
@@ -90,64 +90,6 @@ variable "lex_bots" {
   })))
 }
 
-// Used for lex v2 via awscc leaving here for now
-variable "lex_v2_config" {
-  type = object({
-    child_directed              = optional(bool, true),
-    idle_session_ttl_in_seconds = optional(number, 1500),
-    bot_locales = list(object({
-      locale_id                = string,
-      nlu_confidence_threshold = number,
-      slot_types = list(object({
-        name = string,
-        slot_type_values = list(object({
-          sample_value = optional(object({
-            value    = string
-            synonyms = optional(list(string), null)
-          }), null),
-        })),
-        value_selection_setting = object({
-          resolution_strategy = string
-        })
-      })),
-      intents = list(object({
-        name        = string,
-        description = string,
-        sample_utterances = list(object({
-          utterance = string
-        })),
-        slot_priorities = list(object({
-          priority  = number,
-          slot_name = string
-        })),
-        slots = list(object({
-          name           = string,
-          description    = optional(string, null),
-          slot_type_name = string,
-          obfuscation_setting = optional(object({
-            obfuscation_setting_type = string,
-          }), null),
-          value_elicitation_setting = optional(object({
-            slot_constraint = string,
-            prompt_specification = optional(object({
-              max_retries = number,
-              message_groups_list = list(object({
-                message = object({
-                  plain_text_message = object({
-                    value = string
-                  })
-                })
-              }))
-            }), null)
-          }), null)
-        }))
-      }))
-    }))
-  })
-
-  default = null
-}
-
 variable "enable_lex_v2" {
   description = "Flag to enable or disable the lex_v2 module"
   type        = bool
@@ -162,6 +104,7 @@ variable "lex_v2_bots" {
     child_directed              = optional(bool, true)
     idle_session_ttl_in_seconds = optional(number, 300)
     type                        = optional(string, "Bot")
+    production_bot_alias_id     = optional(string, "TSTALIASID")
   })))
   default = null
   }
