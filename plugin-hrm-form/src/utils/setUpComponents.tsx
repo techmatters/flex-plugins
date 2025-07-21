d20c3dad03d8e2b6200cf80e84fc1047aee8595b
Stephen Hand <steveh@techmatters.org>
2025-07-21 16:11:38 +0100

    Reenable lex v2 jm stg, remove unused feature flags from flex and terraform

--- plugin-hrm-form/src/utils/setUpComponents.tsx
+++ plugin-hrm-form/src/utils/setUpComponents.tsx
@@ -44,7 +44,6 @@ import { Container } from '../components/queuesStatus/styles';
 import { FeatureFlags, standaloneTaskSid } from '../types/types';
 import { colors } from '../channels/colors';
 import { getHrmConfig } from '../hrmConfig';
-import { AseloMessageInput, AseloMessageList } from '../components/AseloMessaging';
 import { changeRoute } from '../states/routing/actions';
 import { AppRoutes, ChangeRouteMode } from '../states/routing/types';
 import { selectCurrentBaseRoute } from '../states/routing/getRoute';
@@ -126,7 +125,9 @@ export const setUpQueuesStatus = (setupObject: SetupObject) => {
 const setUpManualPulling = () => {
   const manager = Flex.Manager.getInstance();
 
-  const [, chatChannel] = Array.from(manager.workerClient.channels).find(c => c[1].taskChannelUniqueName === 'chat');
+  const [, chatChannel] = Object.entries(manager.workerClient.channels).find(
+    c => c[1].taskChannelUniqueName === 'chat',
+  );
 
   manager.store.dispatch(chatCapacityUpdated(chatChannel.capacity));
   (chatChannel as any).on('capacityUpdated', channel => {
@@ -362,14 +363,6 @@ export const removeActionsIfTransferring = () => {
   Flex.ParticipantCanvas.Content.remove('actions', {
     if: props => hasNoControlAndIsWarm(props.task) && props.participant.participantType === 'worker',
   });
-};
-
-/**
- *
- */
-export const replaceTwilioMessageInput = () => {
-  Flex.MessageInputV2.Content.replace(<AseloMessageInput key="textarea" />, { sortOrder: -1 });
-  Flex.MessageList.Content.replace(<AseloMessageList key="list" />);
 };
 
 /**
