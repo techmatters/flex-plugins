a0a41296ce5bbfce93ba138772817fab9bb07ebb
Sinekhaya <sinekhaya@techmatters.org>
2025-08-20 12:47:03 +0200

    more changes

--- webchat/configurations/ukmh-staging.ts
+++ webchat/configurations/ukmh-staging.ts
@@ -167,7 +167,7 @@ const preEngagementConfig: PreEngagementFormDefinition = {
       ],
     },
     {
-      type: 'select',
+      type: 'checkbox',
       name: 'termsAndConditions',
       label:
         'I have read terms of use and the <a href="https://www.themix.org.uk/about-us/privacy-centre/privacy-policy/">privacy policy</a> and agree to them.',
@@ -175,21 +175,6 @@ const preEngagementConfig: PreEngagementFormDefinition = {
         value: true,
         message: "Sorry, if you don't accept our terms and privacy policy we can't provide counselling to you.",
       },
-      defaultValue: '',
-      options: [
-        {
-          value: "",
-          label: ""
-        },
-        {
-          value: 'Yes',
-          label: 'Yes',
-        },
-        {
-          value: 'No',
-          label: 'No',
-        },
-      ],
     },
     {
       type: 'checkbox',
