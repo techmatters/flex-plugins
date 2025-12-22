f4630b121bb60f69e22064f2a377276a2abcdb91
Stephen Hand <steveh@techmatters.org>
2025-12-22 13:19:44 +0000

    Licence headers

--- aselo-webchat-react-app/cypress/support/commands/webchatSessionCookie.ts
+++ aselo-webchat-react-app/cypress/support/commands/webchatSessionCookie.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2021-2026 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 const LOCAL_STORAGE_ITEM_ID = "TWILIO_WEBCHAT_WIDGET";
 let webchatSessionCookie;
 const error = `It looks like you're trying to restore a webchat session, but there isn't one.
--- aselo-webchat-react-app/src/utils/roundFileSizeInMB.ts
+++ aselo-webchat-react-app/src/utils/roundFileSizeInMB.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2021-2026 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 /*
  * Rounds filesize in bytes to megabytes, with 0-2 decimal places ensuring atleast 3 digits.
  * e.g. 100MB, 10.0MB, 1.00MB, 0.10MB, 0.01MB
