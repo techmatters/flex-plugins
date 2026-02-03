61f1bc90a0b214df57b11e6052dba3a5deead5d6
Sinekhaya <sinekhaya@techmatters.org>
2026-02-03 14:50:23 +0200

    webchat

---
+++ webchat/configurations/nzba-staging.ts
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import { Translations, Configuration, MapHelplineLanguage, ContactType } from '../types';
+import type { PreEngagementFormDefinition } from '../src/pre-engagement-form';
+
+const accountSid = 'AC574591f522bc6e981aea7d34fb14fa5e';
+const flexFlowSid = 'FO72895d84c5d2136ef19d0afc50a59f39';
+const defaultLanguage = 'en-NZ';
+const captureIp = true;
+const contactType: ContactType = 'ip';
+
+const translations: Translations = {
+  'en-US': {
+    WelcomeMessage: 'Barnardos Aotearoa',
+    MessageCanvasTrayContent: '',
+    MessageInputDisabledReasonHold:
+      "Thank you very much for this information. We'll transfer you now. Please hold for a counsellor.",
+    AutoFirstMessage: 'Incoming webchat contact from',
+    PreEngagementDescription:
+      'Thank you for contacting Barnardos Aotearoa. To chat with a counsellor, please type your name and select the Start Chat button.',
+    WhatIsYourName: 'What is your name?',
+    StartChat: 'Start Chat!',
+  },
+};
+
+const preEngagementConfig: PreEngagementFormDefinition = {
+  description: 'PreEngagementDescription',
+  submitLabel: 'StartChat',
+  fields: [
+    {
+      type: 'input-text',
+      name: 'friendlyName',
+      label: 'WhatIsYourName',
+      placeholder: 'Insert your name',
+      required: true,
+    },
+  ],
+};
+
+const mapHelplineLanguage: MapHelplineLanguage = (helpline) => {
+  // eslint-disable-next-line sonarjs/no-small-switch
+  switch (helpline) {
+    default:
+      return defaultLanguage;
+  }
+};
+
+const memberDisplayOptions = {
+  yourDefaultName: 'You',
+  yourFriendlyNameOverride: false,
+  theirFriendlyNameOverride: false,
+  theirDefaultName: 'Barnardos Aotearoa Counsellor',
+};
+
+export const config: Configuration = {
+  accountSid,
+  flexFlowSid,
+  defaultLanguage,
+  translations,
+  preEngagementConfig,
+  mapHelplineLanguage,
+  memberDisplayOptions,
+  captureIp,
+  contactType,
+  twilioServicesUrl: new URL(`https://hrm-staging.tl.techmatters.org/lambda/twilio/account-scoped/${accountSid}`),
+};
