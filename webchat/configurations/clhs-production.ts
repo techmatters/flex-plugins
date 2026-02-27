bfa3fa5131afcd6116dd3daaf7e82c922a947fe8
janorivera <alnrivera@gmail.com>
2026-02-27 15:40:54 -0300

    update

--- webchat/configurations/clhs-production.ts
+++ webchat/configurations/clhs-production.ts
@@ -22,7 +22,7 @@ const flexFlowSid = 'FO37ff9d5f1bb8f9bd0b2969013c73b54f';
 
 export const config: Configuration = {
   ...clhsStaging,
-  checkOpenHours: false,
+  checkOpenHours: true,
   accountSid,
   flexFlowSid,
   twilioServicesUrl: new URL(`https://hrm-production.tl.techmatters.org/lambda/twilio/account-scoped/${accountSid}`),
