ab081a73e85383c52115c72327671ec768a5c8d8
janorivera <alnrivera@gmail.com>
2026-03-16 14:40:51 -0300

    fix

--- webchat/configurations/usnc-staging.ts
+++ webchat/configurations/usnc-staging.ts
@@ -37,7 +37,7 @@ const holidayHours: PreEngagementFormDefinition = {
 };
 
 const preEngagementConfig: PreEngagementFormDefinition = {
-  description: "PreEngagementDescription",
+  description: "PreEngagementConfigDescription",
   submitLabel: "LetsChat",
   fields: [
     {
