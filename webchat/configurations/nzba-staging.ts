d6bc6fd20fd57fd98c1dc0868cc98827799d68fb
sinehome <125361122+sinehome@users.noreply.github.com>
2026-04-23 17:33:37 +0200

    contact form (#4120)
    * contact form
    
    * case forms
    
    * camel case
    
    * doc
    
    * call types
    
    * update categories
    
    * studio-webchat
    
    * pre-survey
    
    * flow
    
    * correction
    
    * workflow
    
    * move
    
    * fix workflow

--- webchat/configurations/nzba-staging.ts
+++ webchat/configurations/nzba-staging.ts
@@ -15,7 +15,7 @@
  */
 
 import { Translations, Configuration, MapHelplineLanguage, ContactType } from '../types';
-import type { PreEngagementFormDefinition } from '../src/pre-engagement-form';
+import { EMAIL_PATTERN, PreEngagementFormDefinition } from '../src/pre-engagement-form';
 
 const accountSid = 'AC574591f522bc6e981aea7d34fb14fa5e';
 const flexFlowSid = 'FO72895d84c5d2136ef19d0afc50a59f39';
@@ -44,34 +44,188 @@ const preEngagementConfig: PreEngagementFormDefinition = {
     {
       type: 'input-text',
       name: 'firstName',
-      label: 'First Name',
+      label: 'Name (and pronouns)',
       required: true,
     },
     {
-      label: 'How urgent is your situation?',
+      label: 'Age',
       type: 'select',
-      name: 'urgencyLevel',
-      required: false,
+      name: 'age',
+      required: true,
       defaultValue: '',
       options: [
-        { 'value': '', 'label': '' },
-        { 'value': 'Urgent', 'label': 'Urgent' },
-        { 'value': 'Critical', 'label': 'Critical' },
-        { 'value': 'Non-critical', 'label': 'Non-critical' },
-        { 'value': 'Other', 'label': 'Other' }
+        {
+          value: "",
+          label: ""
+        },
+        {
+          value: "05",
+          label: "5"
+        },
+        {
+          value: "06",
+          label: "6"
+        },
+        {
+          value: "07",
+          label: "7"
+        },
+        {
+          value: "08",
+          label: "8"
+        },
+        {
+          value: "09",
+          label: "9"
+        },
+        {
+          value: "10",
+          label: "10"
+        },
+        {
+          value: "11",
+          label: "11"
+        },
+        {
+          value: "12",
+          label: "12"
+        },
+        {
+          value: "13",
+          label: "13"
+        },
+        {
+          value: "14",
+          label: "14"
+        },
+        {
+          value: "15",
+          label: "15"
+        },
+        {
+          value: "16",
+          label: "16"
+        },
+        {
+          value: "17",
+          label: "17"
+        },
+        {
+          value: "18",
+          label: "18"
+        },
+        {
+          value: "19",
+          label: "19"
+        },
+        {
+          value: "20",
+          label: "20"
+        },
+        {
+          value: "21",
+          label: "21"
+        },
+        {
+          value: "22",
+          label: "22"
+        },
+        {
+          value: "23",
+          label: "23"
+        },
+        {
+          value: "24",
+          label: "24"
+        },
+        {
+          value: "25",
+          label: "25"
+        },
+        {
+          value: ">25 (Adult)",
+          label: ">25 (Adult)"
+        }
       ],
     },
     {
-      type: 'checkbox',
-      name: 'termsAndConditions',
-      label:
-        'I\'ve read and accept the <a href="https://en.wikipedia.org/wiki/Terms_of_service">Terms and Conditions</a>',
-      required: {
-        value: true,
-        message: "Sorry, if you don't accept our terms and conditions we can't provide counselling to you.",
-      }, 
+      type: 'input-text',
+      name: 'personalEmail',
+      label: 'Email Address',
+      required: true,
+      placeholder: 'Email Address',
+      pattern: {
+        value: EMAIL_PATTERN,
+        message: 'Please enter valid email address',
+      },
     },
-   
+    {
+      type: 'input-text',
+      name: 'howDidYouHearAboutWhatsUp',
+      label: "How did you hear about What's Up?",
+    },
+    {
+      label: 'What would you like to talk about?',
+      type: 'select',
+      name: 'topic',
+      required: true,
+      defaultValue: '',
+      options: [
+        { value: "", label: "" }, 
+        { value: "I just want to talk", label: "I just want to talk" }, 
+        { value: "I'm angry", label: "I'm angry" }, 
+        { value: "I'm ashamed", label: "I'm ashamed" }, 
+        { value: "I'm happy", label: "I'm happy" }, 
+        { value: "I'm lonely", label: "I'm lonely" }, 
+        { value: "I'm sad", label: "I'm sad" }, 
+        { value: "I'm scared", label: "I'm scared" }, 
+        { value: "I'm thinking about suicide", label: "I'm thinking about suicide" }, 
+        { value: "I'm unsafe", label: "I'm unsafe" }, 
+        { value: "I'm worried", label: "I'm worried" }, 
+        { value: "Something in the News", label: "Something in the News" }, 
+        { value: "Unsure", label: "Unsure" }
+      ],
+    },
+    {
+      label: 'What is your Ethnicity/cultural background?',
+      type: 'select',
+      name: 'ethnicity1',
+      required: true,
+      defaultValue: '',
+      options: [
+        { value: "", label: "" }, { value: "European (Unspecified)", label: "European (Unspecified)" }, { value: "New Zealand European", label: "New Zealand European" }, { value: "British and Irish", label: "British and Irish" }, { value: "Dutch", label: "Dutch" }, { value: "Greek", label: "Greek" }, { value: "Polish", label: "Polish" }, { value: "South Slav", label: "South Slav" }, { value: "Italian", label: "Italian" }, { value: "German", label: "German" }, { value: "Australian", label: "Australian" }, { value: "Other European", label: "Other European" }, { value: "Māori", label: "Māori" }, { value: "Pacific Peoples (unspecified)", label: "Pacific Peoples (unspecified)" }, { value: "Samoan", label: "Samoan" }, { value: "Cook Islands Maori", label: "Cook Islands Maori" }, { value: "Tongan", label: "Tongan" }, { value: "Niuean", label: "Niuean" }, { value: "Tokelauan", label: "Tokelauan" }, { value: "Fijian", label: "Fijian" }, { value: "French Polynesian", label: "French Polynesian" }, { value: "Indigenous Australian", label: "Indigenous Australian" }, { value: "Kiribati", label: "Kiribati" }, { value: "Micronesian", label: "Micronesian" }, { value: "Papua New Guinea", label: "Papua New Guinea" }, { value: "Solomon Islander", label: "Solomon Islander" }, { value: "Tuvalu Islander", label: "Tuvalu Islander" }, { value: "Other Pacific Peoples", label: "Other Pacific Peoples" }, { value: "Asian (unspecified)", label: "Asian (unspecified)" }, { value: "Southeast Asian (unspecified)", label: "Southeast Asian (unspecified)" }, { value: "Filipino", label: "Filipino" }, { value: "Cambodian", label: "Cambodian" }, { value: "Vietnamese", label: "Vietnamese" }, { value: "Other Southeast Asian", label: "Other Southeast Asian" }, { value: "Chinese", label: "Chinese" }, { value: "Indian", label: "Indian" }, { value: "Sri Lankan", label: "Sri Lankan" }, { value: "Japanese", label: "Japanese" }, { value: "Korean", label: "Korean" }, { value: "Other Asian", label: "Other Asian" }, { value: "Middle Eastern", label: "Middle Eastern" }, { value: "Latin American", label: "Latin American" }, { value: "African", label: "African" }, { value: "Indian Ocean Peoples", label: "Indian Ocean Peoples" }, { value: "Indigenous American", label: "Indigenous American" }, { value: "Other Ethnicity", label: "Other Ethnicity" }, { value: "Not Stated", label: "Not Stated" }
+      ],
+    },
+    {
+      type: 'input-text',
+      name: 'feelingRightNow',
+      label: 'How are you feeling right now? (0 = calm / okay → 10 = extremely upset or distressed)',
+    },
+    {
+      label: 'In the past year, have you been on a waitlist for 1 month or longer for professional mental health services?',
+      type: 'select',
+      name: 'onWaitinglist',
+      required: true,
+      defaultValue: '',
+      options: [
+        { value: "", label: "" }, 
+        { value: "Yes", label: "Yes" }, 
+        { value: "No", label: "No" }
+      ],
+    },
+    {
+      label: 'Is this the first time you have chatted with us?',
+      type: 'select',
+      name: 'firstTime',
+      required: true,
+      defaultValue: '',
+      options: [
+        { value: "", label: "" }, 
+        { value: "Yes", label: "Yes" }, 
+        { value: "No", label: "No" }
+      ],
+    },
+
   ],
 };
 
