f1144a11b4ba32f31f9e982a5e52bc78c5819ffe
Stephen Hand <steveh@techmatters.org>
2026-07-22 19:26:14 +0100

    First passing voice E2E test

--- e2e-tests/formContentsByHelpline.ts
+++ e2e-tests/formContentsByHelpline.ts
@@ -14,6 +14,7 @@
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 
+
 export const formContentsByHelpline = {
   e2e: {
     childInformation: {
@@ -27,7 +28,7 @@ export const formContentsByHelpline = {
       Accessibility: ['Education'],
     },
     caseInformation: {
-      callSummary: 'E2E TEST CALL',
+      callSummary: 'E2E TEST PREPOPULATED FORM',
     },
   },
   ca: {
@@ -51,6 +52,22 @@ export const formContentsByHelpline = {
     },
     caseInformation: {
       contactType: 'Testing',
+    },
+  },
+};
+
+export const formContentsByHelplineForEmptyForm = {
+  ...formContentsByHelpline,
+  e2e: {
+    ...formContentsByHelpline.e2e,
+    childInformation: {
+      ...formContentsByHelpline.e2e.childInformation,
+
+      gender: 'Unknown',
+      age: 'Unknown',
+    },
+    caseInformation: {
+      callSummary: 'E2E TEST EMPTY FORM',
     },
   },
 };
