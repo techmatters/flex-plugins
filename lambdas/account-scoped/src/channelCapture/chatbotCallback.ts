ef600940202f4e58d105932f6211082c8e68e786
janorivera <alnrivera@gmail.com>
2026-08-13 15:31:47 -0400

    send messages before channel clean up

--- lambdas/account-scoped/src/channelCapture/chatbotCallback.ts
+++ lambdas/account-scoped/src/channelCapture/chatbotCallback.ts
@@ -150,18 +150,6 @@ export const handleChatbotCallback: AccountScopedHandler = async (
       const twilioWorkspaceSid = await getWorkspaceSid(accountSid);
 
       const { lexResponse } = lexResult.data;
-      // If the session ended, we should unlock the channel to continue the Studio Flow
-      if (LexClient.isEndOfDialog(lexResponse)) {
-        await chatbotCallbackCleanup({
-          accountSid,
-          twilioClient,
-          conversation,
-          channel,
-          channelAttributes,
-          memory: LexClient.getBotMemory({ lexResponse }),
-          twilioWorkspaceSid,
-        });
-      }
 
       // TODO: unify with functions/channelCapture/channelCaptureHandlers.ts
       const messages = (lexResponse.messages || []).map(m => m.content || '');
@@ -183,6 +171,19 @@ export const handleChatbotCallback: AccountScopedHandler = async (
             xTwilioWebhookEnabled: 'true',
           });
         }
+      }
+      
+      // If the session ended, we should unlock the channel to continue the Studio Flow
+      if (LexClient.isEndOfDialog(lexResponse)) {
+        await chatbotCallbackCleanup({
+          accountSid,
+          twilioClient,
+          conversation,
+          channel,
+          channelAttributes,
+          memory: LexClient.getBotMemory({ lexResponse }),
+          twilioWorkspaceSid,
+        });
       }
 
       return newOk({});
