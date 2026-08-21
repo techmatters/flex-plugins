2bd7e55ea499fe8eb2dde1e99d6cf81bbbc16615
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2026-08-21 12:57:56 -0300

    chore: remove unwanted callback attempt increase

--- lambdas/scheduled-jobs-processor/src/index.ts
+++ lambdas/scheduled-jobs-processor/src/index.ts
@@ -31,7 +31,7 @@ const handleCreateVoicemailJob = async ({
     callSid: attributes.callSid,
     from: attributes.from,
     receivedTime: new Date().toISOString(),
-    callbackAttemptsMade: attributes.callbackAttemptsMade + 1,
+    callbackAttemptsMade: attributes.callbackAttemptsMade,
     workflowSid,
   });
 
