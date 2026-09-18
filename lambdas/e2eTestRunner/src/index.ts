8b053dcf6ce071b3fb75f006291f20c153326189
Stephen Hand <steveh@techmatters.org>
2026-09-18 17:59:10 +0100

    Logging levels & formatting tweaks

--- lambdas/e2eTestRunner/src/index.ts
+++ lambdas/e2eTestRunner/src/index.ts
@@ -170,7 +170,8 @@ export const handler = async (event: E2ETestEvent): Promise<void> => {
   }
 
   if (isError) {
+    console.error(`[${env.HL_ENV}/${env.HL}/${env.TEST_NAME}] Test run failed: `, result);
     throw result;
   }
-  console.info(`Test run (${env.TEST_NAME}) result: `, result);
+  console.info(`[${env.HL_ENV}/${env.HL}/${env.TEST_NAME}] Test run result: `, result);
 };
